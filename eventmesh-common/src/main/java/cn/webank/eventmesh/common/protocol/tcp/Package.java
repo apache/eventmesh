05015c6e42d9f2eef7bcfb0c35dda4a9146689b5
qqeasonchen <chenguangsheng@apache.org>
2020-07-30 11:03:44 +0800

    commit emesher,sdk-java,common

---
+++ eventmesh-common/src/main/java/cn/webank/eventmesh/common/protocol/tcp/Package.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.webank.eventmesh.common.protocol.tcp;
+
+public class Package {
+
+    private Header header;
+    private Object body;
+
+    public Package() {
+    }
+
+    public Package(Header header) {
+        this.header = header;
+    }
+
+    public Package(Header header, Object body) {
+        this.header = header;
+        this.body = body;
+    }
+
+    public Header getHeader() {
+        return header;
+    }
+
+    public void setHeader(Header header) {
+        this.header = header;
+    }
+
+    public Object getBody() {
+        return body;
+    }
+
+    public void setBody(Object body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "header=" + header +
+                ", body=" + body +
+                '}';
+    }
+
+
+}
