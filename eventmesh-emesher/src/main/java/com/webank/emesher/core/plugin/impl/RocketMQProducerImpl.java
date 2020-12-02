a1bbe89a94bb46a40552e790d40223ebad305723
xwm1992 <mike_xwm@126.com>
2020-12-02 12:14:37 +0800

    [issue #176]Support Spi for extended implementation to accommodate a variety of MQ

--- eventmesh-emesher/src/main/java/com/webank/emesher/core/plugin/impl/RocketMQProducerImpl.java
+++ eventmesh-emesher/src/main/java/com/webank/emesher/core/plugin/impl/RocketMQProducerImpl.java
@@ -15,92 +15,80 @@
  * limitations under the License.
  */
 
-package com.webank.emesher.core.plugin;
+package com.webank.emesher.core.plugin.impl;
 
 import com.webank.defibus.client.impl.producer.RRCallback;
 import com.webank.emesher.configuration.CommonConfiguration;
-import com.webank.emesher.core.plugin.impl.DeFiBusProducerImpl;
-import com.webank.emesher.core.plugin.impl.MeshMQProducer;
+import com.webank.emesher.constants.ProxyConstants;
+import com.webank.eventmesh.common.ThreadUtil;
 import org.apache.rocketmq.client.exception.MQBrokerException;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageClientIDSetter;
 import org.apache.rocketmq.remoting.exception.RemotingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ServiceLoader;
-
-public class MQProducerWrapper extends MQWrapper {
+public class RocketMQProducerImpl implements MeshMQProducer {
 
     public Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    protected MeshMQProducer meshMQProducer;
 
-    public synchronized void init(CommonConfiguration commonConfiguration, String producerGroup) throws Exception{
-        if (inited.get()) {
-            return;
-        }
-        meshMQProducer = getMeshMQProducer();
-        meshMQProducer.init(commonConfiguration, producerGroup);
+    protected DefaultMQProducer defaultMQProducer;
 
-        inited.compareAndSet(false, true);
+    @Override
+    public synchronized void init(CommonConfiguration commonConfiguration, String producerGroup) {
+
+        defaultMQProducer = new DefaultMQProducer(ProxyConstants.PRODUCER_GROUP_NAME_PREFIX + producerGroup);
+        defaultMQProducer.setHeartbeatBrokerInterval(commonConfiguration.heartbeatBrokerInterval);
+        defaultMQProducer.setPollNameServerInterval(commonConfiguration.pollNameServerInteval);
+        defaultMQProducer.setNamesrvAddr(commonConfiguration.namesrvAddr);
+        MessageClientIDSetter.createUniqID();
+        defaultMQProducer.setVipChannelEnabled(false);
+        defaultMQProducer.setCompressMsgBodyOverHowmuch(2 * 1024);
+
     }
 
-    private MeshMQProducer getMeshMQProducer() {
-        ServiceLoader<MeshMQProducer> meshMQProducerServiceLoader = ServiceLoader.load(MeshMQProducer.class);
-
-        if (meshMQProducerServiceLoader.iterator().hasNext()){
-            return  meshMQProducerServiceLoader.iterator().next();
-        }
-        return new DeFiBusProducerImpl();
-    }
-
+    @Override
     public synchronized void start() throws Exception {
-        if (started.get()) {
-            return;
-        }
 
-        meshMQProducer.start();
+        defaultMQProducer.start();
+        ThreadUtil.randomSleep(500);
+        defaultMQProducer.getDefaultMQProducerImpl().getmQClientFactory().updateTopicRouteInfoFromNameServer();
 
-        started.compareAndSet(false, true);
     }
 
+    @Override
     public synchronized void shutdown() throws Exception {
-        if (!inited.get()) {
-            return;
-        }
 
-        if (!started.get()) {
-            return;
-        }
-
-        meshMQProducer.shutdown();
-
-        inited.compareAndSet(true, false);
-        started.compareAndSet(true, false);
+        defaultMQProducer.shutdown();
     }
 
+    @Override
     public void send(Message message, SendCallback sendCallback) throws Exception {
-        meshMQProducer.send(message,sendCallback);
+        defaultMQProducer.send(message, sendCallback);
     }
 
+    @Override
     public void request(Message message, SendCallback sendCallback, RRCallback rrCallback, long timeout)
             throws InterruptedException, RemotingException, MQClientException, MQBrokerException {
-        meshMQProducer.request(message, sendCallback, rrCallback, timeout);
+        throw new UnsupportedOperationException("not support request-reply mode when eventstore=rocketmq");
     }
 
-    public Message request(Message message, long timeout) throws Exception {
-        return meshMQProducer.request(message, timeout);
+    @Override
+    public Message request(Message message, long timeout) throws InterruptedException, RemotingException, MQClientException, MQBrokerException {
+        throw new UnsupportedOperationException("not support request-reply mode when eventstore=rocketmq");
     }
 
+    @Override
     public boolean reply(final Message message, final SendCallback sendCallback) throws Exception {
-        meshMQProducer.reply(message, sendCallback);
-        return true;
+        throw new UnsupportedOperationException("not support request-reply mode when eventstore=rocketmq");
     }
 
+    @Override
     public DefaultMQProducer getDefaultMQProducer() {
-        return meshMQProducer.getDefaultMQProducer();
+        return defaultMQProducer;
     }
 }
