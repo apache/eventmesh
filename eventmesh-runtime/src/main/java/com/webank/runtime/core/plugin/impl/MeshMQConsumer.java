addc6722b5d2aa19b5345fe21b811ce1dd0acf36
xwm1992 <mike_xwm@126.com>
2020-12-08 14:31:00 +0800

    event mesh project architecture adjustment

---
+++ eventmesh-runtime/src/main/java/com/webank/runtime/core/plugin/impl/MeshMQConsumer.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.webank.runtime.core.plugin.impl;
+
+import com.webank.runtime.configuration.CommonConfiguration;
+import com.webank.runtime.patch.ProxyConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+public interface MeshMQConsumer {
+
+    void start() throws Exception;
+
+    void updateOffset(List<MessageExt> msgs, ProxyConsumeConcurrentlyContext context);
+
+    void init(boolean isBroadcast, CommonConfiguration commonConfiguration,
+                                  String consumerGroup) throws Exception;
+
+    void registerMessageListener(MessageListenerConcurrently messageListenerConcurrently);
+
+    void subscribe(String topic) throws Exception;
+
+    void unsubscribe(String topic) throws Exception;
+
+    boolean isPause();
+
+    void pause();
+
+    void shutdown() throws Exception;
+
+    void setInstanceName(String instanceName);
+}
---
+++ eventmesh-runtime/src/main/java/com/webank/runtime/patch/ProxyMessageListenerConcurrently.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.webank.runtime.patch;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public abstract class ProxyMessageListenerConcurrently implements MessageListenerConcurrently {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProxyMessageListenerConcurrently.class);
+
+    @Override
+    public ConsumeConcurrentlyStatus consumeMessage(final List<MessageExt> msgs,
+                                                    final ConsumeConcurrentlyContext context) {
+        ConsumeConcurrentlyStatus status = null;
+
+        if (CollectionUtils.isEmpty(msgs)) {
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        }
+
+        MessageExt msg = msgs.get(0);
+        try {
+            ProxyConsumeConcurrentlyContext proxyConsumeConcurrentlyContext = (ProxyConsumeConcurrentlyContext) context;
+            ProxyConsumeConcurrentlyStatus proxyConsumeStatus = handleMessage(msg, proxyConsumeConcurrentlyContext);
+            try {
+                switch (proxyConsumeStatus) {
+                    case CONSUME_SUCCESS:
+                        return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+                    case RECONSUME_LATER:
+                        return ConsumeConcurrentlyStatus.RECONSUME_LATER;
+                    case CONSUME_FINISH:
+                        proxyConsumeConcurrentlyContext.setManualAck(true);
+                        return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+                }
+            } catch (Throwable e) {
+                LOG.info("handleMessage fail", e);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        } catch (Throwable e) {
+            LOG.info("handleMessage fail", e);
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        }
+        return status;
+    }
+
+    public abstract ProxyConsumeConcurrentlyStatus handleMessage(MessageExt msg, ProxyConsumeConcurrentlyContext context);
+}
