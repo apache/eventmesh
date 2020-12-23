13cf9e49a786c77402bea5eec1fdd2c220be96de
lrhkobe <liangrh7@163.com>
2020-12-23 21:40:38 +0800

    fix rr reply bug,rollback delete config file in eventmesh-common

--- eventmesh-connector-rocketmq/src/main/java/com/webank/eventmesh/connector/rocketmq/consumer/RocketMQConsumerImpl.java
+++ eventmesh-connector-rocketmq/src/main/java/com/webank/eventmesh/connector/rocketmq/consumer/RocketMQConsumerImpl.java
@@ -53,6 +53,7 @@ public class RocketMQConsumerImpl implements MeshMQPushConsumer {
         clientConfiguration.init();
         boolean isBroadcast = Boolean.valueOf(keyValue.getString("isBroadcast"));
         String consumerGroup = keyValue.getString("consumerGroup");
+        String instanceName = keyValue.getString("instanceName");
 
         if(isBroadcast){
             consumerGroup = Constants.CONSUMER_GROUP_NAME_PREFIX + Constants.BROADCAST_PREFIX + consumerGroup;
@@ -65,12 +66,14 @@ public class RocketMQConsumerImpl implements MeshMQPushConsumer {
 
         properties.put("ACCESS_POINTS", omsNamesrv)
                 .put("REGION", "namespace")
-                .put(OMSBuiltinKeys.CONSUMER_ID, consumerGroup);
+                .put(OMSBuiltinKeys.CONSUMER_ID, consumerGroup)
+                .put("instanceName", instanceName);
         if (isBroadcast){
             properties.put("MESSAGE_MODEL", MessageModel.BROADCASTING.name());
         }else {
             properties.put("MESSAGE_MODEL", MessageModel.CLUSTERING.name());
         }
+
         MessagingAccessPoint messagingAccessPoint = OMS.getMessagingAccessPoint(omsNamesrv, properties);
         pushConsumer = (PushConsumerImpl)messagingAccessPoint.createPushConsumer();
     }
@@ -78,11 +81,6 @@ public class RocketMQConsumerImpl implements MeshMQPushConsumer {
     @Override
     public void subscribe(String topic, MessageListener listener) throws Exception {
         pushConsumer.attachQueue(topic, listener);
-    }
-
-    @Override
-    public void setInstanceName(String instanceName) {
-        pushConsumer.getRocketmqPushConsumer().setInstanceName(instanceName);
     }
 
     @Override
