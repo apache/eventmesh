84688eac5419ab72b00857a09ca20b05ce02827d
nanoxiong <xiong_mengfei@163.com>
2021-06-22 18:08:53 +0800

    miss group name set for userAgent (#395)
    * remove custom concept{dcn}
    
    * remove custom concept{dcn}
    
    * remove custom concept{dcn}
    
    * remove custom concept{dcn}
    
    * remove custom concept{dcn}
    
    * remove custom concept{dcn}
    
    * remove custom concept{dcn}
    
    * bugfix : miss group set
    
    * bugfix : miss group set

--- eventmesh-sdk-java/src/main/java/org/apache/eventmesh/client/tcp/common/MessageUtils.java
+++ eventmesh-sdk-java/src/main/java/org/apache/eventmesh/client/tcp/common/MessageUtils.java
@@ -108,7 +108,8 @@ public class MessageUtils {
         user.setPid(agent.getPid());
         user.setVersion(agent.getVersion());
         user.setIdc(agent.getIdc());
-
+        user.setConsumerGroup(agent.getConsumerGroup());
+        user.setProducerGroup(agent.getProducerGroup());
         user.setPurpose(EventMeshCommon.USER_AGENT_PURPOSE_SUB);
         return user;
     }
@@ -124,7 +125,7 @@ public class MessageUtils {
         user.setPid(agent.getPid());
         user.setVersion(agent.getVersion());
         user.setIdc(agent.getIdc());
-
+        user.setProducerGroup(agent.getProducerGroup());
         user.setPurpose(EventMeshCommon.USER_AGENT_PURPOSE_PUB);
         return user;
     }
