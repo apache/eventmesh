3801f81f400dd218ead9c60b351430ccc7ece200
tydhot <583125614@qq.com>
2021-08-20 00:22:41 +0800

    otel metric polish

--- eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/metrics/opentelemetry/OpenTelemetryHTTPMetricsExporter.java
+++ eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/metrics/opentelemetry/OpenTelemetryHTTPMetricsExporter.java
@@ -15,303 +15,295 @@
  * limitations under the License.
  */
 
-package org.apache.eventmesh.runtime.metrics.openTelemetry;
+package org.apache.eventmesh.runtime.metrics.opentelemetry;
 
+import io.opentelemetry.api.metrics.GlobalMeterProvider;
 import io.opentelemetry.api.metrics.Meter;
-import io.opentelemetry.api.metrics.MeterProvider;
 import io.opentelemetry.api.metrics.common.Labels;
 import org.apache.eventmesh.runtime.configuration.EventMeshHTTPConfiguration;
 import org.apache.eventmesh.runtime.metrics.http.HTTPMetricsServer;
 import org.apache.eventmesh.runtime.metrics.http.SummaryMetrics;
 
 public class OpenTelemetryHTTPMetricsExporter {
-    OpenTelemetryExporterConfiguration configuration = new OpenTelemetryExporterConfiguration();
 
-    private Meter meter;
+    private final SummaryMetrics summaryMetrics;
 
-    private SummaryMetrics summaryMetrics;
-
-    private HTTPMetricsServer httpMetricsServer;
+    private final HTTPMetricsServer httpMetricsServer;
 
     public OpenTelemetryHTTPMetricsExporter(HTTPMetricsServer httpMetricsServer, EventMeshHTTPConfiguration eventMeshHTTPConfiguration) {
+        OpenTelemetryPrometheusExporter.initialize(eventMeshHTTPConfiguration);
         this.httpMetricsServer = httpMetricsServer;
-        summaryMetrics = httpMetricsServer.summaryMetrics;
-
-        // it is important to initialize the OpenTelemetry SDK as early as possible in your process.
-        MeterProvider meterProvider = configuration.initializeOpenTelemetry(eventMeshHTTPConfiguration);
-        meter = meterProvider.get("OpenTelemetryHTTPExporter", "0.13.1");
+        this.summaryMetrics = httpMetricsServer.summaryMetrics;
     }
 
     public void start(){
-        if (meter==null){
-            return;
-        }
+        Meter meter = GlobalMeterProvider.getMeter("apache-eventmesh");
         //maxHTTPTPS
         meter
-                .doubleValueObserverBuilder("eventmesh.http.request.tps.elapsed.max")
-                .setDescription("max TPS of HTTP")
+                .doubleValueObserverBuilder("eventmesh.http.request.tps.max")
+                .setDescription("max TPS of HTTP.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.maxHTTPTPS(),Labels.empty()))
                 .build();
 
         //avgHTTPTPS
         meter
-                .doubleValueObserverBuilder("eventmesh.http.request.tps.elapsed.avg")
-                .setDescription("avg TPS of HTTP")
+                .doubleValueObserverBuilder("eventmesh.http.request.tps.avg")
+                .setDescription("avg TPS of HTTP.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.avgHTTPTPS(),Labels.empty()))
                 .build();
 
         //maxHTTPCost
         meter
-                .longValueObserverBuilder("eventmesh.http.request.elapsed.max")
-                .setDescription("max cost of HTTP")
+                .longValueObserverBuilder("eventmesh.http.request.cost.max")
+                .setDescription("max cost of HTTP.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.maxHTTPCost(), Labels.empty()))
                 .build();
 
         //avgHTTPCost
         meter
-                .doubleValueObserverBuilder("eventmesh.http.request.elapsed.avg")
-                .setDescription("avg cost of HTTP")
+                .doubleValueObserverBuilder("eventmesh.http.request.cost.avg")
+                .setDescription("avg cost of HTTP.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.avgHTTPCost(), Labels.empty()))
                 .build();
 
         //avgHTTPBodyDecodeCost
         meter
-                .doubleValueObserverBuilder("eventmesh.http.body.decode.cost.elapsed.avg")
-                .setDescription("avg body decode cost of HTTP")
+                .doubleValueObserverBuilder("eventmesh.http.body.decode.cost.avg")
+                .setDescription("avg body decode cost of HTTP.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.avgHTTPBodyDecodeCost(), Labels.empty()))
                 .build();
 
         //httpDiscard
         meter
-                .longValueObserverBuilder("eventmesh.http.request.discard.elapsed")
-                .setDescription("http request discard")
+                .longValueObserverBuilder("eventmesh.http.request.discard.num")
+                .setDescription("http request discard num.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.getHttpDiscard(), Labels.empty()))
                 .build();
 
         //maxBatchSendMsgTPS
         meter
-                .doubleValueObserverBuilder("eventmesh.batch.send.message.tps.elapsed.max")
-                .setDescription("max of batch send message tps")
+                .doubleValueObserverBuilder("eventmesh.batch.send.message.tps.max")
+                .setDescription("max of batch send message tps.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.maxSendBatchMsgTPS(), Labels.empty()))
                 .build();
 
         //avgBatchSendMsgTPS
         meter
-                .doubleValueObserverBuilder("eventmesh.batch.send.message.tps.elapsed.avg")
-                .setDescription("avg of batch send message tps")
+                .doubleValueObserverBuilder("eventmesh.batch.send.message.tps.avg")
+                .setDescription("avg of batch send message tps.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.avgSendBatchMsgTPS(), Labels.empty()))
                 .build();
 
         //sum
         meter
-                .doubleValueObserverBuilder("eventmesh.batch.send.message.elapsed.sum")
-                .setDescription("sum of batch send message number")
+                .doubleValueObserverBuilder("eventmesh.batch.send.message.num")
+                .setDescription("sum of batch send message number.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.getSendBatchMsgNumSum(), Labels.empty()))
                 .build();
 
         //sumFail
         meter
-                .doubleValueObserverBuilder("eventmesh.batch.send.message.fail.elapsed.sum")
-                .setDescription("sum of batch send message fail message number")
+                .doubleValueObserverBuilder("eventmesh.batch.send.message.fail.num")
+                .setDescription("sum of batch send message fail message number.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.getSendBatchMsgFailNumSum(), Labels.empty()))
                 .build();
 
         //sumFailRate
         meter
-                .doubleValueObserverBuilder("eventmesh.batch.send.message.fail.rate.elapsed")
-                .setDescription("send batch message fail rate")
+                .doubleValueObserverBuilder("eventmesh.batch.send.message.fail.rate")
+                .setDescription("send batch message fail rate.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.getSendBatchMsgFailRate(), Labels.empty()))
                 .build();
 
         //discard
         meter
-                .doubleValueObserverBuilder("eventmesh.batch.send.message.discard.elapsed.sum")
-                .setDescription("sum of send batch message discard number")
+                .doubleValueObserverBuilder("eventmesh.batch.send.message.discard.num")
+                .setDescription("sum of send batch message discard number.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.getSendBatchMsgDiscardNumSum(), Labels.empty()))
                 .build();
 
         //maxSendMsgTPS
         meter
-                .doubleValueObserverBuilder("eventmesh.send.message.tps.elapsed.max")
-                .setDescription("max of send message tps")
+                .doubleValueObserverBuilder("eventmesh.send.message.tps.max")
+                .setDescription("max of send message tps.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.maxSendMsgTPS(), Labels.empty()))
                 .build();
 
         //avgSendMsgTPS
         meter
-                .doubleValueObserverBuilder("eventmesh.send.message.tps.elapsed.avg")
-                .setDescription("avg of send message tps")
+                .doubleValueObserverBuilder("eventmesh.send.message.tps.avg")
+                .setDescription("avg of send message tps.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.avgSendMsgTPS(), Labels.empty()))
                 .build();
 
         //sum
         meter
-                .doubleValueObserverBuilder("eventmesh.send.message.elapsed.sum")
-                .setDescription("sum of send message number")
+                .doubleValueObserverBuilder("eventmesh.send.message.num")
+                .setDescription("sum of send message number.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.getSendMsgNumSum(), Labels.empty()))
                 .build();
 
         //sumFail
         meter
-                .doubleValueObserverBuilder("eventmesh.send.message.fail.elapsed.sum")
-                .setDescription("sum of send message fail number")
+                .doubleValueObserverBuilder("eventmesh.send.message.fail.num")
+                .setDescription("sum of send message fail number.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.getSendMsgFailNumSum(), Labels.empty()))
                 .build();
 
         //sumFailRate
         meter
-                .doubleValueObserverBuilder("eventmesh.send.message.fail.rate.elapsed")
-                .setDescription("send message fail rate")
+                .doubleValueObserverBuilder("eventmesh.send.message.fail.rate")
+                .setDescription("send message fail rate.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.getSendMsgFailRate(), Labels.empty()))
                 .build();
 
         //replyMsg
         meter
-                .doubleValueObserverBuilder("eventmesh.reply.message.elapsed.sum")
-                .setDescription("sum of reply message number")
+                .doubleValueObserverBuilder("eventmesh.reply.message.num")
+                .setDescription("sum of reply message number.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.getReplyMsgNumSum(), Labels.empty()))
                 .build();
 
         //replyFail
         meter
-                .doubleValueObserverBuilder("eventmesh.reply.message.fail.elapsed.sum")
-                .setDescription("sum of reply message fail number")
+                .doubleValueObserverBuilder("eventmesh.reply.message.fail.num")
+                .setDescription("sum of reply message fail number.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.getReplyMsgFailNumSum(), Labels.empty()))
                 .build();
 
         //maxPushMsgTPS
         meter
-                .doubleValueObserverBuilder("eventmesh.push.message.tps.elapsed.max")
-                .setDescription("max of push message tps")
+                .doubleValueObserverBuilder("eventmesh.push.message.tps.max")
+                .setDescription("max of push message tps.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.maxPushMsgTPS(), Labels.empty()))
                 .build();
 
         //avgPushMsgTPS
         meter
-                .doubleValueObserverBuilder("eventmesh.push.message.tps.elapsed.avg")
-                .setDescription("avg of push message tps")
+                .doubleValueObserverBuilder("eventmesh.push.message.tps.avg")
+                .setDescription("avg of push message tps.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.avgPushMsgTPS(), Labels.empty()))
                 .build();
 
         //sum
         meter
-                .doubleValueObserverBuilder("eventmesh.http.push.message.elapsed.sum")
-                .setDescription("sum of http push message number")
+                .doubleValueObserverBuilder("eventmesh.http.push.message.num")
+                .setDescription("sum of http push message number.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.getHttpPushMsgNumSum(), Labels.empty()))
                 .build();
 
         //sumFail
         meter
-                .doubleValueObserverBuilder("eventmesh.http.push.message.fail.elapsed.sum")
-                .setDescription("sum of http push message fail number")
+                .doubleValueObserverBuilder("eventmesh.http.push.message.fail.num")
+                .setDescription("sum of http push message fail number.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.getHttpPushFailNumSum(), Labels.empty()))
                 .build();
 
         //sumFailRate
         meter
-                .doubleValueObserverBuilder("eventmesh.http.push.message.fail.rate.elapsed")
-                .setDescription("http push message fail rate")
+                .doubleValueObserverBuilder("eventmesh.http.push.message.fail.rate")
+                .setDescription("http push message fail rate.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.getHttpPushMsgFailRate(), Labels.empty()))
                 .build();
 
         //maxClientLatency
         meter
-                .doubleValueObserverBuilder("eventmesh.http.push.latency.elapsed.max")
-                .setDescription("max of http push latency")
+                .doubleValueObserverBuilder("eventmesh.http.push.latency.max")
+                .setDescription("max of http push latency.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.maxHTTPPushLatency(), Labels.empty()))
                 .build();
 
         //avgClientLatency
         meter
-                .doubleValueObserverBuilder("eventmesh.http.push.latency.elapsed.avg")
-                .setDescription("avg of http push latency")
+                .doubleValueObserverBuilder("eventmesh.http.push.latency.avg")
+                .setDescription("avg of http push latency.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.avgHTTPPushLatency(), Labels.empty()))
                 .build();
 
         //batchMsgQ
         meter
-                .longValueObserverBuilder("eventmesh.batch.message.queue.elapsed.size")
-                .setDescription("size of batch message queue")
+                .longValueObserverBuilder("eventmesh.batch.message.queue.size")
+                .setDescription("size of batch message queue.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(httpMetricsServer.getBatchMsgQ(), Labels.empty()))
                 .build();
 
         //sendMsgQ
         meter
-                .longValueObserverBuilder("eventmesh.send.message.queue.elapsed.size")
-                .setDescription("size of send message queue")
+                .longValueObserverBuilder("eventmesh.send.message.queue.size")
+                .setDescription("size of send message queue.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(httpMetricsServer.getSendMsgQ(), Labels.empty()))
                 .build();
 
         //pushMsgQ
         meter
-                .longValueObserverBuilder("eventmesh.push.message.queue.elapsed.size")
-                .setDescription("size of push message queue")
+                .longValueObserverBuilder("eventmesh.push.message.queue.size")
+                .setDescription("size of push message queue.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(httpMetricsServer.getPushMsgQ(), Labels.empty()))
                 .build();
 
         //httpRetryQ
         meter
-                .longValueObserverBuilder("eventmesh.http.retry.queue.elapsed.size")
-                .setDescription("size of http retry queue")
+                .longValueObserverBuilder("eventmesh.http.retry.queue.size")
+                .setDescription("size of http retry queue.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(httpMetricsServer.getHttpRetryQ(), Labels.empty()))
                 .build();
 
         //batchAvgSend2MQCost
         meter
-                .doubleValueObserverBuilder("eventmesh.batch.send.message.cost.elapsed.avg")
-                .setDescription("avg of batch send message cost")
+                .doubleValueObserverBuilder("eventmesh.batch.send.message.cost.avg")
+                .setDescription("avg of batch send message cost.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.avgBatchSendMsgCost(), Labels.empty()))
                 .build();
 
         //avgSend2MQCost
         meter
-                .doubleValueObserverBuilder("eventmesh.send.message.cost.elapsed.avg")
-                .setDescription("avg of send message cost")
+                .doubleValueObserverBuilder("eventmesh.send.message.cost.avg")
+                .setDescription("avg of send message cost.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.avgSendMsgCost(), Labels.empty()))
                 .build();
 
         //avgReply2MQCost
         meter
-                .doubleValueObserverBuilder("eventmesh.reply.message.cost.elapsed.avg")
-                .setDescription("avg of reply message cost")
+                .doubleValueObserverBuilder("eventmesh.reply.message.cost.avg")
+                .setDescription("avg of reply message cost.")
                 .setUnit("HTTP")
                 .setUpdater(result -> result.observe(summaryMetrics.avgReplyMsgCost(), Labels.empty()))
                 .build();
     }
 
     public void shutdown(){
-        configuration.shutdownPrometheusEndpoint();
+        OpenTelemetryPrometheusExporter.shutdown();
     }
 }
