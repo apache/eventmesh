2b10d7e7e573a9f441f2bbdb6180b7e75c98e407
YuDong Tang <583125614@qq.com>
2021-08-25 19:46:27 +0800

    [ISSUE #505]remove all RandomStringUtils. (#510)
    * remove all RandomStringUtils.
    
    * remove all RandomStringUtils.

--- eventmesh-test/src/main/java/org/apache/eventmesh/http/demo/AsyncPublishInstance.java
+++ eventmesh-test/src/main/java/org/apache/eventmesh/http/demo/AsyncPublishInstance.java
@@ -19,13 +19,13 @@ package org.apache.eventmesh.http.demo;
 
 import java.util.Properties;
 
-import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.eventmesh.client.http.conf.LiteClientConfig;
 import org.apache.eventmesh.client.http.producer.LiteProducer;
 import org.apache.eventmesh.common.Constants;
 import org.apache.eventmesh.common.IPUtil;
 import org.apache.eventmesh.common.LiteMessage;
+import org.apache.eventmesh.common.RandomStringUtil;
 import org.apache.eventmesh.common.ThreadUtil;
 import org.apache.eventmesh.util.Utils;
 import org.slf4j.Logger;
@@ -67,11 +67,11 @@ public class AsyncPublishInstance {
             liteProducer.start();
             for (int i = 0; i < messageSize; i++) {
                 LiteMessage liteMessage = new LiteMessage();
-                liteMessage.setBizSeqNo(RandomStringUtils.randomNumeric(30))
+                liteMessage.setBizSeqNo(RandomStringUtil.generateNum(30))
 //                    .setContent("contentStr with special protocal")
                         .setContent("testPublishMessage")
                         .setTopic(topic)
-                        .setUniqueId(RandomStringUtils.randomNumeric(30))
+                        .setUniqueId(RandomStringUtil.generateNum(30))
                         .addProp(Constants.EVENTMESH_MESSAGE_CONST_TTL, String.valueOf(4 * 1000));
 
                 boolean flag = liteProducer.publish(liteMessage);
