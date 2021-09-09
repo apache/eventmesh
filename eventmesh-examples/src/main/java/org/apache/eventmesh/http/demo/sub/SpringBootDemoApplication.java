16ae31a622fe493358a5e7011787f8d2e915e4b2
qqeasonchen <qqeasonchen@gmail.com>
2021-09-09 20:53:31 +0800

    change eventmesh-test to eventmesh-examples
    Signed-off-by: qqeasonchen <qqeasonchen@gmail.com>

--- eventmesh-examples/src/main/java/org/apache/eventmesh/http/demo/sub/SpringBootDemoApplication.java
+++ eventmesh-examples/src/main/java/org/apache/eventmesh/http/demo/sub/SpringBootDemoApplication.java
@@ -18,12 +18,10 @@
 package org.apache.eventmesh.http.demo.sub;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
