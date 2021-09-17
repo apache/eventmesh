498327d21b8a3cb0e68fa6c5d3078c753193353a
jinrongluo <kapoking@gmail.com>
2021-09-17 09:52:35 -0400

    Issue #523 adding FlowControl Ratelimiter support for Http message send processors

--- eventmesh-common/src/main/java/org/apache/eventmesh/common/protocol/http/common/EventMeshRetCode.java
+++ eventmesh-common/src/main/java/org/apache/eventmesh/common/protocol/http/common/EventMeshRetCode.java
@@ -39,7 +39,8 @@ public enum EventMeshRetCode {
     EVENTMESH_SUBSCRIBE_ERR(17, "eventMesh subscribe err"),
     EVENTMESH_UNSUBSCRIBE_ERR(18, "eventMesh unsubscribe err"),
     EVENTMESH_HEARTBEAT_ERR(19, "eventMesh heartbeat err"),
-    EVENTMESH_ACL_ERR(20, "eventMesh acl err");
+    EVENTMESH_ACL_ERR(20, "eventMesh acl err"),
+    EVENTMESH_HTTP_MES_SEND_OVER_LIMIT_ERR(20, "eventMesh http msg send over the limit, ");
 
     private Integer retCode;
 
