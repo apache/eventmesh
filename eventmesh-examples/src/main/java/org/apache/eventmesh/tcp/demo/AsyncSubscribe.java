882268822b0a1b412e2a011cfa67208cf70d2a99
wangshaojie4039 <15001782969@163.com>
2021-11-02 17:47:49 +0800

    [ISSUE #563] SDK SUPPORT CLOUD EVENT (#575)
    * SDK SUPPORT CLOUD EVENT
    
    * SDK SUPPORT CLOUD EVENT
    
    * SDK SUPPORT CLOUD EVENT
    
    * SDK SUPPORT CLOUD EVENT
    
    * SDK SUPPORT CLOUD EVENT
    
    * SDK SUPPORT CLOUD EVENT
    
    * SDK SUPPORT CLOUD EVENT
    
    * SDK SUPPORT CLOUD EVENT
    
    * SDK SUPPORT CLOUD EVENT
    
    Co-authored-by: wangshaojie <wangshaojie@cmss.chinamobile.com>

--- eventmesh-examples/src/main/java/org/apache/eventmesh/tcp/demo/AsyncSubscribe.java
+++ eventmesh-examples/src/main/java/org/apache/eventmesh/tcp/demo/AsyncSubscribe.java
@@ -34,7 +34,7 @@ import org.apache.eventmesh.util.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class AsyncSubscribe implements ReceiveMsgHook {
+public class AsyncSubscribe implements ReceiveMsgHook<EventMeshMessage> {
 
     public static Logger logger = LoggerFactory.getLogger(AsyncSubscribe.class);
 
@@ -68,7 +68,12 @@ public class AsyncSubscribe implements ReceiveMsgHook {
 
     @Override
     public void handle(Package msg, ChannelHandlerContext ctx) {
-        EventMeshMessage eventMeshMessage = (EventMeshMessage) msg.getBody();
+        EventMeshMessage eventMeshMessage = convert(msg);
         logger.info("receive async msg====================={}", eventMeshMessage);
+    }
+
+    @Override
+    public EventMeshMessage convert(Package pkg) {
+        return (EventMeshMessage) pkg.getBody();
     }
 }
