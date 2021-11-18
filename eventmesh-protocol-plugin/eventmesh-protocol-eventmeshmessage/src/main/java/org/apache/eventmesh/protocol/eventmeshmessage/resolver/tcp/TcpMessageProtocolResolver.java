09af4fd85a45e67c00798b6e81d6c55fda09ce13
mike_xwm <mike_xwm@126.com>
2021-11-18 17:44:01 +0800

    [Feature #562] Implement EventMeshMessage protocol adaptor (#597)
    * [Feature #564] Support CloudEvents protocols for pub/sub in EventMesh-feature design
    
    * support cloudevents api in eventmesh-connector-api module
    
    * fix checkStyle
    
    * fix checkStyle
    
    * fix checkStyle
    
    * 1.support LifeCycle.java
    2.update Consumer and Producer
    
    * fix remove the extra blank line
    
    * support cloudEvents
    
    * Add files via upload
    
    * Update README.md
    
    * support cloudEvents
    
    * support cloudEvents
    
    * [ISSUE #580] Add checkstyle gradle plugin (#581)
    
    * Add checkstyle gradle plugin, change plugin package
    
    * skip check in ci
    
    * support cloudEvents
    
    * support cloudevents
    
    * update wechat-official qr code
    
    * update mesh-helper qr code
    
    * Add files via upload
    
    * update README.md
    
    * update README.md
    
    * Update .asf.yaml
    
    * support cloudEvents
    
    * support cloudEvents
    
    * [ISSUE #588] Fix typo in README.md (#589)
    
    close #588
    
    * support cloudEvents
    
    * [Bug #590] Consumer subscription topic is invalid (#590) (#592)
    
    * [Bug #590] Consumer subscription topic is invalid (#590)
    
    * [Bug #590] Consumer subscription topic is invalid (#590)
    
    close #590
    
    * support cloudEvents adaptor
    
    * [Feature #562] Implement CloudEvents adaptor
    
    * [Feature #562] Implement EventMeshMessage protocol adaptor
    
    * supplement apache header
    
    Co-authored-by: Eason Chen <qqeasonchen@gmail.com>
    Co-authored-by: Wenjun Ruan <wenjun@apache.org>
    Co-authored-by: Nicholas Zhan <zhan_nicholas@outlook.com>
    Co-authored-by: hagsyn <44764414+hagsyn@users.noreply.github.com>

---
+++ eventmesh-protocol-plugin/eventmesh-protocol-eventmeshmessage/src/main/java/org/apache/eventmesh/protocol/eventmeshmessage/resolver/tcp/TcpMessageProtocolResolver.java
@@ -0,0 +1,106 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.eventmesh.protocol.eventmeshmessage.resolver.tcp;
+
+import io.cloudevents.CloudEvent;
+import io.cloudevents.SpecVersion;
+import io.cloudevents.core.builder.CloudEventBuilder;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.eventmesh.common.Constants;
+import org.apache.eventmesh.common.protocol.tcp.EventMeshMessage;
+import org.apache.eventmesh.common.protocol.tcp.Header;
+import org.apache.eventmesh.common.protocol.tcp.Package;
+import org.apache.eventmesh.protocol.api.exception.ProtocolHandleException;
+import org.apache.eventmesh.protocol.eventmeshmessage.EventMeshMessageProtocolConstant;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TcpMessageProtocolResolver {
+
+
+    public static CloudEvent buildEvent(Header header, Object body) throws ProtocolHandleException {
+
+        CloudEventBuilder cloudEventBuilder;
+
+        String protocolType = header.getProperty(Constants.PROTOCOL_TYPE).toString();
+        String protocolVersion = header.getProperty(Constants.PROTOCOL_VERSION).toString();
+        String protocolDesc = header.getProperty(Constants.PROTOCOL_DESC).toString();
+
+        if (StringUtils.isBlank(protocolType)
+                || StringUtils.isBlank(protocolVersion)
+                || StringUtils.isBlank(protocolDesc)) {
+            throw new ProtocolHandleException(String.format("invalid protocol params protocolType %s|protocolVersion %s|protocolDesc %s",
+                    protocolType, protocolVersion, protocolDesc));
+        }
+
+        if (!StringUtils.equals(EventMeshMessageProtocolConstant.PROTOCOL_NAME, protocolType)) {
+            throw new ProtocolHandleException(String.format("Unsupported protocolType: %s", protocolType));
+        }
+
+        EventMeshMessage message = (EventMeshMessage) body;
+
+        String topic = message.getTopic();
+
+        String content = message.getBody();
+
+        if (StringUtils.equals(SpecVersion.V1.toString(), protocolVersion)) {
+            cloudEventBuilder = CloudEventBuilder.v1();
+
+        } else if (StringUtils.equals(SpecVersion.V03.toString(), protocolVersion)) {
+            cloudEventBuilder = CloudEventBuilder.v03();
+
+        } else {
+            throw new ProtocolHandleException(String.format("Unsupported protocolVersion: %s", protocolVersion));
+        }
+
+        cloudEventBuilder = cloudEventBuilder
+                .withId(header.getSeq())
+                .withSubject(topic)
+                .withData(content.getBytes(StandardCharsets.UTF_8));
+
+        for (String propKey : header.getProperties().keySet()) {
+            cloudEventBuilder.withExtension(propKey, header.getProperty(propKey).toString());
+        }
+
+        for (String propKey : message.getProperties().keySet()) {
+            cloudEventBuilder.withExtension(propKey, message.getProperties().get(propKey));
+        }
+
+        return cloudEventBuilder.build();
+
+    }
+
+    public static Package buildEventMeshMessage(CloudEvent cloudEvent) {
+        Package pkg = new Package();
+        EventMeshMessage eventMeshMessage = new EventMeshMessage();
+        eventMeshMessage.setTopic(cloudEvent.getSubject());
+        eventMeshMessage.setBody(new String(cloudEvent.getData().toBytes(), StandardCharsets.UTF_8));
+
+        Map<String, String> prop = new HashMap<>();
+        for (String extKey : cloudEvent.getExtensionNames()) {
+            prop.put(extKey, cloudEvent.getExtension(extKey).toString());
+        }
+        eventMeshMessage.setProperties(prop);
+
+        pkg.setBody(eventMeshMessage);
+
+        return pkg;
+    }
+}
