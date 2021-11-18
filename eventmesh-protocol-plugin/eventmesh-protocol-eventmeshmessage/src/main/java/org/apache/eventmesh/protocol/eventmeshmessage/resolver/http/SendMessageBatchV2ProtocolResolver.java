3fec9e1c779301cf681245a5450aecf4240e3fdc
mike_xwm <mike_xwm@126.com>
2021-11-18 17:44:01 +0800

    [Feature #562] Implement EventMeshMessage protocol adaptor (#597)
    * [Feature #564] Support CloudEvents protocols for pub/sub in EventMesh-feature design
    
    * support cloudevents api in eventmesh-connector-api module
    
    * fix checkStyle
    
    * fix checkStyle
    
    * fix checkStyle
    
    * 1.support LifeCycle.java
    2.update Consumer and Producer
    
    * fix remove the extra blank line
    
    * support cloudEvents
    
    * Add files via upload
    
    * Update README.md
    
    * support cloudEvents
    
    * support cloudEvents
    
    * [ISSUE #580] Add checkstyle gradle plugin (#581)
    
    * Add checkstyle gradle plugin, change plugin package
    
    * skip check in ci
    
    * support cloudEvents
    
    * support cloudevents
    
    * update wechat-official qr code
    
    * update mesh-helper qr code
    
    * Add files via upload
    
    * update README.md
    
    * update README.md
    
    * Update .asf.yaml
    
    * support cloudEvents
    
    * support cloudEvents
    
    * [ISSUE #588] Fix typo in README.md (#589)
    
    close #588
    
    * support cloudEvents
    
    * [Bug #590] Consumer subscription topic is invalid (#590) (#592)
    
    * [Bug #590] Consumer subscription topic is invalid (#590)
    
    * [Bug #590] Consumer subscription topic is invalid (#590)
    
    close #590
    
    * support cloudEvents adaptor
    
    * [Feature #562] Implement CloudEvents adaptor
    
    * [Feature #562] Implement EventMeshMessage protocol adaptor
    
    * supplement apache header
    
    Co-authored-by: Eason Chen <qqeasonchen@gmail.com>
    Co-authored-by: Wenjun Ruan <wenjun@apache.org>
    Co-authored-by: Nicholas Zhan <zhan_nicholas@outlook.com>
    Co-authored-by: hagsyn <44764414+hagsyn@users.noreply.github.com>

--- eventmesh-protocol-plugin/eventmesh-protocol-eventmeshmessage/src/main/java/org/apache/eventmesh/protocol/eventmeshmessage/resolver/http/SendMessageBatchV2ProtocolResolver.java
+++ eventmesh-protocol-plugin/eventmesh-protocol-eventmeshmessage/src/main/java/org/apache/eventmesh/protocol/eventmeshmessage/resolver/http/SendMessageBatchV2ProtocolResolver.java
@@ -1,4 +1,21 @@
-package org.apache.eventmesh.protocol.cloudevents.resolver.http;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.eventmesh.protocol.eventmeshmessage.resolver.http;
 
 import io.cloudevents.CloudEvent;
 import io.cloudevents.SpecVersion;
@@ -12,8 +29,11 @@ import org.apache.eventmesh.common.protocol.http.common.ProtocolKey;
 import org.apache.eventmesh.common.protocol.http.common.ProtocolVersion;
 import org.apache.eventmesh.common.protocol.http.header.Header;
 import org.apache.eventmesh.common.protocol.http.header.message.SendMessageBatchV2RequestHeader;
+import org.apache.eventmesh.common.protocol.tcp.EventMeshMessage;
 import org.apache.eventmesh.common.utils.JsonUtils;
 import org.apache.eventmesh.protocol.api.exception.ProtocolHandleException;
+
+import java.nio.charset.StandardCharsets;
 
 public class SendMessageBatchV2ProtocolResolver {
     public static CloudEvent buildEvent(Header header, Body body) throws ProtocolHandleException {
@@ -39,9 +59,13 @@ public class SendMessageBatchV2ProtocolResolver {
             String content = sendMessageBatchV2RequestBody.getMsg();
 
             CloudEvent event = null;
+            CloudEventBuilder cloudEventBuilder;
             if (StringUtils.equals(SpecVersion.V1.toString(), protocolVersion)) {
-                event = JsonUtils.deserialize(content, CloudEventV1.class);
-                event = CloudEventBuilder.from(event)
+                cloudEventBuilder = CloudEventBuilder.v1();
+
+                event = cloudEventBuilder.withId(sendMessageBatchV2RequestBody.getBizSeqNo())
+                        .withSubject(sendMessageBatchV2RequestBody.getTopic())
+                        .withData(content.getBytes(StandardCharsets.UTF_8))
                         .withExtension(ProtocolKey.REQUEST_CODE, code)
                         .withExtension(ProtocolKey.ClientInstanceKey.ENV, env)
                         .withExtension(ProtocolKey.ClientInstanceKey.IDC, idc)
@@ -55,10 +79,16 @@ public class SendMessageBatchV2ProtocolResolver {
                         .withExtension(ProtocolKey.PROTOCOL_TYPE, protocolType)
                         .withExtension(ProtocolKey.PROTOCOL_DESC, protocolDesc)
                         .withExtension(ProtocolKey.PROTOCOL_VERSION, protocolVersion)
+                        .withExtension(SendMessageBatchV2RequestBody.BIZSEQNO, sendMessageBatchV2RequestBody.getBizSeqNo())
+                        .withExtension(SendMessageBatchV2RequestBody.PRODUCERGROUP, sendMessageBatchV2RequestBody.getProducerGroup())
+                        .withExtension(SendMessageBatchV2RequestBody.TTL, sendMessageBatchV2RequestBody.getTtl())
+                        .withExtension(SendMessageBatchV2RequestBody.TAG, sendMessageBatchV2RequestBody.getTag())
                         .build();
             } else if (StringUtils.equals(SpecVersion.V03.toString(), protocolVersion)) {
-                event = JsonUtils.deserialize(content, CloudEventV03.class);
-                event = CloudEventBuilder.from(event)
+                cloudEventBuilder = CloudEventBuilder.v03();
+                event = cloudEventBuilder.withId(sendMessageBatchV2RequestBody.getBizSeqNo())
+                        .withSubject(sendMessageBatchV2RequestBody.getTopic())
+                        .withData(content.getBytes(StandardCharsets.UTF_8))
                         .withExtension(ProtocolKey.REQUEST_CODE, code)
                         .withExtension(ProtocolKey.ClientInstanceKey.ENV, env)
                         .withExtension(ProtocolKey.ClientInstanceKey.IDC, idc)
@@ -72,6 +102,10 @@ public class SendMessageBatchV2ProtocolResolver {
                         .withExtension(ProtocolKey.PROTOCOL_TYPE, protocolType)
                         .withExtension(ProtocolKey.PROTOCOL_DESC, protocolDesc)
                         .withExtension(ProtocolKey.PROTOCOL_VERSION, protocolVersion)
+                        .withExtension(SendMessageBatchV2RequestBody.BIZSEQNO, sendMessageBatchV2RequestBody.getBizSeqNo())
+                        .withExtension(SendMessageBatchV2RequestBody.PRODUCERGROUP, sendMessageBatchV2RequestBody.getProducerGroup())
+                        .withExtension(SendMessageBatchV2RequestBody.TTL, sendMessageBatchV2RequestBody.getTtl())
+                        .withExtension(SendMessageBatchV2RequestBody.TAG, sendMessageBatchV2RequestBody.getTag())
                         .build();
             }
             return event;
