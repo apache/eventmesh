c1bbccb234677b89663278365ee300ef3a180bd4
mike_xwm <mike_xwm@126.com>
2021-11-23 18:29:30 +0800

    update java sdk (#607)

--- eventmesh-sdk-java/src/main/java/org/apache/eventmesh/client/tcp/impl/cloudevent/CloudEventTCPPubClient.java
+++ eventmesh-sdk-java/src/main/java/org/apache/eventmesh/client/tcp/impl/cloudevent/CloudEventTCPPubClient.java
@@ -1,54 +1,132 @@
 package org.apache.eventmesh.client.tcp.impl.cloudevent;
 
+
+
 import org.apache.eventmesh.client.tcp.EventMeshTCPPubClient;
-import org.apache.eventmesh.client.tcp.common.AsyncRRCallback;
-import org.apache.eventmesh.client.tcp.common.ReceiveMsgHook;
+import org.apache.eventmesh.client.tcp.common.*;
+import org.apache.eventmesh.client.tcp.conf.EventMeshTcpClientConfig;
+import org.apache.eventmesh.client.tcp.impl.eventmeshmessage.EventMeshMessageTCPPubClient;
+import org.apache.eventmesh.common.EventMeshMessage;
 import org.apache.eventmesh.common.exception.EventMeshException;
+import org.apache.eventmesh.common.protocol.tcp.Command;
 import org.apache.eventmesh.common.protocol.tcp.Package;
 import org.apache.eventmesh.common.protocol.tcp.UserAgent;
 
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.cloudevents.CloudEvent;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A CloudEvent TCP publish client implementation.
  */
 @Slf4j
-public class CloudEventTCPPubClient implements EventMeshTCPPubClient<CloudEvent> {
+public class CloudEventTCPPubClient extends TcpClient implements EventMeshTCPPubClient<CloudEvent> {
+
+    private final UserAgent userAgent;
+
+    private ReceiveMsgHook<EventMeshMessage> callback;
+
+    private final ConcurrentHashMap<String, AsyncRRCallback> callbackConcurrentHashMap = new ConcurrentHashMap<>();
+    private ScheduledFuture<?> task;
+
+    public CloudEventTCPPubClient(EventMeshTcpClientConfig eventMeshTcpClientConfig) {
+        super(eventMeshTcpClientConfig);
+        this.userAgent = eventMeshTcpClientConfig.getUserAgent();
+    }
 
     @Override
     public void init() throws EventMeshException {
-
+        try {
+            open(new Handler());
+            hello();
+        } catch (Exception ex) {
+            throw new EventMeshException("Initialize EventMeshMessageTCPPubClient error", ex);
+        }
     }
 
     @Override
     public void heartbeat() throws EventMeshException {
-
+        if (task != null) {
+            synchronized (EventMeshMessageTCPPubClient.class) {
+                task = scheduler.scheduleAtFixedRate(() -> {
+                    try {
+                        if (!isActive()) {
+                            reconnect();
+                        }
+                        Package msg = MessageUtils.heartBeat();
+                        io(msg, EventMeshCommon.DEFAULT_TIME_OUT_MILLS);
+                    } catch (Exception ignore) {
+                        // ignore
+                    }
+                }, EventMeshCommon.HEARTBEAT, EventMeshCommon.HEARTBEAT, TimeUnit.MILLISECONDS);
+            }
+        }
     }
 
     @Override
     public void reconnect() throws EventMeshException {
-
+        try {
+            super.reconnect();
+            hello();
+        } catch (Exception ex) {
+            throw new EventMeshException("reconnect error", ex);
+        }
     }
 
     @Override
-    public Package rr(CloudEvent msg, long timeout) throws EventMeshException {
-        return null;
+    public Package rr(CloudEvent event, long timeout) throws EventMeshException {
+        try {
+            Package msg = MessageUtils.buildPackage(event, Command.REQUEST_TO_SERVER);
+            log.info("{}|rr|send|type={}|msg={}", clientNo, msg, msg);
+            return io(msg, timeout);
+        } catch (Exception ex) {
+            throw new EventMeshException("rr error");
+        }
     }
 
     @Override
-    public void asyncRR(CloudEvent msg, AsyncRRCallback callback, long timeout) throws EventMeshException {
-
+    public void asyncRR(CloudEvent event, AsyncRRCallback callback, long timeout) throws EventMeshException {
+        try {
+            Package msg = MessageUtils.buildPackage(event, Command.REQUEST_TO_SERVER);
+            super.send(msg);
+            this.callbackConcurrentHashMap.put((String) RequestContext._key(msg), callback);
+        } catch (Exception ex) {
+            // should trigger callback?
+            throw new EventMeshException("asyncRR error", ex);
+        }
     }
 
     @Override
     public Package publish(CloudEvent cloudEvent, long timeout) throws EventMeshException {
-        return null;
+        try {
+            // todo: transform EventMeshMessage to Package
+            Package msg = MessageUtils.buildPackage(cloudEvent, Command.ASYNC_MESSAGE_TO_SERVER);
+            log.info("SimplePubClientImpl cloud event|{}|publish|send|type={}|protocol={}|msg={}",
+                clientNo, msg.getHeader().getCommand(),
+                msg.getHeader().getProperty(PropertyConst.PROPERTY_MESSAGE_PROTOCOL), msg);
+            return io(msg, timeout);
+        } catch (Exception ex) {
+            throw new EventMeshException("publish error", ex);
+        }
     }
 
     @Override
     public void broadcast(CloudEvent cloudEvent, long timeout) throws EventMeshException {
-
+        try {
+            // todo: transform EventMeshMessage to Package
+            Package msg = MessageUtils.buildPackage(cloudEvent, Command.BROADCAST_MESSAGE_TO_SERVER);
+            log.info("{}|publish|send|type={}|protocol={}|msg={}", clientNo, msg.getHeader().getCommand(),
+                msg.getHeader().getProperty(PropertyConst.PROPERTY_MESSAGE_PROTOCOL), msg);
+            super.send(msg);
+        } catch (Exception ex) {
+            throw new EventMeshException("Broadcast message error", ex);
+        }
     }
 
     @Override
@@ -57,7 +135,45 @@ public class CloudEventTCPPubClient implements EventMeshTCPPubClient<CloudEvent>
     }
 
     @Override
-    public void close() throws EventMeshException {
+    public void close() {
 
+    }
+
+    // todo: move to abstract class
+    @ChannelHandler.Sharable
+    private class Handler extends SimpleChannelInboundHandler<Package> {
+        @Override
+        protected void channelRead0(ChannelHandlerContext ctx, Package msg) throws Exception {
+            log.info("SimplePubClientImpl|{}|receive|type={}|msg={}", clientNo, msg.getHeader(), msg);
+
+            Command cmd = msg.getHeader().getCommand();
+            if (cmd == Command.RESPONSE_TO_CLIENT) {
+                if (callback != null) {
+                    callback.handle(msg, ctx);
+                }
+                Package pkg = MessageUtils.responseToClientAck(msg);
+                send(pkg);
+            } else if (cmd == Command.SERVER_GOODBYE_REQUEST) {
+                //TODO
+            }
+
+            RequestContext context = contexts.get(RequestContext._key(msg));
+            if (context != null) {
+                contexts.remove(context.getKey());
+                context.finish(msg);
+            }
+        }
+    }
+
+    // todo: remove hello
+    private void hello() throws Exception {
+        Package msg = MessageUtils.hello(userAgent);
+        this.io(msg, EventMeshCommon.DEFAULT_TIME_OUT_MILLS);
+    }
+
+    // todo: remove goodbye
+    private void goodbye() throws Exception {
+        Package msg = MessageUtils.goodbye();
+        this.io(msg, EventMeshCommon.DEFAULT_TIME_OUT_MILLS);
     }
 }
