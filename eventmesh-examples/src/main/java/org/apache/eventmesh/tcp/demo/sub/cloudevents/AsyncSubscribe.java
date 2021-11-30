d1059ed2bdd757bad0ed3e31881e714526fb89d4
mike_xwm <mike_xwm@126.com>
2021-11-30 19:29:58 +0800

    Java sdk update cloudevents pub/sub (#620)
    1.cloudevents protocol tcp pub/sub for sdk

---
+++ eventmesh-examples/src/main/java/org/apache/eventmesh/tcp/demo/sub/cloudevents/AsyncSubscribe.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.eventmesh.tcp.demo.sub.cloudevents;
+
+import org.apache.eventmesh.client.tcp.EventMeshTCPClient;
+import org.apache.eventmesh.client.tcp.common.ReceiveMsgHook;
+import org.apache.eventmesh.client.tcp.conf.EventMeshTCPClientConfig;
+import org.apache.eventmesh.client.tcp.impl.EventMeshTCPClientFactory;
+import org.apache.eventmesh.common.protocol.SubscriptionMode;
+import org.apache.eventmesh.common.protocol.SubscriptionType;
+import org.apache.eventmesh.common.protocol.tcp.EventMeshMessage;
+import org.apache.eventmesh.common.protocol.tcp.Package;
+import org.apache.eventmesh.common.protocol.tcp.UserAgent;
+import org.apache.eventmesh.tcp.common.EventMeshTestUtils;
+import org.apache.eventmesh.util.Utils;
+
+import java.util.Properties;
+
+import io.cloudevents.CloudEvent;
+import io.cloudevents.SpecVersion;
+import io.cloudevents.core.builder.CloudEventBuilder;
+import io.netty.channel.ChannelHandlerContext;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AsyncSubscribe implements ReceiveMsgHook<CloudEvent> {
+
+    public static AsyncSubscribe handler = new AsyncSubscribe();
+
+    private static EventMeshTCPClient client;
+
+    public static void main(String[] agrs) throws Exception {
+        Properties properties = Utils.readPropertiesFile("application.properties");
+        final String eventMeshIp = properties.getProperty("eventmesh.ip");
+        final int eventMeshTcpPort = Integer.parseInt(properties.getProperty("eventmesh.tcp.port"));
+        UserAgent userAgent = EventMeshTestUtils.generateClient2();
+        EventMeshTCPClientConfig eventMeshTcpClientConfig = EventMeshTCPClientConfig.builder()
+            .host(eventMeshIp)
+            .port(eventMeshTcpPort)
+            .userAgent(userAgent)
+            .build();
+        try {
+            client = EventMeshTCPClientFactory.createEventMeshTCPClient(eventMeshTcpClientConfig, CloudEvent.class);
+            client.init();
+            client.heartbeat();
+
+            client.subscribe("TEST-TOPIC-TCP-ASYNC", SubscriptionMode.CLUSTERING, SubscriptionType.ASYNC);
+            client.registerSubBusiHandler(handler);
+
+            client.listen();
+
+            //client.unsubscribe();
+
+            // release resource and close client
+            // client.close();
+
+        } catch (Exception e) {
+            log.warn("AsyncSubscribe failed", e);
+        }
+    }
+
+    @Override
+    public void handle(Package msg, ChannelHandlerContext ctx) {
+        CloudEvent event = convertToProtocolMessage(msg);
+        log.info("receive async msg====================={}", event);
+    }
+
+    @Override
+    public CloudEvent convertToProtocolMessage(Package pkg) {
+        return CloudEventBuilder.from((CloudEvent) pkg.getBody()).build();
+    }
+}
