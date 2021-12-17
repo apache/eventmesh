2c97cd9d9803064b0ab3e25dd7e46b3a005483dc
jinrongluo <kapoking@gmail.com>
2021-12-17 11:39:25 -0500

    Adding grpc publish and subscribe API examples

--- eventmesh-sdk-java/src/main/java/org/apache/eventmesh/client/grpc/EventMeshGrpcConsumer.java
+++ eventmesh-sdk-java/src/main/java/org/apache/eventmesh/client/grpc/EventMeshGrpcConsumer.java
@@ -3,7 +3,9 @@ package org.apache.eventmesh.client.grpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.apache.eventmesh.client.grpc.config.EventMeshGrpcClientConfig;
+import org.apache.eventmesh.client.grpc.util.EventMeshClientUtil;
 import org.apache.eventmesh.common.protocol.grpc.protos.ConsumerServiceGrpc;
+import org.apache.eventmesh.common.protocol.grpc.protos.Response;
 import org.apache.eventmesh.common.protocol.grpc.protos.Subscription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,10 +27,17 @@ public class EventMeshGrpcConsumer implements AutoCloseable {
             .usePlaintext().build();
     }
 
-    public boolean subscribe(Subscription subscription) {
+    public Response subscribe(Subscription subscription) {
+        logger.info("Subscribe topic " + subscription.toString());
         ConsumerServiceGrpc.ConsumerServiceBlockingStub consumerClient = ConsumerServiceGrpc.newBlockingStub(channel);
-        consumerClient.subscribe(subscription);
-        return true;
+
+        Subscription enhancedSubscription = Subscription.newBuilder(subscription)
+            .setHeader(EventMeshClientUtil.buildHeader(clientConfig))
+            .setConsumerGroup(clientConfig.getConsumerGroup())
+            .build();
+        Response response = consumerClient.subscribe(enhancedSubscription);
+        logger.info("Received response " + response.toString());
+        return response;
     }
 
     @Override
