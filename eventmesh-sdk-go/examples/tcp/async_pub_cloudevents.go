8732d4e381f932a0d0f7f06a849220983d3b2284
walleliu1016 <liulefirst@163.com>
2022-04-03 15:49:09 +0800

    1. change the module to github.com/apache/incubator-eventmesh/eventmesh-sdk-go 2. add licence for all go files 3. and add grpc proto

--- eventmesh-sdk-go/examples/tcp/async_pub_cloudevents.go
+++ eventmesh-sdk-go/examples/tcp/async_pub_cloudevents.go
@@ -1,18 +1,33 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package tcp
 
 import (
-	"eventmesh/common"
-	"eventmesh/common/protocol"
-	gtcp "eventmesh/common/protocol/tcp"
-	"eventmesh/common/utils"
-	"eventmesh/tcp"
 	"time"
 
-	//"eventmesh/tcp/common"
-	"eventmesh/tcp/conf"
-	cloudevents "github.com/cloudevents/sdk-go/v2"
 	"github.com/google/uuid"
 	"strconv"
+
+	"github.com/apache/incubator-eventmesh/eventmesh-sdk-go/common"
+	"github.com/apache/incubator-eventmesh/eventmesh-sdk-go/common/protocol"
+	gtcp "github.com/apache/incubator-eventmesh/eventmesh-sdk-go/common/protocol/tcp"
+	"github.com/apache/incubator-eventmesh/eventmesh-sdk-go/common/utils"
+	"github.com/apache/incubator-eventmesh/eventmesh-sdk-go/tcp"
+	"github.com/apache/incubator-eventmesh/eventmesh-sdk-go/tcp/conf"
+	cloudevents "github.com/cloudevents/sdk-go/v2"
 )
 
 func AsyncPubCloudEvents() {
