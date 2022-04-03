4b039f031167e2e50ee7144e16ad1ec8e6e123a5
walleliu1016 <liulefirst@163.com>
2022-04-03 15:49:09 +0800

    1. change the module to github.com/apache/incubator-eventmesh/eventmesh-sdk-go 2. add licence for all go files 3. and add grpc proto

--- eventmesh-sdk-go/http/consumer/eventmesh_http_consumer.go
+++ eventmesh-sdk-go/http/consumer/eventmesh_http_consumer.go
@@ -1,15 +1,30 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package consumer
 
 import (
-	gcommon "eventmesh/common"
-	"eventmesh/common/protocol"
-	"eventmesh/common/protocol/http/body/client"
-	"eventmesh/common/protocol/http/common"
-	gutils "eventmesh/common/utils"
-	"eventmesh/http"
-	"eventmesh/http/conf"
-	"eventmesh/http/model"
-	"eventmesh/http/utils"
+	gcommon "github.com/apache/incubator-eventmesh/eventmesh-sdk-go/common"
+	"github.com/apache/incubator-eventmesh/eventmesh-sdk-go/common/protocol"
+	"github.com/apache/incubator-eventmesh/eventmesh-sdk-go/common/protocol/http/body/client"
+	"github.com/apache/incubator-eventmesh/eventmesh-sdk-go/common/protocol/http/common"
+	gutils "github.com/apache/incubator-eventmesh/eventmesh-sdk-go/common/utils"
+	"github.com/apache/incubator-eventmesh/eventmesh-sdk-go/http"
+	"github.com/apache/incubator-eventmesh/eventmesh-sdk-go/http/conf"
+	"github.com/apache/incubator-eventmesh/eventmesh-sdk-go/http/model"
+	"github.com/apache/incubator-eventmesh/eventmesh-sdk-go/http/utils"
 	"log"
 	nethttp "net/http"
 	"strconv"
