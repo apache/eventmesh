ce6a2944879dea42c187d4b86b2873feab6fae9b
Mark Li <limarksmail@gmail.com>
2022-05-10 19:26:57 -0700

    [ISSUE #843] Some enhancement to the eventmesh-sdk module (#853)
    * Some optimizations to the eventmesh-sdk module
    
    - Removed PropertyConst.java, which is not used in the project
    - Extracted a constant class for protocol type and protocol description
    
    * Style fix on ProtocolConstant
    
    close #843

--- eventmesh-sdk-java/src/main/java/org/apache/eventmesh/client/http/ProtocolConstant.java
+++ eventmesh-sdk-java/src/main/java/org/apache/eventmesh/client/http/ProtocolConstant.java
@@ -15,14 +15,11 @@
  * limitations under the License.
  */
 
-package org.apache.eventmesh.client.tcp.common;
+package org.apache.eventmesh.client.http;
 
-/**
- * properties key name
- */
-public class PropertyConst {
-
-    public static String PROPERTY_MESSAGE_PROTOCOL = "message_protocol";
-
-    public static String PROPERTY_CLOUD_EVENT_VERSION = "cloud_event_version";
+public final class ProtocolConstant {
+    public static final String CE_PROTOCOL = "cloudevents";
+    public static final String EM_MESSAGE_PROTOCOL = "eventmeshmessage";
+    public static final String OP_MESSAGE_PROTOCOL = "openmessage";
+    public static final String PROTOCOL_DESC = "http";
 }
