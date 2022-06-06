8baf741a528f37bdf984ef22ad5c6c3013e89ec2
walleliu1016 <liulefirst@163.com>
2022-06-06 18:09:12 +0800

    update example

--- eventmesh-sdk-go/examples/grpc/consumer/rr/main.go
+++ eventmesh-sdk-go/examples/grpc/consumer/rr/main.go
@@ -24,7 +24,7 @@ import (
 
 func main() {
 	cli, err := grpc.New(&conf.GRPCConfig{
-		Host:         "101.43.84.47",
+		Host:         "127.0.0.1",
 		Port:         10205,
 		ENV:          "go-grpc-test-env",
 		Region:       "sh",
