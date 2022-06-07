f1f0e4abf6530d6c4c7d52cfeacae8d679e98177
walleliu1016 <liulefirst@163.com>
2022-06-07 10:38:01 +0800

    update em host in example

--- eventmesh-sdk-go/grpc/conf/config_test.go
+++ eventmesh-sdk-go/grpc/conf/config_test.go
@@ -1,3 +1,18 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package conf
 
 import "testing"
@@ -22,7 +37,7 @@ func TestValidateDefaultConf(t *testing.T) {
 			name: "test default duration",
 			args: args{
 				cfg: &GRPCConfig{
-					Hosts: []string{"1", "2"},
+					Host: "1",
 				},
 			},
 			wantErr: false,
@@ -31,7 +46,7 @@ func TestValidateDefaultConf(t *testing.T) {
 			name: "test consumer enable, but no group",
 			args: args{
 				cfg: &GRPCConfig{
-					Hosts: []string{"1"},
+					Host: "1",
 					ConsumerConfig: ConsumerConfig{
 						Enabled: true,
 					},
@@ -43,7 +58,7 @@ func TestValidateDefaultConf(t *testing.T) {
 			name: "test consumer enable, but no group",
 			args: args{
 				cfg: &GRPCConfig{
-					Hosts: []string{"1"},
+					Host: "1",
 					ConsumerConfig: ConsumerConfig{
 						Enabled:       true,
 						ConsumerGroup: "test",
