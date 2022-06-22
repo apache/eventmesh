f9ccece7d632efad4c22c235b46abd417ab7d51e
lrhkobe <liangrh7@163.com>
2022-06-22 17:41:27 +0800

    optimize trace module in eventmesh

--- eventmesh-trace-plugin/eventmesh-trace-api/src/main/java/org/apache/eventmesh/trace/api/EventMeshSpan.java
+++ eventmesh-trace-plugin/eventmesh-trace-api/src/main/java/org/apache/eventmesh/trace/api/EventMeshSpan.java
@@ -17,27 +17,19 @@
 
 package org.apache.eventmesh.trace.api;
 
-import static com.google.common.base.Preconditions.checkNotNull;
+import org.apache.eventmesh.trace.api.common.ProtocolType;
 
-import org.apache.eventmesh.spi.EventMeshExtensionFactory;
-
-import lombok.experimental.UtilityClass;
-
-/**
- * to get the trace service
- */
-@UtilityClass
-public class TracePluginFactory {
-    /**
-     * to get TraceService
-     *
-     * @param traceServiceType
-     * @return
-     */
-    public static EventMeshTraceService getEventMeshTraceService(String traceServiceType) {
-        checkNotNull(traceServiceType, "traceServiceType cannot be null");
-
-        EventMeshTraceService eventMeshTraceService = EventMeshExtensionFactory.getExtension(EventMeshTraceService.class, traceServiceType);
-        return checkNotNull(eventMeshTraceService, "traceServiceType: " + traceServiceType + " is not supported");
-    }
+public interface EventMeshSpan {
+    void start();
+    void start(long timestamp);
+    void finish();
+    void finish(long timestamp);
+    void setOperationName(String operationName);
+    void setRemoteAddress(String remoteAddress);
+    void setProtocolType(ProtocolType type);
+    void addTag(String key, String value);
+    void addError(Throwable throwable);
+    void setAsync();
+    void setComponent(String componentName);
+    boolean isAsync();
 }
