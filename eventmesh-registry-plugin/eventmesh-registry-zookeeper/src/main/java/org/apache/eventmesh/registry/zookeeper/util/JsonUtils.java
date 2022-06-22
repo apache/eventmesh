3dcee5c5a86359a0f5894a2794c088fdef4ef459
鹿鸣 <tangmengyang@cai-inc.com>
2022-06-22 20:37:24 +0800

    Doc tag problem fix.

--- eventmesh-registry-plugin/eventmesh-registry-zookeeper/src/main/java/org/apache/eventmesh/registry/zookeeper/util/JsonUtils.java
+++ eventmesh-registry-plugin/eventmesh-registry-zookeeper/src/main/java/org/apache/eventmesh/registry/zookeeper/util/JsonUtils.java
@@ -3,21 +3,14 @@ package org.apache.eventmesh.registry.zookeeper.util;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-/**
- * @Author: moxing
- * @Date: 2022/6/17 13:57
- * @Description:
- */
 public class JsonUtils {
 
     public static String toJSON(Object o) {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            String string = objectMapper.writeValueAsString(o);
-            return string;
+            return objectMapper.writeValueAsString(o);
         } catch (JsonProcessingException e) {
             return null;
         }
     }
-
 }
