891c176a1d4a6f6e9b76bf8b5b4bb2a36ddcc6e1
= <=>
2022-06-25 16:47:08 +0800

    add runtime unit tests

--- eventmesh-runtime/src/test/java/org/apache/eventmesh/runtime/util/HttpTinyClientTest.java
+++ eventmesh-runtime/src/test/java/org/apache/eventmesh/runtime/util/HttpTinyClientTest.java
@@ -17,15 +17,61 @@
 
 package org.apache.eventmesh.runtime.util;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.apache.eventmesh.runtime.util.HttpTinyClient.HttpResult;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 public class HttpTinyClientTest {
 
     @Test
-    public void testHttpGet() {
+    public void testHttpGet() throws IOException {
+        String content = "http mock response";
+        HttpURLConnection conn = mock(HttpURLConnection.class);
+        URL url = mock(URL.class);
+        doNothing().when(conn).connect();
+        when(url.openConnection()).thenReturn(conn);
+        when(conn.getResponseCode()).thenReturn(HttpURLConnection.HTTP_OK);
+        try (MockedStatic<IOTinyUtils> dummyStatic = Mockito.mockStatic(IOTinyUtils.class)) {
+            dummyStatic.when(() -> IOTinyUtils.toString(any(), any())).thenReturn(content);
+            String requestUrl = "https://eventmesh.apache.org";
+            HttpResult result = HttpTinyClient.httpGet(requestUrl, null, null, "utf-8", 0);
+            Assert.assertEquals(result.getContent(), content);
+            Assert.assertEquals(result.getCode(), HttpURLConnection.HTTP_OK);
+        }
     }
 
     @Test
-    public void testHttpPost() {
+    public void testHttpPost() throws IOException {
+        String content = "http mock response";
+        HttpURLConnection conn = mock(HttpURLConnection.class);
+        URL url = mock(URL.class);
+        doNothing().when(conn).connect();
+        when(url.openConnection()).thenReturn(conn);
+        when(conn.getResponseCode()).thenReturn(HttpURLConnection.HTTP_OK);
+
+        OutputStream outputStream = mock(OutputStream.class);
+        doNothing().when(outputStream).write(new byte[0]);
+        when(conn.getOutputStream()).thenReturn(outputStream);
+        try (MockedStatic<IOTinyUtils> dummyStatic = Mockito.mockStatic(IOTinyUtils.class)) {
+            dummyStatic.when(() -> IOTinyUtils.toString(any(), any())).thenReturn(content);
+            String requestUrl = "https://eventmesh.apache.org";
+            HttpResult result = HttpTinyClient.httpPost(requestUrl, anyList(), anyList(), "utf-8", 0);
+            Assert.assertEquals(result.getContent(), content);
+            Assert.assertEquals(result.getCode(), HttpURLConnection.HTTP_OK);
+        }
     }
 }
