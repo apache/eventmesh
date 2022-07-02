fe728d58e7917b4f2443c6e8e76674eeb51f269c
tangjs <641509923@qq.com>
2022-07-02 16:03:18 +0800

    Set up redis project

---
+++ eventmesh-connector-plugin/eventmesh-connector-redis/src/test/java/org/appache/eventmesh/connector/redis/EventmeshConnectorRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.appache.eventmesh.connector.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventmeshConnectorRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
