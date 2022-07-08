6ee394c26cff2932a9ad1606d0011afe9df82949
Mark Li <limarksmail@gmail.com>
2022-07-08 02:22:10 -0700

    Gradle and Module settings

---
+++ eventmesh-connector-plugin/eventmesh-connector-kafka/src/main/java/org/apache/eventmesh/connector/kafka/cloudevent/KafkaMessageFactory.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.eventmesh.connector.rocketmq.cloudevent;
+
+import java.util.Map;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import io.cloudevents.core.message.MessageReader;
+import io.cloudevents.core.message.MessageWriter;
+import io.cloudevents.core.message.impl.MessageUtils;
+import io.cloudevents.lang.Nullable;
+import io.cloudevents.rw.CloudEventRWException;
+import io.cloudevents.rw.CloudEventWriter;
+
+
+@ParametersAreNonnullByDefault
+public final class KafkaMessageFactory {
+
+    private KafkaMessageFactory() {
+        // prevent instantiation
+    }
+
+    public static MessageReader createReader(final Message message) throws CloudEventRWException {
+        return createReader(message.getProperties(), message.getBody());
+    }
+
+
+    public static MessageReader createReader(final Map<String, String> props,
+                                             @Nullable final byte[] body)
+            throws CloudEventRWException {
+
+        return MessageUtils.parseStructuredOrBinaryMessage(
+                () -> null,
+                format -> null,
+                () -> props.get(RocketMQHeaders.SPEC_VERSION),
+                sv -> new RocketMQBinaryMessageReader(sv, props, body)
+        );
+    }
+
+
+    public static MessageWriter<CloudEventWriter<Message>, Message> createWriter(String topic) {
+        return new RocketMQMessageWriter<>(topic);
+    }
+
+    public static MessageWriter<CloudEventWriter<Message>, Message> createWriter(String topic,
+                                                                                 String keys) {
+        return new RocketMQMessageWriter<>(topic, keys);
+    }
+
+    public static MessageWriter<CloudEventWriter<Message>, Message> createWriter(String topic,
+                                                                                 String keys,
+                                                                                 String tags) {
+        return new RocketMQMessageWriter<>(topic, keys, tags);
+    }
+
+}
