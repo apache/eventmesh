a560e612207c6f37c1cf5f592237bb121a32f40d
鹿鸣 <tangmengyang@cai-inc.com>
2022-07-13 10:29:45 +0800

    Review suggestion: remove the unified configuration function.

--- eventmesh-common/src/main/java/org/apache/eventmesh/common/utils/ConfigurationContextUtil.java
+++ eventmesh-common/src/main/java/org/apache/eventmesh/common/utils/ConfigurationContextUtil.java
@@ -18,14 +18,9 @@
 package org.apache.eventmesh.common.utils;
 
 import org.apache.eventmesh.common.config.CommonConfiguration;
-import org.apache.eventmesh.common.config.ConfigurationWrapper;
-
-import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
-import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
 
 import com.google.common.collect.Lists;
@@ -35,9 +30,7 @@ import com.google.common.collect.Lists;
  */
 public class ConfigurationContextUtil {
 
-    private static final Map<String, CommonConfiguration> CONFIGURATION_MAP = new ConcurrentHashMap<>();
-
-    private static ConfigurationWrapper baseConfigurationWrapper;
+    private static final ConcurrentHashMap<String, CommonConfiguration> CONFIGURATION_MAP = new ConcurrentHashMap<>();
 
     public static final String HTTP = "HTTP";
 
@@ -46,42 +39,12 @@ public class ConfigurationContextUtil {
 
     public static final List<String> KEYS = Lists.newArrayList(HTTP, TCP, GRPC);
 
-    /**
-     * Set global configurationWrapper.
-     */
-    public static void setBaseConfigurationWrapper(ConfigurationWrapper configurationWrapper) {
-        baseConfigurationWrapper = configurationWrapper;
-    }
-
-    /**
-     * Get property from the global configurationWrapper.
-     */
-    public static String getProp(String key) {
-        return baseConfigurationWrapper != null ? baseConfigurationWrapper.getProp(key) : null;
-    }
-
-    /**
-     * Get properties by prefix from the global configurationWrapper.
-     */
-    public static Properties getPropertiesByPrefix(String prefix) {
-        if (StringUtils.isBlank(prefix) || baseConfigurationWrapper == null) {
-            return null;
-        }
-        return baseConfigurationWrapper.getPropertiesByPrefix(prefix);
-    }
-
-    /**
-     * Get properties by prefix from the global configurationWrapper.
-     */
-    public static Properties getPropertiesByPrefix(String prefix, Properties to) {
-        if (StringUtils.isBlank(prefix) || baseConfigurationWrapper == null) {
-            return to;
-        }
-        return baseConfigurationWrapper.getPropertiesByPrefix(prefix, to);
-    }
 
     /**
      * Save http, tcp, grpc configuration at startup for global use.
+     *
+     * @param key
+     * @param configuration
      */
     public static void putIfAbsent(String key, CommonConfiguration configuration) {
         if (Objects.isNull(configuration)) {
@@ -92,13 +55,17 @@ public class ConfigurationContextUtil {
 
     /**
      * Get the configuration of the specified key mapping.
+     *
+     * @param key
+     * @return
      */
     public static CommonConfiguration get(String key) {
         return CONFIGURATION_MAP.get(key);
     }
 
+
     /**
-     * Removes all the mappings from this map.
+     * Removes all of the mappings from this map.
      */
     public static void clear() {
         CONFIGURATION_MAP.clear();
