8492c706ead50059ba871b93a2c450e31f5799a4
xwm1992 <mike_xwm@126.com>
2022-07-22 23:44:18 +0800

    update HandlerService

--- eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/boot/HTTPTrace.java
+++ eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/boot/HTTPTrace.java
@@ -17,10 +17,7 @@
 
 package org.apache.eventmesh.runtime.boot;
 
-import org.apache.eventmesh.runtime.trace.SpanKey;
 import org.apache.eventmesh.runtime.trace.TraceUtils;
-import org.apache.eventmesh.trace.api.TracePluginFactory;
-import org.apache.eventmesh.trace.api.TraceService;
 import org.apache.eventmesh.trace.api.common.EventMeshTraceConstants;
 
 import org.apache.commons.lang3.StringUtils;
@@ -31,16 +28,13 @@ import io.netty.channel.Channel;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpRequest;
 import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.api.trace.SpanKind;
-import io.opentelemetry.api.trace.StatusCode;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
-import io.opentelemetry.context.propagation.TextMapGetter;
 import io.opentelemetry.context.propagation.TextMapPropagator;
-import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.Setter;
 
 import java.util.HashMap;
@@ -52,61 +46,18 @@ public class HTTPTrace {
 
     private Tracer tracer;
 
-//    private boolean useTrace = false;
-
     @Setter
     private String serviceAddress;
 
     @Setter
     private String servicePort;
 
-//    public void initTrace(Tracer tracer, TextMapPropagator textMapPropagator, boolean useTrace) {
-//        this.tracer = tracer;
-//        this.textMapPropagator = textMapPropagator;
-//        this.useTrace = useTrace;
-//    }
-//
-//    public void initTrace(String traceType, boolean traceEnable, Class<?> clazz) {
-//        if (StringUtils.isNotEmpty(traceType) && traceEnable) {
-//            TraceService traceService = TracePluginFactory.getTraceService(traceType);
-//            traceService.init();
-//            tracer = traceService.getTracer(clazz.toString());
-//            textMapPropagator = traceService.getTextMapPropagator();
-//            useTrace = true;
-//        }
-//    }
-
     public TraceOperation getTraceOperation(HttpRequest httpRequest, Channel channel) {
-        if (!useTrace) {
-            return new TraceOperation();
-        }
 
         final Map<String, Object> headerMap = parseHttpHeader(httpRequest);
         Span span = TraceUtils.prepareServerSpan(headerMap, EventMeshTraceConstants.TRACE_UPSTREAM_EVENTMESH_SERVER_SPAN,
             false);
-
-        Context context = textMapPropagator.extract(Context.current(), httpRequest, new TextMapGetter<HttpRequest>() {
-            @Override
-            public Iterable<String> keys(HttpRequest carrier) {
-                return carrier.headers().names();
-            }
-
-            @Override
-            public String get(HttpRequest carrier, String key) {
-                return carrier.headers().get(key);
-            }
-        });
-
-        Span span = tracer.spanBuilder("HTTP " + httpRequest.uri() + " " + httpRequest.method().toString())
-                .setParent(context)
-                .setSpanKind(SpanKind.SERVER)
-                .startSpan();
-        context = context.with(SpanKey.SERVER_KEY, span);
-
-        span.setAttribute(SemanticAttributes.HTTP_METHOD, httpRequest.method().name());
-        span.setAttribute(SemanticAttributes.HTTP_FLAVOR, httpRequest.protocolVersion().protocolName());
-        span.setAttribute(String.valueOf(SemanticAttributes.HTTP_URL), httpRequest.uri());
-        return new TraceOperation(context, span, useTrace);
+        return new TraceOperation(span);
     }
 
     private Map<String, Object> parseHttpHeader(HttpRequest fullReq) {
@@ -123,31 +74,21 @@ public class HTTPTrace {
     }
 
     @AllArgsConstructor
+    @Getter
     public static class TraceOperation {
 
-        private final Context context;
         private final Span span;
 
         public void endTrace() {
-            if (!useTrace) {
-                return;
-            }
 
-            try (Scope ignored = context.makeCurrent()) {
-                span.end();
+            try (Scope ignored = span.makeCurrent()) {
+                TraceUtils.finishSpan(span, null);
             }
         }
 
         public void exceptionTrace(@Nullable Throwable ex) {
-            if (!useTrace) {
-                return;
-            }
-            try (Scope ignored = context.makeCurrent()) {
-                ex = ex != null ? ex : new RuntimeException("exception for trace");
-                span.setAttribute(SemanticAttributes.EXCEPTION_MESSAGE, ex.getMessage());
-                span.setStatus(StatusCode.ERROR, ex.getMessage());
-                span.recordException(ex);
-                span.end();
+            try (Scope ignored = span.makeCurrent()) {
+                TraceUtils.finishSpanWithException(span, null, ex.getMessage(), ex);
             }
         }
     }
