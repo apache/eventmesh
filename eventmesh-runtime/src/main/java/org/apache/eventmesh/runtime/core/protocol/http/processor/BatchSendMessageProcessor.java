8b9ed9e224a1bc72d976f8b8a04fc7689cdbc804
KevinClair <704714211@qq.com>
2022-08-09 22:44:25 +0800

    Remove the invalid code.

--- eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/core/protocol/http/processor/BatchSendMessageProcessor.java
+++ eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/core/protocol/http/processor/BatchSendMessageProcessor.java
@@ -289,19 +289,7 @@ public class BatchSendMessageProcessor implements HttpRequestProcessor {
             for (List<CloudEvent> eventlist : topicBatchMessageMappings.values()) {
                 // TODO: Implementation in API. Consider whether to put it in the plug-in.
                 CloudEvent event = null;
-                //Message omsMsg = new Message();
-                //try {
-                //    msgBatch = msgBatch.generateFromList(batchMsgs);
-                //    for (Message message : msgBatch.getMessages()) {
-                //        // TODO: Detect the maximum length of messages for different producers.
-                //        Validators.checkMessage(message, batchEventMeshProducer.getMqProducerWrapper().getDefaultMQProducer());
-                //        MessageClientIDSetter.setUniqID(message);
-                //    }
-                //    msgBatch.setBody(msgBatch.encode());
-                //} catch (Exception e) {
-                //    continue;
-                //}
-
+                // TODO: Detect the maximum length of messages for different producers.
                 final SendMessageContext sendMessageContext = new SendMessageContext(batchId, event, batchEventMeshProducer,
                         eventMeshHTTPServer);
                 sendMessageContext.setEventList(eventlist);
