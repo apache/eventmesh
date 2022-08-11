3f777269eb5a90a594d7d3c594462d2865837496
Xiaoyang Liu <siujoeng.lau@gmail.com>
2022-08-11 11:50:28 -0400

    Implement the Event Management for Standalone
    Signed-off-by: Xiaoyang Liu <siujoeng.lau@gmail.com>

--- eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/admin/handler/EventHandler.java
+++ eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/admin/handler/EventHandler.java
@@ -17,17 +17,23 @@
 
 package org.apache.eventmesh.runtime.admin.handler;
 
+import io.cloudevents.jackson.JsonFormat;
+import io.cloudevents.CloudEvent;
+import io.cloudevents.core.provider.EventFormatProvider;
+import org.apache.eventmesh.runtime.admin.request.CreateTopicRequest;
 import org.apache.eventmesh.runtime.admin.response.Error;
-import org.apache.eventmesh.runtime.admin.response.GetConfigurationResponse;
+import org.apache.eventmesh.runtime.admin.utils.HttpExchangeUtils;
 import org.apache.eventmesh.runtime.admin.utils.JsonUtils;
-import org.apache.eventmesh.runtime.configuration.EventMeshGrpcConfiguration;
-import org.apache.eventmesh.runtime.configuration.EventMeshHTTPConfiguration;
-import org.apache.eventmesh.runtime.configuration.EventMeshTCPConfiguration;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.eventmesh.runtime.core.plugin.MQAdminWrapper;
 import org.slf4j.Logger;
@@ -42,12 +48,17 @@ import com.sun.net.httpserver.HttpHandler;
 public class EventHandler implements HttpHandler {
     private static final Logger logger = LoggerFactory.getLogger(ConfigurationHandler.class);
 
-    private final String connectorPluginType;
+    private final MQAdminWrapper admin;
 
     public EventHandler(
             String connectorPluginType
     ) {
-        this.connectorPluginType = connectorPluginType;
+        admin = new MQAdminWrapper(connectorPluginType);
+        try {
+            admin.init(null);
+        } catch (Exception ignored) {
+            logger.info("failed to initialize MQAdminWrapper");
+        }
     }
 
     /**
@@ -63,9 +74,25 @@ public class EventHandler implements HttpHandler {
         out.close();
     }
 
+    private Map<String, String> queryToMap(String query) {
+        if (query == null) {
+            return null;
+        }
+        Map<String, String> result = new HashMap<>();
+        for (String param : query.split("&")) {
+            String[] entry = param.split("=");
+            if (entry.length > 1) {
+                result.put(entry[0], entry[1]);
+            } else {
+                result.put(entry[0], "");
+            }
+        }
+        return result;
+    }
+
     /**
      * GET /event
-     * Return a response that contains the EventMesh configuration
+     * Return the list of event
      */
     void get(HttpExchange httpExchange) throws IOException {
         OutputStream out = httpExchange.getResponseBody();
@@ -73,34 +100,69 @@ public class EventHandler implements HttpHandler {
         httpExchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
 
         try {
-                //            GetConfigurationResponse getConfigurationResponse = new GetConfigurationResponse(
-                //                    eventMeshTCPConfiguration.sysID,
-                //                    eventMeshTCPConfiguration.namesrvAddr,
-                //                    eventMeshTCPConfiguration.eventMeshEnv,
-                //                    eventMeshTCPConfiguration.eventMeshIDC,
-                //                    eventMeshTCPConfiguration.eventMeshCluster,
-                //                    eventMeshTCPConfiguration.eventMeshServerIp,
-                //                    eventMeshTCPConfiguration.eventMeshName,
-                //                    eventMeshTCPConfiguration.eventMeshWebhookOrigin,
-                //                    eventMeshTCPConfiguration.eventMeshServerSecurityEnable,
-                //                    eventMeshTCPConfiguration.eventMeshServerRegistryEnable,
-                //
-                //                    // TCP Configuration
-                //                    eventMeshTCPConfiguration.eventMeshTcpServerPort,
-                //                    eventMeshTCPConfiguration.eventMeshTcpServerEnabled,
-                //
-                //                    // HTTP Configuration
-                //                    eventMeshHTTPConfiguration.httpServerPort,
-                //                    eventMeshHTTPConfiguration.eventMeshServerUseTls,
-                //
-                //                    // gRPC Configuration
-                //                    eventMeshGrpcConfiguration.grpcServerPort,
-                //                    eventMeshGrpcConfiguration.eventMeshServerUseTls
-                //            );
-                //
-                //            String result = JsonUtils.toJson(getConfigurationResponse);
-                //            httpExchange.sendResponseHeaders(200, result.getBytes().length);
-                //            out.write(result.getBytes());
+            String queryString = httpExchange.getRequestURI().getQuery();
+            if (queryString == null || queryString.equals("")) {
+                httpExchange.sendResponseHeaders(401, 0);
+                out.close();
+                return;
+            }
+
+            Map<String, String> queryMap = queryToMap(queryString);
+            String topicName = queryMap.get("topicName");
+            int offset = Integer.parseInt(queryMap.get("offset"));
+            int length = Integer.parseInt(queryMap.get("length"));
+            List<CloudEvent> eventList = admin.getEvent(topicName, offset, length);
+
+            List<String> eventJsonList = new ArrayList<>();
+            for (CloudEvent event : eventList) {
+                byte[]serializedEvent = EventFormatProvider
+                        .getInstance()
+                        .resolveFormat(JsonFormat.CONTENT_TYPE)
+                        .serialize(event);
+                eventJsonList.add(new String(serializedEvent, StandardCharsets.UTF_8));
+            }
+            String result = JsonUtils.toJson(eventJsonList);
+            httpExchange.sendResponseHeaders(200, result.getBytes().length);
+            out.write(result.getBytes());
+        } catch (Exception e) {
+            StringWriter writer = new StringWriter();
+            PrintWriter printWriter = new PrintWriter(writer);
+            e.printStackTrace(printWriter);
+            printWriter.flush();
+            String stackTrace = writer.toString();
+
+            Error error = new Error(e.toString(), stackTrace);
+            String result = JsonUtils.toJson(error);
+            httpExchange.sendResponseHeaders(500, result.getBytes().length);
+            out.write(result.getBytes());
+        } finally {
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    logger.warn("out close failed...", e);
+                }
+            }
+        }
+    }
+
+    /**
+     * POST /event
+     * Create an event
+     */
+    void post(HttpExchange httpExchange) throws IOException {
+        OutputStream out = httpExchange.getResponseBody();
+        httpExchange.getResponseHeaders().add("Content-Type", "application/json");
+        httpExchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+
+        try {
+            String request = HttpExchangeUtils.streamToString(httpExchange.getRequestBody());
+            byte[] rawRequest = request.getBytes(StandardCharsets.UTF_8);
+            CloudEvent event = EventFormatProvider
+                    .getInstance()
+                    .resolveFormat(JsonFormat.CONTENT_TYPE).deserialize(rawRequest);
+            admin.publish(event);
+            httpExchange.sendResponseHeaders(200, 0);
         } catch (Exception e) {
             StringWriter writer = new StringWriter();
             PrintWriter printWriter = new PrintWriter(writer);
@@ -127,6 +189,9 @@ public class EventHandler implements HttpHandler {
     public void handle(HttpExchange httpExchange) throws IOException {
         if (httpExchange.getRequestMethod().equals("OPTIONS")) {
             preflight(httpExchange);
+        }
+        if (httpExchange.getRequestMethod().equals("POST")) {
+            post(httpExchange);
         }
         if (httpExchange.getRequestMethod().equals("GET")) {
             get(httpExchange);
