ade1e21f14b42e861ce8273abe356727604a1b40
Xiaoyang Liu <siujoeng.lau@gmail.com>
2022-08-11 13:20:01 -0400

    Improve the client management
    Signed-off-by: Xiaoyang Liu <siujoeng.lau@gmail.com>

---
+++ eventmesh-dashboard/components/client/GrpcClientTable.tsx
@@ -0,0 +1,217 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import {
+  HStack,
+  Select,
+  Input,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  useToast,
+  Box,
+  Button,
+} from '@chakra-ui/react';
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
+import { AppContext } from '../../context/context';
+
+interface GrpcClient {
+  env: string,
+  subsystem: string,
+  url: string,
+  pid: number,
+  host: string,
+  port: number,
+  version: string,
+  idc: string,
+  group: string,
+  purpose: string,
+  protocol: string,
+}
+
+interface GrpcClientProps {
+  url: string,
+  group: string,
+}
+
+interface RemoveGrpcClientRequest {
+  url: string,
+}
+
+const GrpcClientRow = ({
+  url, group,
+}: GrpcClientProps) => {
+  const { state } = useContext(AppContext);
+
+  const toast = useToast();
+  const [loading, setLoading] = useState(false);
+  const onRemoveClick = async () => {
+    try {
+      setLoading(true);
+      await axios.delete<RemoveGrpcClientRequest>(`${state.endpoint}/client/grpc`, {
+        data: {
+          url,
+        },
+      });
+      setLoading(false);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        toast({
+          title: 'Failed to remove the gRPC Client',
+          description: error.message,
+          status: 'error',
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+    }
+  };
+
+  return (
+    <Tr>
+      <Td>{url}</Td>
+      <Td>{group}</Td>
+      <Td>
+        <HStack>
+          <Button
+            colorScheme="red"
+            isLoading={loading}
+            onClick={onRemoveClick}
+          >
+            Remove
+          </Button>
+        </HStack>
+      </Td>
+    </Tr>
+  );
+};
+
+const GrpcClientTable = () => {
+  const { state } = useContext(AppContext);
+
+  const [searchInput, setSearchInput] = useState<string>('');
+  const handleSearchInputChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setSearchInput(event.currentTarget.value);
+  };
+
+  const [groupSet, setGroupSet] = useState<Set<string>>(new Set());
+  const [groupFilter, setGroupFilter] = useState<string>('');
+  const handleGroupSelectChange = (event: React.FormEvent<HTMLSelectElement>) => {
+    setGroupFilter(event.currentTarget.value);
+  };
+
+  const [GrpcClientList, setGrpcClientList] = useState<GrpcClient[]>([]);
+  const toast = useToast();
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const { data } = await axios.get<GrpcClient[]>(`${state.endpoint}/client/grpc`);
+        setGrpcClientList(data);
+
+        const nextGroupSet = new Set<string>();
+        data.forEach(({ group }) => {
+          nextGroupSet.add(group);
+        });
+        setGroupSet(nextGroupSet);
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          toast({
+            title: 'Failed to fetch the list of gRPC Clients',
+            description: error.message,
+            status: 'error',
+            duration: 3000,
+            isClosable: true,
+          });
+          setGrpcClientList([]);
+        }
+      }
+    };
+
+    fetch();
+  }, []);
+
+  return (
+    <Box
+      maxW="full"
+      bg="white"
+      borderWidth="1px"
+      borderRadius="md"
+      overflow="hidden"
+      p="4"
+    >
+      <HStack
+        spacing="2"
+      >
+        <Input
+          w="200%"
+          placeholder="Search"
+          value={searchInput}
+          onChange={handleSearchInputChange}
+        />
+        <Select
+          placeholder="Select Group"
+          onChange={handleGroupSelectChange}
+        >
+          {Array.from(groupSet).map((group) => (
+            <option value={group} key={group}>{group}</option>
+          ))}
+        </Select>
+      </HStack>
+
+      <TableContainer>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>URL</Th>
+              <Th>Group</Th>
+              <Th>Action</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {GrpcClientList && GrpcClientList.filter(({
+              url, group,
+            }) => {
+              if (searchInput && !url.includes(searchInput)) {
+                return false;
+              }
+              if (groupFilter && groupFilter !== group) {
+                return false;
+              }
+              return true;
+            }).map(({
+              url, group,
+            }) => (
+              <GrpcClientRow
+                url={url}
+                group={group}
+              />
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+};
+
+export default GrpcClientTable;
---
+++ eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/admin/handler/TCPClientHandler.java
@@ -0,0 +1,204 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.eventmesh.runtime.admin.handler;
+
+import org.apache.eventmesh.common.protocol.tcp.UserAgent;
+import org.apache.eventmesh.runtime.admin.request.DeleteTCPClientRequest;
+import org.apache.eventmesh.runtime.admin.response.Error;
+import org.apache.eventmesh.runtime.admin.response.GetClientResponse;
+import org.apache.eventmesh.runtime.admin.utils.HttpExchangeUtils;
+import org.apache.eventmesh.runtime.admin.utils.JsonUtils;
+import org.apache.eventmesh.runtime.boot.EventMeshTCPServer;
+import org.apache.eventmesh.runtime.core.protocol.grpc.consumer.ConsumerManager;
+import org.apache.eventmesh.runtime.core.protocol.grpc.consumer.consumergroup.ConsumerGroupClient;
+import org.apache.eventmesh.runtime.core.protocol.http.processor.inf.Client;
+import org.apache.eventmesh.runtime.core.protocol.tcp.client.EventMeshTcp2Client;
+import org.apache.eventmesh.runtime.core.protocol.tcp.client.group.ClientSessionGroupMapping;
+import org.apache.eventmesh.runtime.core.protocol.tcp.client.session.Session;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+/**
+ * The client handler
+ */
+public class TCPClientHandler implements HttpHandler {
+    private static final Logger logger = LoggerFactory.getLogger(TCPClientHandler.class);
+
+    private final EventMeshTCPServer eventMeshTCPServer;
+
+    public TCPClientHandler(
+            EventMeshTCPServer eventMeshTCPServer
+    ) {
+        this.eventMeshTCPServer = eventMeshTCPServer;
+    }
+
+    /**
+     * OPTIONS /client
+     */
+    void preflight(HttpExchange httpExchange) throws IOException {
+        httpExchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+        httpExchange.getResponseHeaders().add("Access-Control-Allow-Methods", "*");
+        httpExchange.getResponseHeaders().add("Access-Control-Allow-Headers", "*");
+        httpExchange.getResponseHeaders().add("Access-Control-Max-Age", "86400");
+        httpExchange.sendResponseHeaders(200, 0);
+        OutputStream out = httpExchange.getResponseBody();
+        out.close();
+    }
+
+    /**
+     * DELETE /client/tcp
+     */
+    void delete(HttpExchange httpExchange) throws IOException {
+        OutputStream out = httpExchange.getResponseBody();
+        try {
+            String request = HttpExchangeUtils.streamToString(httpExchange.getRequestBody());
+            DeleteTCPClientRequest deleteTCPClientRequest = JsonUtils.toObject(request, DeleteTCPClientRequest.class);
+            String host = deleteTCPClientRequest.host;
+            int port = deleteTCPClientRequest.port;
+
+            ClientSessionGroupMapping clientSessionGroupMapping = eventMeshTCPServer.getClientSessionGroupMapping();
+            ConcurrentHashMap<InetSocketAddress, Session> sessionMap = clientSessionGroupMapping.getSessionMap();
+            if (!sessionMap.isEmpty()) {
+                for (Map.Entry<InetSocketAddress, Session> entry : sessionMap.entrySet()) {
+                    if (entry.getKey().getHostString().equals(host) && entry.getKey().getPort() == port) {
+                        EventMeshTcp2Client.serverGoodby2Client(
+                                eventMeshTCPServer,
+                                entry.getValue(),
+                                clientSessionGroupMapping
+                        );
+                    }
+                }
+            }
+
+            httpExchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+            httpExchange.sendResponseHeaders(200, 0);
+        } catch (Exception e) {
+            StringWriter writer = new StringWriter();
+            PrintWriter printWriter = new PrintWriter(writer);
+            e.printStackTrace(printWriter);
+            printWriter.flush();
+            String stackTrace = writer.toString();
+
+            Error error = new Error(e.toString(), stackTrace);
+            String result = JsonUtils.toJson(error);
+            httpExchange.sendResponseHeaders(500, result.getBytes().length);
+            out.write(result.getBytes());
+        } finally {
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    logger.warn("out close failed...", e);
+                }
+            }
+        }
+    }
+
+    /**
+     * GET /client/tcp
+     * Return a response that contains the list of clients
+     */
+    void list(HttpExchange httpExchange) throws IOException {
+        OutputStream out = httpExchange.getResponseBody();
+        httpExchange.getResponseHeaders().add("Content-Type", "application/json");
+        httpExchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+
+        try {
+            // Get the list of TCP clients
+            ClientSessionGroupMapping clientSessionGroupMapping = eventMeshTCPServer.getClientSessionGroupMapping();
+            Map<InetSocketAddress, Session> sessionMap = clientSessionGroupMapping.getSessionMap();
+            List<GetClientResponse> getClientResponseList = new ArrayList<>();
+            for (Session session : sessionMap.values()) {
+                UserAgent userAgent = session.getClient();
+                GetClientResponse getClientResponse = new GetClientResponse(
+                        Optional.ofNullable(userAgent.getEnv()).orElse(""),
+                        Optional.ofNullable(userAgent.getSubsystem()).orElse(""),
+                        Optional.ofNullable(userAgent.getPath()).orElse(""),
+                        String.valueOf(userAgent.getPid()),
+                        Optional.ofNullable(userAgent.getHost()).orElse(""),
+                        userAgent.getPort(),
+                        Optional.ofNullable(userAgent.getVersion()).orElse(""),
+                        Optional.ofNullable(userAgent.getIdc()).orElse(""),
+                        Optional.ofNullable(userAgent.getGroup()).orElse(""),
+                        Optional.ofNullable(userAgent.getPurpose()).orElse(""),
+                        "TCP"
+                );
+                getClientResponseList.add(getClientResponse);
+            }
+
+            getClientResponseList.sort((lhs, rhs) -> {
+                if (lhs.host.equals(rhs.host)) {
+                    return lhs.host.compareTo(rhs.host);
+                }
+                return Integer.compare(rhs.port, lhs.port);
+            });
+
+            String result = JsonUtils.toJson(getClientResponseList);
+            httpExchange.sendResponseHeaders(200, result.getBytes().length);
+            out.write(result.getBytes());
+        } catch (Exception e) {
+            StringWriter writer = new StringWriter();
+            PrintWriter printWriter = new PrintWriter(writer);
+            e.printStackTrace(printWriter);
+            printWriter.flush();
+            String stackTrace = writer.toString();
+
+            Error error = new Error(e.toString(), stackTrace);
+            String result = JsonUtils.toJson(error);
+            httpExchange.sendResponseHeaders(500, result.getBytes().length);
+            out.write(result.getBytes());
+        } finally {
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    logger.warn("out close failed...", e);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        if (httpExchange.getRequestMethod().equals("OPTIONS")) {
+            preflight(httpExchange);
+        }
+        if (httpExchange.getRequestMethod().equals("GET")) {
+            list(httpExchange);
+        }
+        if (httpExchange.getRequestMethod().equals("DELETE")) {
+            delete(httpExchange);
+        }
+    }
+}
