fc06d0ed29b9e270505f2160009ec38b7b956fcc
walleliu1016 <liulefirst@163.com>
2022-08-22 21:25:39 +0800

    adapter the new connector plugin

--- eventmesh-server-go/pkg/connector/action.go
+++
@@ -1,9 +0,0 @@
-package connector
-
-type EventMeshAction string
-
-const (
-	CommitMessage  EventMeshAction = "CommitMessage"
-	ReconsumeLater EventMeshAction = "ReconsumeLater"
-	ManualAck      EventMeshAction = "ManualAck"
-)
