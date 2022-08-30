b3352a16b6f34df6131a71c63e80e9642e46a622
Timcross-WEI <timcross-wei@qq.com>
2022-08-31 00:15:04 +0800

    add config item

--- eventmesh-connector-plugin/eventmesh-connector-pravega/src/main/java/org/apache/eventmesh/connector/pravega/config/PravegaConnectorConfig.java
+++ eventmesh-connector-plugin/eventmesh-connector-pravega/src/main/java/org/apache/eventmesh/connector/pravega/config/PravegaConnectorConfig.java
@@ -27,6 +27,10 @@ import lombok.Getter;
 public class PravegaConnectorConfig {
     public static String EVENTMESH_PRAVEGA_CONTROLLER_URI = "eventMesh.server.pravega.controller.uri";
     public static String EVENTMESH_PRAVEGA_SCOPE = "eventMesh.server.pravega.scope";
+    public static String EVENTMESH_PRAVEGA_USERNAME = "eventMesh.server.pravega.username";
+    public static String EVENTMESH_PRAVEGA_PASSWORD = "eventMesh.server.pravega.password";
+    public static String EVENTMESH_PRAVEGA_TLS_ENABLED = "eventMesh.server.pravega.tlsEnabled";
+    public static String EVENTMESH_PRAVEGA_TRUSTSTORE = "eventMesh.server.pravega.truststore";
     public static String EVENTMESH_PRAVEGA_CLIENTPOOL_SIZE = "eventMesh.server.pravega.clientPool.size";
     public static String EVENTMESH_PRAVEGA_QUEUE_SIZE = "eventMesh.server.pravega.queue.size";
 
@@ -34,6 +38,11 @@ public class PravegaConnectorConfig {
     private String scope = "eventmesh-pravega";
     private int clientPoolSize = 8;
     private int queueSize = 512;
+    private String username = "";
+    private String password = "";
+    private boolean authEnabled = false;
+    private boolean tlsEnable = false;
+    private String truststore = "";
 
     private static PravegaConnectorConfig INSTANCE = null;
 
@@ -59,6 +68,26 @@ public class PravegaConnectorConfig {
             scope = StringUtils.trim(scopeStr);
         }
 
+        String usernameStr = PravegaConnectorConfigWrapper.getProp(EVENTMESH_PRAVEGA_USERNAME);
+        if (StringUtils.isNotBlank(usernameStr)) {
+            username = StringUtils.trim(usernameStr);
+        }
+
+        String passwordStr = PravegaConnectorConfigWrapper.getProp(EVENTMESH_PRAVEGA_PASSWORD);
+        if (StringUtils.isNotBlank(passwordStr)) {
+            password = StringUtils.trim(passwordStr);
+        }
+
+        String tlsEnableStr = PravegaConnectorConfigWrapper.getProp(EVENTMESH_PRAVEGA_TLS_ENABLED);
+        if (StringUtils.isNotBlank(tlsEnableStr)) {
+            tlsEnable = Boolean.parseBoolean(StringUtils.trim(tlsEnableStr));
+        }
+
+        String truststoreStr = PravegaConnectorConfigWrapper.getProp(EVENTMESH_PRAVEGA_TRUSTSTORE);
+        if (StringUtils.isNotBlank(truststoreStr)) {
+            truststore = StringUtils.trim(truststoreStr);
+        }
+
         String clientPoolSizeStr = PravegaConnectorConfigWrapper.getProp(EVENTMESH_PRAVEGA_CLIENTPOOL_SIZE);
         if (StringUtils.isNumeric(clientPoolSizeStr)) {
             clientPoolSize = Integer.parseInt(clientPoolSizeStr);
@@ -67,6 +96,10 @@ public class PravegaConnectorConfig {
         String queueSizeStr = PravegaConnectorConfigWrapper.getProp(EVENTMESH_PRAVEGA_QUEUE_SIZE);
         if (StringUtils.isNumeric(queueSizeStr)) {
             queueSize = Integer.parseInt(queueSizeStr);
+        }
+
+        if (StringUtils.isNotBlank(username) && StringUtils.isNotBlank(password)) {
+            authEnabled = true;
         }
     }
 }
