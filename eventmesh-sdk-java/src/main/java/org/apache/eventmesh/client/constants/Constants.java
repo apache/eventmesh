f2c997fd11228209fd6578aa21f212e000b943eb
walterlife <walterzywei@gmail.com>
2022-09-10 18:02:43 +0800

    fix checkstyle problems

--- eventmesh-sdk-java/src/main/java/org/apache/eventmesh/client/constants/Constants.java
+++ eventmesh-sdk-java/src/main/java/org/apache/eventmesh/client/constants/Constants.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.eventmesh.client.constants;
 
 public class Constants {
@@ -5,9 +22,9 @@ public class Constants {
 
     public static final String EVENTMESH_SELECTOR_NACOS_ADDRESS = "eventmesh.selector.nacos.address_list";
 
-    public static final String EVENTMESH_SELECTOR_NACOS_USERNAME= "eventmesh.selector.nacos.username";
+    public static final String EVENTMESH_SELECTOR_NACOS_USERNAME = "eventmesh.selector.nacos.username";
 
-    public static final String EVENTMESH_SELECTOR_NACOS_PASSWORD= "eventmesh.selector.nacos.password";
+    public static final String EVENTMESH_SELECTOR_NACOS_PASSWORD = "eventmesh.selector.nacos.password";
 
     public static final String EVENTMESH_WORKFLOW_SERVER_NAME = "eventmesh.workflow.server.name";
 
