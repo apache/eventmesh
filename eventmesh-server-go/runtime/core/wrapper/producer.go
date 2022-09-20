90dc6c208f9ef67158bf27e36216ecca7cab1fe9
horoc <horoc.chen@gmail.com>
2022-09-20 20:40:51 +0800

    combine connector api

--- eventmesh-server-go/runtime/core/wrapper/producer.go
+++ eventmesh-server-go/runtime/core/wrapper/producer.go
@@ -47,8 +47,8 @@ func NewProducer() (*Producer, error) {
 		return nil, ErrNoConnectorName
 	}
 	log.Infof("init producer with connector name:%s", connectorPluginName)
-	factory := plugin.Get(connector.ProducerPluginType, connectorPluginName.Value).(connector.ProducerFactory)
-	consu, err := factory.Get()
+	factory := plugin.Get(connector.PluginType, connectorPluginName.Value).(connector.Factory)
+	consu, err := factory.GetProducer()
 	if err != nil {
 		return nil, err
 	}
