bb3acab1a6abf6efdd2fde176d6f04b6443d0dd7
walterlife <walterzywei@gmail.com>
2022-10-01 23:58:55 +0800

    modify event/operation/switch task execute logic

--- eventmesh-workflow-go/internal/task/switch_task.go
+++ eventmesh-workflow-go/internal/task/switch_task.go
@@ -16,19 +16,54 @@
 package task
 
 import (
+	"github.com/apache/incubator-eventmesh/eventmesh-server-go/config"
 	"github.com/apache/incubator-eventmesh/eventmesh-workflow-go/internal/constants"
 	"github.com/apache/incubator-eventmesh/eventmesh-workflow-go/internal/dal/model"
+	"github.com/apache/incubator-eventmesh/eventmesh-workflow-go/internal/queue"
+	"github.com/apache/incubator-eventmesh/eventmesh-workflow-go/third_party/jqer"
+	"github.com/gogf/gf/util/gconv"
 )
 
-type SwitchTask struct {
-	BaseTask
-	Transitions []*model.WorkflowTaskRelation
+type switchTask struct {
+	baseTask
+	transitions []*model.WorkflowTaskRelation
+	jq          jqer.JQ
 }
 
-func (t *SwitchTask) Type() string {
-	return constants.TaskTypeSwitch
+func NewSwitchTask(instance *model.WorkflowTaskInstance) Task {
+	var t switchTask
+	if instance == nil || instance.Task == nil {
+		return nil
+	}
+	t.baseTask = baseTask{taskID: instance.TaskID, taskInstanceID: instance.TaskInstanceID, input: instance.Input,
+		workflowID: instance.WorkflowID, workflowInstanceID: instance.WorkflowInstanceID,
+		taskType: instance.Task.TaskType}
+	t.transitions = instance.Task.ChildTasks
+	t.baseTask.queue = queue.GetQueue(config.GlobalConfig().Flow.Queue.Store)
+	t.jq = jqer.NewJQ()
+	return &t
 }
 
-func (t *SwitchTask) Run() error {
-	return nil
+func (t *switchTask) Run() error {
+	if len(t.transitions) == 0 {
+		return nil
+	}
+	var tasks []*model.WorkflowTaskInstance
+	for _, transition := range t.transitions {
+		res, err := t.jq.One(t.input, transition.Condition)
+		if err != nil {
+			return err
+		}
+		if !gconv.Bool(res) {
+			continue
+		}
+		if transition.ToTaskID == constants.TaskEndID {
+			break
+		}
+		var taskInstance = model.WorkflowTaskInstance{WorkflowInstanceID: t.workflowInstanceID,
+			WorkflowID: t.workflowID, TaskID: transition.ToTaskID, TaskInstanceID: t.taskInstanceID,
+			Status: constants.TaskInstanceWaitStatus, Input: t.baseTask.input}
+		tasks = append(tasks, &taskInstance)
+	}
+	return t.baseTask.queue.Publish(tasks)
 }
