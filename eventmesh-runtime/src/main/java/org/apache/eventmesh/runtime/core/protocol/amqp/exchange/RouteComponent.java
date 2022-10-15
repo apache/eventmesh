65c295a70b12356a4e64b7ef646b05ba0fae2fba
wangshaojie <wangshaojie@cmss.chinamobile.com>
2022-10-15 10:59:41 +0800

    define message router interface

--- eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/core/protocol/amqp/exchange/RouteComponent.java
+++ eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/core/protocol/amqp/exchange/RouteComponent.java
@@ -15,25 +15,16 @@
  * limitations under the License.
  */
 
-package org.apache.eventmesh.runtime.core.protocol.amqp;
+package org.apache.eventmesh.runtime.core.protocol.amqp.exchange;
 
-import org.apache.eventmesh.runtime.core.protocol.amqp.remoting.metamodels.AmqpQueue;
+import org.apache.eventmesh.runtime.core.protocol.amqp.metadata.model.BindingInfo;
 
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.Set;
 
-/**
- * manage all queues used in the server
- */
-public class QueueContainer {
-    private ExchangeContainer exchangeContainer;
+public interface RouteComponent {
 
-    private Map<VirtualHost, Map<String, AmqpQueue>> queueMap;
-
-    public QueueContainer(ExchangeContainer exchangeContainer) {
-        this.exchangeContainer = exchangeContainer;
-        this.queueMap = new ConcurrentHashMap<>();
-    }
-
+    RoutingResult routMessage(Set<BindingInfo> bindingInfos, AmqpRouter.Type type,
+                              Map<String, Object> routingInfo);
 
 }
