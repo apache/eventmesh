bb25283ff815d2ddac322108b32c347b9cf22af1
horoc <horoc.chen@gmail.com>
2022-10-30 00:16:16 +0800

    add license header

--- eventmesh-sdk-go/http/producer/cloudevent_producer_test.go
+++ eventmesh-sdk-go/http/producer/cloudevent_producer_test.go
@@ -1,3 +1,18 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package producer
 
 import (
@@ -47,12 +62,6 @@ func TestEventMeshHttpProducer_Publish(t *testing.T) {
 }
 
 func TestEventMeshHttpProducer_Request(t *testing.T) {
-	//replyMessage := http_protocol.ReplyMessage{
-	//	Topic: "test-topic",
-	//	Body:  `{"data" : "val"}`,
-	//}
-	//replyMessageJson := utils.MarshalJsonString(replyMessage)
-
 	f := func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(`{"retCode":0, "retMsg":"{\"topic\":\"test-topic\",\"body\":\"{\\\"data\\\":1}\",\"properties\":null}"}`))
