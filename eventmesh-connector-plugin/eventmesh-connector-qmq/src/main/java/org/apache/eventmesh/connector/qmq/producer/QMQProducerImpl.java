6823b46786f6919f9ed6ec6ddaa56e8ecb1dd9a1
jonyangx <jonyang1120@gmail.com>
2022-11-03 15:25:00 +0800

    add qmq connector

---
+++ eventmesh-connector-plugin/eventmesh-connector-qmq/src/main/java/org/apache/eventmesh/connector/qmq/producer/QMQProducerImpl.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.eventmesh.connector.qmq.producer;
+
+import org.apache.eventmesh.api.RequestReplyCallback;
+import org.apache.eventmesh.api.SendCallback;
+import org.apache.eventmesh.api.exception.ConnectorRuntimeException;
+import org.apache.eventmesh.api.producer.Producer;
+
+import java.util.Properties;
+
+import io.cloudevents.CloudEvent;
+
+public class QMQProducerImpl implements Producer {
+
+    private ProducerImpl producer;
+
+    @Override
+    public synchronized void init(Properties properties) {
+        producer = new ProducerImpl(properties);
+        producer.init();
+    }
+
+    //for test
+    public ProducerImpl getProducer() {
+        return this.producer;
+    }
+
+    @Override
+    public void publish(CloudEvent cloudEvent, SendCallback sendCallback) throws Exception {
+        producer.publish(cloudEvent, sendCallback);
+    }
+
+    @Override
+    public boolean isStarted() {
+        return producer.isStarted();
+    }
+
+    @Override
+    public boolean isClosed() {
+        return producer.isClosed();
+    }
+
+    @Override
+    public void start() {
+        producer.start();
+    }
+
+    @Override
+    public void shutdown() {
+        producer.shutdown();
+    }
+
+    @Override
+    public void sendOneway(CloudEvent cloudEvent) {
+        throw new ConnectorRuntimeException("SendOneWay is not supported");
+    }
+
+    @Override
+    public void request(CloudEvent cloudEvent, RequestReplyCallback rrCallback, long timeout) throws Exception {
+        throw new ConnectorRuntimeException("Request is not supported");
+    }
+
+    @Override
+    public boolean reply(CloudEvent cloudEvent, SendCallback sendCallback) throws Exception {
+        throw new ConnectorRuntimeException("Reply is not supported");
+    }
+
+    @Override
+    public void checkTopicExist(String topic) throws Exception {
+        throw new ConnectorRuntimeException("CheckTopicExist is not supported");
+    }
+
+    @Override
+    public void setExtFields() {
+        throw new ConnectorRuntimeException("SetExtFields is not supported");
+    }
+}
