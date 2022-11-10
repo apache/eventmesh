92e36d5efe71512303230b9857d57c8030c5e624
774263648 <32287650+774263648@users.noreply.github.com>
2022-11-10 17:13:09 +0800

    Update RedirectClientByIpPortHandler.java
    use try-with-resources

--- eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/admin/handler/RedirectClientByIpPortHandler.java
+++ eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/admin/handler/RedirectClientByIpPortHandler.java
@@ -56,11 +56,15 @@ public class RedirectClientByIpPortHandler extends AbstractHttpHandler {
     public void handle(HttpExchange httpExchange) throws IOException {
         String result = "";
 <<<<<<< HEAD
+<<<<<<< HEAD
         try (OutputStream out = httpExchange.getResponseBody()) {
 =======
         OutputStream out = httpExchange.getResponseBody();
         try(out) {
 >>>>>>> 9f48f05c (Update RedirectClientByIpPortHandler.java)
+=======
+        try(OutputStream out = httpExchange.getResponseBody()) {
+>>>>>>> 2eed2012 (Update RedirectClientByIpPortHandler.java)
             String queryString = httpExchange.getRequestURI().getQuery();
             Map<String, String> queryStringInfo = NetUtils.formData2Dic(queryString);
             String ip = queryStringInfo.get(EventMeshConstants.MANAGE_IP);
