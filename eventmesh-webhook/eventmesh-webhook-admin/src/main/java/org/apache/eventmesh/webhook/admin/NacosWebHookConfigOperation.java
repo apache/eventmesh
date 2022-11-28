cb012f79cdfd37062a0203b2ac4cc9b171255600
Bigotry-zb <1923613738@qq.com>
2022-11-28 17:16:44 +0800

    Change Boolean to boolean

--- eventmesh-webhook/eventmesh-webhook-admin/src/main/java/org/apache/eventmesh/webhook/admin/NacosWebHookConfigOperation.java
+++ eventmesh-webhook/eventmesh-webhook-admin/src/main/java/org/apache/eventmesh/webhook/admin/NacosWebHookConfigOperation.java
@@ -106,7 +106,8 @@ public class NacosWebHookConfigOperation implements WebHookConfigOperation {
 
     @Override
     public Integer updateWebHookConfig(WebHookConfig webHookConfig) {
-        Boolean result = false;
+        // 修改代码部分
+        boolean result = false;
         try {
             if (configService.getConfig(getWebHookConfigDataId(webHookConfig), getManuGroupId(webHookConfig), TIMEOUT_MS) == null) {
                 logger.error("updateWebHookConfig failed, config is not existed");
@@ -122,7 +123,7 @@ public class NacosWebHookConfigOperation implements WebHookConfigOperation {
 
     @Override
     public Integer deleteWebHookConfig(WebHookConfig webHookConfig) {
-        Boolean result = false;
+        boolean result = false;
         String manufacturerName = webHookConfig.getManufacturerName();
         try {
             result = configService.removeConfig(getWebHookConfigDataId(webHookConfig), getManuGroupId(webHookConfig));
