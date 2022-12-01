8ea8fbd589b1d35c91471de49aae315e3a194b61
weihubeats <weihubeats@163.com>
2022-12-01 15:50:43 +0800

    [ISSUE #2317] Refactoring tool class JsonUtils (#2318)
    * update JsonUtils
    
    * update json

--- eventmesh-common/src/main/java/org/apache/eventmesh/common/utils/JsonUtils.java
+++ eventmesh-common/src/main/java/org/apache/eventmesh/common/utils/JsonUtils.java
@@ -19,8 +19,15 @@ package org.apache.eventmesh.common.utils;
 
 import org.apache.eventmesh.common.exception.JsonException;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
@@ -32,7 +39,10 @@ public class JsonUtils {
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     static {
-        OBJECT_MAPPER.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        OBJECT_MAPPER.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+            .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        OBJECT_MAPPER.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
     }
 
     /**
@@ -42,8 +52,22 @@ public class JsonUtils {
      * @return json string
      */
     public static String serialize(Object obj) {
+        if (Objects.isNull(obj)) {
+            return null;
+        }
         try {
             return OBJECT_MAPPER.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            throw new JsonException("serialize to json error", e);
+        }
+    }
+
+    public static <T> byte[] serialize(String topic, Class<T> data) throws JsonProcessingException {
+        if (Objects.isNull(data)) {
+            return null;
+        }
+        try {
+            return OBJECT_MAPPER.writeValueAsBytes(data);
         } catch (JsonProcessingException e) {
             throw new JsonException("serialize to json error", e);
         }
@@ -52,16 +76,30 @@ public class JsonUtils {
     /**
      * Deserialize json string to object.
      *
-     * @param str json string
-     * @param clz object class
-     * @param <T> object type
+     * @param json json string
+     * @param clz  object class
+     * @param <T>  object type
      * @return object
      */
-    public static <T> T deserialize(String str, Class<T> clz) {
+    public static <T> T deserialize(String json, Class<T> clz) {
+        if (StringUtils.isEmpty(json)) {
+            return null;
+        }
         try {
-            return OBJECT_MAPPER.readValue(str, clz);
+            return OBJECT_MAPPER.readValue(json, clz);
         } catch (JsonProcessingException e) {
             throw new JsonException("deserialize json string to object error", e);
+        }
+    }
+
+    public static <T> T deserialize(Class<T> clazz, byte[] bytes) {
+        if (bytes == null || bytes.length == 0) {
+            return null;
+        }
+        try {
+            return OBJECT_MAPPER.readValue(bytes, clazz);
+        } catch (IOException e) {
+            throw new JsonException(String.format("deserialize bytes to %s error", clazz), e);
         }
     }
 
@@ -74,10 +112,24 @@ public class JsonUtils {
      * @return object
      */
     public static <T> T deserialize(String str, TypeReference<T> typeReference) {
+        if (StringUtils.isEmpty(str)) {
+            return null;
+        }
         try {
             return OBJECT_MAPPER.readValue(str, typeReference);
         } catch (JsonProcessingException e) {
-            throw new JsonException("deserialize json string to object error", e);
+            throw new JsonException("deserialize json string to typeReference error", e);
+        }
+    }
+
+    public static JsonNode getJsonNode(String json) {
+        if (StringUtils.isEmpty(json)) {
+            return null;
+        }
+        try {
+            return OBJECT_MAPPER.readTree(json);
+        } catch (JsonProcessingException e) {
+            throw new JsonException("deserialize json string to JsonNode error", e);
         }
     }
 }
