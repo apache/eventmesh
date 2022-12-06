f196e1c46c6156271d8afbb9c57d7a66142ff0b6
MajorHe1 <53824889+MajorHe1@users.noreply.github.com>
2022-12-06 14:55:11 +0800

    use log instead of print when exception occurred in CloudEventTCPSubClient.java

--- eventmesh-sdk-java/src/main/java/org/apache/eventmesh/client/tcp/impl/cloudevent/CloudEventTCPSubClient.java
+++ eventmesh-sdk-java/src/main/java/org/apache/eventmesh/client/tcp/impl/cloudevent/CloudEventTCPSubClient.java
@@ -135,7 +135,7 @@ class CloudEventTCPSubClient extends TcpClient implements EventMeshTCPSubClient<
             goodbye();
             super.close();
         } catch (Exception ex) {
-            ex.printStackTrace();
+            log.warn("exception occurred when close", ex);
         }
     }
 
