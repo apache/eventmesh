b78996d60310571d3806b1e9c99036f7e6e304b6
jonyangx <yangjun1120@gmail.com>
2022-12-10 19:42:41 +0800

    fix issue2574

--- eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/util/IOTinyUtils.java
+++ eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/util/IOTinyUtils.java
@@ -17,143 +17,65 @@
 
 package org.apache.eventmesh.runtime.util;
 
-import org.apache.eventmesh.runtime.constants.EventMeshConstants;
+import org.apache.commons.io.Charsets;
+import org.apache.commons.io.IOUtils;
 
 import java.io.BufferedReader;
-import java.io.CharArrayWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.Reader;
 import java.io.Writer;
-import java.nio.channels.FileChannel;
-import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.commons.io.FileUtils;
 
 public class IOTinyUtils {
 
-    public static String toString(InputStream input, String encoding) throws IOException {
-        return (null == encoding) ? toString(new InputStreamReader(input, EventMeshConstants.DEFAULT_CHARSET))
-                : toString(new InputStreamReader(input, encoding));
+    public static String toString(final InputStream inputStream, final String charsetName) throws IOException {
+        return IOUtils.toString(inputStream, Charsets.toCharset(charsetName));
     }
 
-    public static String toString(Reader reader) throws IOException {
-        CharArrayWriter sw = new CharArrayWriter();
-        copy(reader, sw);
-        return sw.toString();
+    public static String toString(final Reader reader) throws IOException {
+        return IOUtils.toString(reader);
     }
 
-    public static long copy(Reader input, Writer output) throws IOException {
-        char[] buffer = new char[1 << 12];
-        long count = 0;
-        for (int n = 0; (n = input.read(buffer)) >= 0; ) {
-            output.write(buffer, 0, n);
-            count += n;
-        }
-        return count;
+    public static long copy(final Reader reader, final Writer writer) throws IOException {
+        return IOUtils.copy(reader, writer);
     }
 
-    public static List<String> readLines(Reader input) throws IOException {
-        BufferedReader reader = toBufferedReader(input);
-        List<String> list = new ArrayList<String>();
-        String line;
-        for (; ; ) {
-            line = reader.readLine();
-            if (null != line) {
-                list.add(line);
-            } else {
-                break;
-            }
-        }
-        return list;
+    public static List<String> readLines(final Reader reader) throws IOException {
+        return IOUtils.readLines(reader);
     }
 
-    private static BufferedReader toBufferedReader(Reader reader) {
-        return reader instanceof BufferedReader ? (BufferedReader) reader : new BufferedReader(reader);
+    private static BufferedReader toBufferedReader(final Reader reader) {
+        return IOUtils.toBufferedReader(reader);
     }
 
-    public static void copyFile(String source, String target) throws IOException {
-        File sf = new File(source);
-        if (!sf.exists()) {
-            throw new IllegalArgumentException("source file does not exist.");
-        }
-        File tf = new File(target);
-        tf.getParentFile().mkdirs();
-        if (!tf.exists() && !tf.createNewFile()) {
-            throw new RuntimeException("failed to create target file.");
-        }
-
-        FileChannel sc = null;
-        FileChannel tc = null;
-        try {
-            tc = new FileOutputStream(tf).getChannel();
-            sc = new FileInputStream(sf).getChannel();
-            sc.transferTo(0, sc.size(), tc);
-        } finally {
-            if (null != sc) {
-                sc.close();
-            }
-            if (null != tc) {
-                tc.close();
-            }
-        }
+    public static void copyFile(final String src, final String dest) throws IOException {
+        final File srcFile = new File(src);
+        final File destFile = new File(dest);
+        FileUtils.copyFile(srcFile, destFile);
     }
 
-    public static void delete(File fileOrDir) throws IOException {
-        if (fileOrDir == null) {
+    public static void delete(final File file) throws IOException {
+
+        if (file == null || !file.exists()) {
             return;
         }
 
-        if (fileOrDir.isDirectory()) {
-            cleanDirectory(fileOrDir);
+        if (file.isDirectory()) {
+            FileUtils.deleteDirectory(file);
         }
 
-        fileOrDir.delete();
+        FileUtils.delete(file);
     }
 
-    public static void cleanDirectory(File directory) throws IOException {
-        if (!directory.exists()) {
-            String message = directory + " does not exist";
-            throw new IllegalArgumentException(message);
-        }
-
-        if (!directory.isDirectory()) {
-            String message = directory + " is not a directory";
-            throw new IllegalArgumentException(message);
-        }
-
-        File[] files = directory.listFiles();
-        if (files == null) { // null if security restricted
-            throw new IOException("Failed to list contents of " + directory);
-        }
-
-        IOException exception = null;
-        for (File file : files) {
-            try {
-                delete(file);
-            } catch (IOException ioe) {
-                exception = ioe;
-            }
-        }
-
-        if (null != exception) {
-            throw exception;
-        }
+    public static void cleanDirectory(final File directory) throws IOException {
+        FileUtils.cleanDirectory(directory);
     }
 
-    public static void writeStringToFile(File file, String data, String encoding) throws IOException {
-        OutputStream os = null;
-        try {
-            os = new FileOutputStream(file);
-            os.write(data.getBytes(encoding));
-        } finally {
-            if (null != os) {
-                os.close();
-            }
-        }
+    public static void writeStringToFile(final File file, final String data, final String charset) throws IOException {
+        FileUtils.writeStringToFile(file, data, charset);
     }
 }
