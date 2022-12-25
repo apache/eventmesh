da0cc821a7dfc640c1dae22e888b7ea7b10f43ff
eight-nines <1748526511@qq.com>
2022-12-25 16:16:01 +0800

    Improve configuration management of Redis SPI Impl [1] RedisProperties config improve

--- eventmesh-connector-plugin/eventmesh-connector-redis/src/main/java/org/apache/eventmesh/connector/redis/config/RedisProperties.java
+++ eventmesh-connector-plugin/eventmesh-connector-redis/src/main/java/org/apache/eventmesh/connector/redis/config/RedisProperties.java
@@ -17,33 +17,42 @@
 
 package org.apache.eventmesh.connector.redis.config;
 
+import org.apache.eventmesh.common.config.Config;
+import org.apache.eventmesh.common.config.ConfigFiled;
+
 import java.util.Properties;
 
+@Config(prefix = "eventMesh.server.redis", path = "classPath://redis-client.properties")
 public class RedisProperties {
 
     /**
      * The redis server configuration to be used.
      */
+    @ConfigFiled(field = "serverType")
     private ServerType serverType = ServerType.SINGLE;
 
     /**
      * The master server name used by Redis Sentinel servers and master change monitoring task.
      */
+    @ConfigFiled(field = "serverMasterName")
     private String serverMasterName = "master";
 
     /**
      * The address of the redis server following format -- host1:port1,host2:port2,……
      */
+    @ConfigFiled(field = "serverAddress")
     private String serverAddress;
 
     /**
      * The password for redis authentication.
      */
+    @ConfigFiled(field = "serverPassword")
     private String serverPassword;
 
     /**
      * The redisson options, redisson properties, please refer to the redisson manual.
      */
+    @ConfigFiled(field = "redisson")
     private Properties redissonProperties;
 
     public ServerType getServerType() {
