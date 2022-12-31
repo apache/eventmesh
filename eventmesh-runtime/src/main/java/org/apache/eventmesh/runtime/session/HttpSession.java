762a3de0f7e796fb98f7b20152186a98b5078614
laohu <2372554140@qq.com>
2023-01-01 00:33:36 +0800

    fix(*):1. http ci/cd

--- eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/session/HttpSession.java
+++ eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/session/HttpSession.java
@@ -16,46 +16,13 @@
  */
 package org.apache.eventmesh.runtime.session;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.eventmesh.common.Constants;
-import org.apache.eventmesh.common.protocol.ProtocolTransportObject;
-import org.apache.eventmesh.common.protocol.SubscriptionType;
-import org.apache.eventmesh.common.protocol.http.HttpCommand;
-import org.apache.eventmesh.common.protocol.http.body.message.PushMessageRequestBody;
-import org.apache.eventmesh.common.protocol.http.common.ClientRetCode;
-import org.apache.eventmesh.common.protocol.http.common.ProtocolKey;
-import org.apache.eventmesh.common.protocol.http.common.ProtocolVersion;
-import org.apache.eventmesh.common.protocol.http.common.RequestCode;
-import org.apache.eventmesh.common.utils.IPUtils;
-import org.apache.eventmesh.common.utils.JsonUtils;
-import org.apache.eventmesh.common.utils.RandomStringUtils;
-import org.apache.eventmesh.protocol.api.ProtocolAdaptor;
-import org.apache.eventmesh.protocol.api.ProtocolPluginFactory;
-import org.apache.eventmesh.runtime.constants.EventMeshConstants;
 import org.apache.eventmesh.runtime.core.protocol.api.EventMeshRequest;
 import org.apache.eventmesh.runtime.core.protocol.api.EventMeshResponse;
-import org.apache.eventmesh.runtime.core.protocol.http.push.AsyncHTTPPushRequest;
-import org.apache.eventmesh.runtime.util.EventMeshUtil;
-import org.apache.eventmesh.runtime.util.WebhookUtil;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 import io.cloudevents.CloudEvent;
-import io.cloudevents.core.builder.CloudEventBuilder;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
@@ -78,22 +45,22 @@ public class HttpSession extends AbstractSession<Channel>{
 				@Override
 				public void success(EventMeshResponse eventMeshResponse) {
 					downstreamHandler.success(eventMeshResponse);
-					HttpSession.this.after();
+					HttpSession.this.after(event);
 				}
 				
 				@Override
 				public void fail(EventMeshResponse eventMeshResponse) {
 					downstreamHandler.fail(eventMeshResponse);
-					HttpSession.this.after();
+					HttpSession.this.after(event);
 				}
 				
 				@Override
 				public void exception(Throwable e) {
 					try {
 						downstreamHandler.exception(e);
-						HttpSession.this.exception(e);
+						HttpSession.this.exception(e,event);
 					}catch(Exception e1) {
-						HttpSession.this.exception(e1);
+						HttpSession.this.exception(e1,event);
 					}
 				}
 				
@@ -105,7 +72,7 @@ public class HttpSession extends AbstractSession<Channel>{
 			
 			DefaultFullHttpRequest request = new DefaultFullHttpRequest(HttpVersion.HTTP_1_0, HttpMethod.POST, "");
 			ChannelFuture channelFuture = this.channel.writeAndFlush(request);
-			this.before();
+			this.before(event);
 			channelFuture.addListener(new ChannelFutureListener() {
 	
 				@Override
@@ -113,22 +80,22 @@ public class HttpSession extends AbstractSession<Channel>{
 					if(future.isSuccess()) {
 						try {
 							downstreamHandler.downstreamSuccess();
-							HttpSession.this.after();
+							HttpSession.this.after(event);
 						}catch(Exception e) {
-							HttpSession.this.exception(e);
+							HttpSession.this.exception(e,event);
 						}
 					}else {
 						try {
 							downstreamHandler.exception(future.cause());
-							HttpSession.this.exception(future.cause());
+							HttpSession.this.exception(future.cause(),event);
 						}catch(Exception e) {
-							HttpSession.this.exception(e);
+							HttpSession.this.exception(e,event);
 						}
 					}
 				}
 			});
 		}catch(Exception e) {
-			this.exception(e);
+			this.exception(e,event);
 		}
 		
 	}
