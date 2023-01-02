e279288d6affdac8efa62ddb08da01e308787475
eight-nines <1748526511@qq.com>
2023-01-02 17:12:03 +0800

    Improve configuration management of Pulsar SPI Impl
    [1] PulsarConsumerImpl and PulsarProducerImpl config improve
    [2] PulsarConsumerImpl and PulsarProducerImpl config UT

--- eventmesh-connector-plugin/eventmesh-connector-pulsar/src/main/java/org/apache/eventmesh/connector/pulsar/config/ClientConfiguration.java
+++ eventmesh-connector-plugin/eventmesh-connector-pulsar/src/main/java/org/apache/eventmesh/connector/pulsar/config/ClientConfiguration.java
@@ -17,44 +17,23 @@
 
 package org.apache.eventmesh.connector.pulsar.config;
 
-import org.apache.commons.lang3.StringUtils;
-
-import com.google.common.base.Preconditions;
+import org.apache.eventmesh.common.config.Config;
+import org.apache.eventmesh.common.config.ConfigFiled;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Config(prefix = "eventMesh.server.pulsar", path = "classPath://pulsar-client.properties")
 public class ClientConfiguration {
 
+    @ConfigFiled(field = "service")
     private String serviceAddr;
+
+    @ConfigFiled(field = "authPlugin")
     private String authPlugin;
+
+    @ConfigFiled(field = "authParams")
     private String authParams;
-
-    private static ClientConfiguration INSTANCE = null;
-
-    public void init() {
-        String serviceAddrStr = ConfigurationWrapper.getProp(ConfKeys.KEYS_EVENTMESH_PULSAR_SERVICE_ADDR);
-        Preconditions.checkState(StringUtils.isNotEmpty(serviceAddrStr),
-                String.format("%s error", ConfKeys.KEYS_EVENTMESH_PULSAR_SERVICE_ADDR));
-        serviceAddr = StringUtils.trim(serviceAddrStr);
-        authPlugin = ConfigurationWrapper.getProp(ConfKeys.KEYS_EVENTMESH_PULSAR_AUTH_PLUGIN);
-        authParams = ConfigurationWrapper.getProp(ConfKeys.KEYS_EVENTMESH_PULSAR_AUTH_PARAMS);
-    }
-
-    public static ClientConfiguration getInstance() {
-        if (INSTANCE == null) {
-            INSTANCE = new ClientConfiguration();
-            INSTANCE.init();
-        }
-        return INSTANCE;
-    }
-
-    static class ConfKeys {
-        public static final String KEYS_EVENTMESH_PULSAR_SERVICE_ADDR = "eventMesh.server.pulsar.service";
-        public static final String KEYS_EVENTMESH_PULSAR_AUTH_PLUGIN = "eventMesh.server.pulsar.authPlugin";
-        public static final String KEYS_EVENTMESH_PULSAR_AUTH_PARAMS = "eventMesh.server.pulsar.authParams";
-    }
-
 }
