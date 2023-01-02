108252e5bf4976d709550cf28c3e4d69f363ea52
eight-nines <1748526511@qq.com>
2023-01-02 19:51:19 +0800

    Improve configuration management of AuthHttpBasic SPI Impl
    [1] AuthHttpBasicService config improve
    [2] AuthHttpBasicService config UT

--- eventmesh-security-plugin/eventmesh-security-auth-http-basic/src/test/java/org/apache/eventmesh/auth/http/basic/config/AuthConfigsTest.java
+++ eventmesh-security-plugin/eventmesh-security-auth-http-basic/src/test/java/org/apache/eventmesh/auth/http/basic/config/AuthConfigsTest.java
@@ -17,23 +17,26 @@
 
 package org.apache.eventmesh.auth.http.basic.config;
 
-import org.apache.eventmesh.common.config.Config;
-import org.apache.eventmesh.common.config.ConfigFiled;
+import org.apache.eventmesh.api.auth.AuthService;
+import org.apache.eventmesh.auth.http.basic.impl.AuthHttpBasicService;
+import org.apache.eventmesh.spi.EventMeshExtensionFactory;
 
-@Config(prefix = "auth", path = "classPath://auth-http-basic.properties")
-public class AuthConfigs {
+import org.junit.Assert;
+import org.junit.Test;
 
-    @ConfigFiled(field = "username")
-    public String username;
+public class AuthConfigsTest {
 
-    @ConfigFiled(field = "password")
-    public String password;
+    @Test
+    public void getConfigWhenAuthHttpBasicServiceInit() {
+        AuthHttpBasicService authService = (AuthHttpBasicService) EventMeshExtensionFactory.getExtension(
+                AuthService.class, "auth-http-basic");
 
-    public String getUsername() {
-        return username;
+        AuthConfigs config = authService.getClientConfiguration();
+        assertConfig(config);
     }
 
-    public String getPassword() {
-        return password;
+    private void assertConfig(AuthConfigs config) {
+        Assert.assertEquals(config.getUsername(), "username-success!!!");
+        Assert.assertEquals(config.getPassword(), "password-success!!!");
     }
 }
