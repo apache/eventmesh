f4d0e5457b7e7fcc2a948495aba9df219f11167a
nicolchen <409696597@qq.com>
2023-01-09 13:07:24 +0800

    extract getLocalAddress for single variable for reused. (#2840)

--- eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/core/protocol/http/processor/LocalSubscribeEventProcessor.java
+++ eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/core/protocol/http/processor/LocalSubscribeEventProcessor.java
@@ -63,11 +63,11 @@ public class LocalSubscribeEventProcessor extends AbstractEventProcessor {
 
         final Channel channel = handlerSpecific.getCtx().channel();
         final HttpEventWrapper requestWrapper = handlerSpecific.getAsyncContext().getRequest();
-
+        String localAddress = IPUtils.getLocalAddress();
         if (log.isInfoEnabled()) {
             log.info("uri={}|{}|client2eventMesh|from={}|to={}", requestWrapper.getRequestURI(),
                     EventMeshConstants.PROTOCOL_HTTP, RemotingHelper.parseChannelRemoteAddr(channel),
-                    IPUtils.getLocalAddress());
+                    localAddress);
         }
 
         // user request header
