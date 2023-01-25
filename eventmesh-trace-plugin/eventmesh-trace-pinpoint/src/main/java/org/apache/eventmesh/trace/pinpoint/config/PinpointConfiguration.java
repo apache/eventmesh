c3eb1908a45af5854481aa2e53d54e64c2b2cdec
jonyangx <jonyang1120@gmail.com>
2023-01-25 11:41:33 +0800

    refactor code

--- eventmesh-trace-plugin/eventmesh-trace-pinpoint/src/main/java/org/apache/eventmesh/trace/pinpoint/config/PinpointConfiguration.java
+++ eventmesh-trace-plugin/eventmesh-trace-pinpoint/src/main/java/org/apache/eventmesh/trace/pinpoint/config/PinpointConfiguration.java
@@ -25,14 +25,6 @@ import org.apache.eventmesh.common.utils.RandomStringUtils;
 
 import org.apache.commons.lang3.StringUtils;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.Optional;
 import java.util.Properties;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
