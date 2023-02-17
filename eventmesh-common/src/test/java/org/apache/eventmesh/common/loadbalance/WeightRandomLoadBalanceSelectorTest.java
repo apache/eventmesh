37e31e6cec49618f14e24a91b48eb0d386c47a83
dingrui <bannirui@outlook.com>
2023-02-17 08:46:19 +0800

    [ISSUE #3169] Replace `print` with `logger`

--- eventmesh-common/src/test/java/org/apache/eventmesh/common/loadbalance/WeightRandomLoadBalanceSelectorTest.java
+++ eventmesh-common/src/test/java/org/apache/eventmesh/common/loadbalance/WeightRandomLoadBalanceSelectorTest.java
@@ -34,7 +34,7 @@ import org.slf4j.LoggerFactory;
 
 public class WeightRandomLoadBalanceSelectorTest {
 
-    private Logger logger = LoggerFactory.getLogger(WeightRandomLoadBalanceSelectorTest.class);
+    private final Logger logger = LoggerFactory.getLogger(WeightRandomLoadBalanceSelectorTest.class);
 
     @Test
     public void testSelect() throws Exception {
@@ -46,13 +46,13 @@ public class WeightRandomLoadBalanceSelectorTest {
         Assert.assertEquals(LoadBalanceType.WEIGHT_RANDOM, weightRandomLoadBalanceSelector.getType());
         int testRange = 100_000;
         Map<String, Integer> addressToNum = IntStream.range(0, testRange)
-                .mapToObj(i -> weightRandomLoadBalanceSelector.select())
-                .collect(groupingBy(Function.identity(), summingInt(i -> 1)));
+            .mapToObj(i -> weightRandomLoadBalanceSelector.select())
+            .collect(groupingBy(Function.identity(), summingInt(i -> 1)));
 
         addressToNum.forEach((key, value) -> {
             logger.info("{}: {}", key, value);
         });
-        System.out.printf(addressToNum.toString());
+        logger.info("addressToNum: {}", addressToNum);
         // the error less than 5%
         Assert.assertTrue(Math.abs(addressToNum.get("192.168.0.3") - addressToNum.get("192.168.0.2") * 2) < testRange / 20);
         Assert.assertTrue(Math.abs(addressToNum.get("192.168.0.3") - addressToNum.get("192.168.0.1") * 4) < testRange / 20);
@@ -69,8 +69,8 @@ public class WeightRandomLoadBalanceSelectorTest {
 
         int testRange = 100_000;
         Map<String, Integer> addressToNum = IntStream.range(0, testRange)
-                .mapToObj(i -> weightRandomLoadBalanceSelector.select())
-                .collect(groupingBy(Function.identity(), summingInt(i -> 1)));
+            .mapToObj(i -> weightRandomLoadBalanceSelector.select())
+            .collect(groupingBy(Function.identity(), summingInt(i -> 1)));
 
         Field field = WeightRandomLoadBalanceSelector.class.getDeclaredField("sameWeightGroup");
         field.setAccessible(true);
