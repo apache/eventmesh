4530414a96f7defb46153a25fce48a1e6957c4fe
Dimuthu Weerathunga <dimu.wa.98@gmail.com>
2023-02-20 00:16:38 +0530

    [ISSUE #3168] IO streams closed, encoding set to UTF_8

--- eventmesh-common/src/test/java/org/apache/eventmesh/common/file/WatchFileManagerTest.java
+++ eventmesh-common/src/test/java/org/apache/eventmesh/common/file/WatchFileManagerTest.java
@@ -22,8 +22,6 @@ import org.apache.eventmesh.common.utils.ThreadUtils;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
