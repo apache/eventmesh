5833cff4a092f5881e3e5142cbb22217117faf13
dingrui <bannirui@outlook.com>
2023-02-20 14:12:05 +0800

    [ISSUE #3157] Using placeholder

--- eventmesh-runtime/src/test/java/org/apache/eventmesh/runtime/demo/SyncSubClient.java
+++ eventmesh-runtime/src/test/java/org/apache/eventmesh/runtime/demo/SyncSubClient.java
@@ -36,7 +36,7 @@ public class SyncSubClient {
 
     public static void main(String[] args) throws Exception {
         try (SubClientImpl client =
-                     new SubClientImpl("localhost", 10000, MessageUtils.generateSubServer())) {
+            new SubClientImpl("localhost", 10000, MessageUtils.generateSubServer())) {
             client.init();
             client.heartbeat();
             client.justSubscribe(ClientConstants.SYNC_TOPIC, SubscriptionMode.CLUSTERING, SubscriptionType.SYNC);
@@ -45,7 +45,7 @@ public class SyncSubClient {
                 public void handle(Package msg, ChannelHandlerContext ctx) {
                     if (msg.getHeader().getCommand() == Command.REQUEST_TO_CLIENT) {
                         if ((log.isInfoEnabled())) {
-                            log.info("receive message -------------------------------" + msg);
+                            log.info("receive message: {}", msg);
                         }
                     }
                 }
