1f8013e60f7d1496f85b05e846cf6ec89b8ba87c
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2023-04-06 21:17:55 +0530

    Update Trace.java

--- eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/trace/Trace.java
+++ eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/trace/Trace.java
@@ -24,6 +24,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.Objects;
 
 import io.cloudevents.CloudEvent;
 import io.netty.channel.ChannelHandlerContext;
