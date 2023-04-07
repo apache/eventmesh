6778cdd04250e6959a77741f6735815076896e30
Harshitha Sudhakar <111514477+harshithasudhakar@users.noreply.github.com>
2023-04-07 21:28:17 +0530

    Update ClientConfigurationTest.java
    Swapped actual and expected values at lines 46-48.

--- eventmesh-storage-plugin/eventmesh-storage-pulsar/src/test/java/org/apache/eventmesh/storage/pulsar/config/ClientConfigurationTest.java
+++ eventmesh-storage-plugin/eventmesh-storage-pulsar/src/test/java/org/apache/eventmesh/storage/pulsar/config/ClientConfigurationTest.java
@@ -43,8 +43,8 @@ public class ClientConfigurationTest {
     }
 
     private void assertConfig(ClientConfiguration config) {
-        Assert.assertEquals(config.getServiceAddr(), "127.0.0.1:6650");
-        Assert.assertEquals(config.getAuthPlugin(), "authPlugin-success!!!");
-        Assert.assertEquals(config.getAuthParams(), "authParams-success!!!");
+        Assert.assertEquals("127.0.0.1:6650", config.getServiceAddr());
+        Assert.assertEquals("authPlugin-success!!!", config.getAuthPlugin());
+        Assert.assertEquals("authParams-success!!!", config.getAuthParams());
     }
 }
