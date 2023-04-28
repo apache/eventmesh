9a0d5fa46b836a230d83f516103b5d5e98e814f0
pandaapo <1052156701@qq.com>
2023-04-28 21:47:08 +0800

    Simplify code.

--- eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/core/protocol/tcp/client/task/UnSubscribeTask.java
+++ eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/core/protocol/tcp/client/task/UnSubscribeTask.java
@@ -17,33 +17,31 @@
 
 package org.apache.eventmesh.runtime.core.protocol.tcp.client.task;
 
+import static org.apache.eventmesh.common.protocol.tcp.Command.UNSUBSCRIBE_RESPONSE;
+import static org.apache.eventmesh.common.protocol.tcp.OPStatus.FAIL;
+import static org.apache.eventmesh.common.protocol.tcp.OPStatus.SUCCESS;
+import static org.apache.eventmesh.runtime.constants.EventMeshConstants.MESSAGE;
+
 import org.apache.eventmesh.common.protocol.SubscriptionItem;
-import org.apache.eventmesh.common.protocol.tcp.Command;
 import org.apache.eventmesh.common.protocol.tcp.Header;
-import org.apache.eventmesh.common.protocol.tcp.OPStatus;
 import org.apache.eventmesh.common.protocol.tcp.Package;
 import org.apache.eventmesh.runtime.boot.EventMeshTCPServer;
-import org.apache.eventmesh.runtime.constants.EventMeshConstants;
 import org.apache.eventmesh.runtime.util.Utils;
 
 import org.apache.commons.collections4.MapUtils;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.netty.channel.ChannelHandlerContext;
 
-
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class UnSubscribeTask extends AbstractTask {
 
-    private static final Logger MESSAGE_LOGGER = LoggerFactory.getLogger(EventMeshConstants.MESSAGE);
+    private static final Logger MESSAGE_LOGGER = LoggerFactory.getLogger(MESSAGE);
 
     public UnSubscribeTask(Package pkg, ChannelHandlerContext ctx, long startTime, EventMeshTCPServer eventMeshTCPServer) {
         super(pkg, ctx, startTime, eventMeshTCPServer);
@@ -55,20 +53,18 @@ public class UnSubscribeTask extends AbstractTask {
         Package msg = new Package();
         try {
             synchronized (session) {
-                List<SubscriptionItem> topics = new ArrayList<SubscriptionItem>();
-                if (MapUtils.isNotEmpty(session.getSessionContext().getSubscribeTopics())) {
-                    for (Map.Entry<String, SubscriptionItem> entry : session.getSessionContext().getSubscribeTopics().entrySet()) {
-                        topics.add(entry.getValue());
-                    }
+                ConcurrentHashMap<String, SubscriptionItem> subscribeTopics = session.getSessionContext().getSubscribeTopics();
+                if (MapUtils.isNotEmpty(subscribeTopics)) {
+                    List<SubscriptionItem> topics = new ArrayList<>(subscribeTopics.values());
                     session.unsubscribe(topics);
                     MESSAGE_LOGGER.info("UnSubscriberTask succeed|user={}|topics={}", session.getClient(), topics);
                 }
             }
-            msg.setHeader(new Header(Command.UNSUBSCRIBE_RESPONSE, OPStatus.SUCCESS.getCode(), OPStatus.SUCCESS.getDesc(), pkg.getHeader()
+            msg.setHeader(new Header(UNSUBSCRIBE_RESPONSE, SUCCESS.getCode(), SUCCESS.getDesc(), pkg.getHeader()
                 .getSeq()));
         } catch (Exception e) {
             MESSAGE_LOGGER.error("UnSubscribeTask failed|user={}|errMsg={}", session.getClient(), e);
-            msg.setHeader(new Header(Command.UNSUBSCRIBE_RESPONSE, OPStatus.FAIL.getCode(), "exception while "
+            msg.setHeader(new Header(UNSUBSCRIBE_RESPONSE, FAIL.getCode(), "exception while "
                 +
                 "unSubscribing", pkg.getHeader().getSeq()));
         } finally {
