22991e0ef369e5d609cdcfe00840b4b93c471e2d
Piyush <118425491+piyush280599@users.noreply.github.com>
2023-05-04 07:15:24 +0530

    [ISSUE #3013] Updated ConfigurationHandler.java  (#3777)
    * Updated ConfigurationHandler.java #3013
    
    * Update ConfigurationHandler.java
    
    * Update ConfigurationHandler.java
    
    ---------
    
    Co-authored-by: mike_xwm <mike_xwm@126.com>

--- eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/admin/handler/ConfigurationHandler.java
+++ eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/admin/handler/ConfigurationHandler.java
@@ -17,6 +17,7 @@
 
 package org.apache.eventmesh.runtime.admin.handler;
 
+import org.apache.eventmesh.common.Constants;
 import org.apache.eventmesh.common.utils.JsonUtils;
 import org.apache.eventmesh.runtime.admin.controller.HttpHandlerManager;
 import org.apache.eventmesh.runtime.admin.response.Error;
@@ -102,8 +103,8 @@ public class ConfigurationHandler extends AbstractHttpHandler {
                 );
 
                 String result = JsonUtils.toJSONString(getConfigurationResponse);
-                httpExchange.sendResponseHeaders(200, result.getBytes().length);
-                out.write(result.getBytes());
+                httpExchange.sendResponseHeaders(200, result.getBytes(Constants.DEFAULT_CHARSET).length);
+                out.write(result.getBytes(Constants.DEFAULT_CHARSET));
             } catch (Exception e) {
                 StringWriter writer = new StringWriter();
                 PrintWriter printWriter = new PrintWriter(writer);
@@ -113,8 +114,8 @@ public class ConfigurationHandler extends AbstractHttpHandler {
 
                 Error error = new Error(e.toString(), stackTrace);
                 String result = JsonUtils.toJSONString(error);
-                httpExchange.sendResponseHeaders(500, result.getBytes().length);
-                out.write(result.getBytes());
+                httpExchange.sendResponseHeaders(500, result.getBytes(Constants.DEFAULT_CHARSET).length);
+                out.write(result.getBytes(Constants.DEFAULT_CHARSET));
             }
         }
     }
