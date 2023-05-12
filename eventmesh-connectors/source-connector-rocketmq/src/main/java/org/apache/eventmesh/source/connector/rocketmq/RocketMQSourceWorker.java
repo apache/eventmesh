5a9dd94100117c17262956511a3bbd700dc7f044
mike_xwm <mike_xwm@126.com>
2023-05-12 17:34:29 +0800

    Update RocketMQSourceWorker.java

--- eventmesh-connectors/source-connector-rocketmq/src/main/java/org/apache/eventmesh/source/connector/rocketmq/RocketMQSourceWorker.java
+++ eventmesh-connectors/source-connector-rocketmq/src/main/java/org/apache/eventmesh/source/connector/rocketmq/RocketMQSourceWorker.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.eventmesh.source.connector.rocketmq;
 
 import io.cloudevents.CloudEvent;
@@ -48,7 +65,7 @@ public class RocketMQSourceWorker {
         while(true) {
             List<ConnectRecord> connectorRecordList = rocketMQSourceConnector.poll();
             for(ConnectRecord connectRecord : connectorRecordList) {
-                // todo:connectorRecord 转换 cloudEvents
+                // todo:connectorRecord convert cloudEvents
                 CloudEvent event = EventMeshTestUtils.generateCloudEventV1(connectRecord.getExtension("topic"), connectRecord.getData().toString());
                 client.publish(event, 3000);
                 Thread.sleep(500);
