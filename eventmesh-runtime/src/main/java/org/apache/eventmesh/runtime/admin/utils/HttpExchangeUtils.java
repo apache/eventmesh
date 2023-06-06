b476c8dd252a7b012e2a168455dbba72218ac511
kyooosukedn <danielpurnomo50@gmail.com>
2023-06-06 11:54:27 +0200

    used try-with-resources to manage resources

--- eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/admin/utils/HttpExchangeUtils.java
+++ eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/admin/utils/HttpExchangeUtils.java
@@ -21,22 +21,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.Buffer;
 import java.nio.charset.StandardCharsets;
 
 public class HttpExchangeUtils {
 
     public static String streamToString(InputStream stream) throws IOException {
-        InputStreamReader isr = new InputStreamReader(stream, StandardCharsets.UTF_8);
-        BufferedReader bufferedReader = new BufferedReader(isr);
+        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))) {
 
-        int b;
-        StringBuilder buffer = new StringBuilder();
-        while ((b = bufferedReader.read()) != -1) {
-            buffer.append((char) b);
+            StringBuilder buffer = new StringBuilder();
+            int b;
+
+            while ((b = bufferedReader.read()) != -1) {
+                buffer.append((char) b);
+            }
+
+            return buffer.toString();
         }
-
-        bufferedReader.close();
-        isr.close();
-        return buffer.toString();
     }
 }
