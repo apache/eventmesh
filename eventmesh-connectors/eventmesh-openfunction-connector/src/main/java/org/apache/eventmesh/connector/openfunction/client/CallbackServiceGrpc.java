41adba308f5dd898dbcf49149c4f0160d520c0e8
HattoriHenzo <esombugma@gmail.com>
2023-06-16 18:12:14 -0400

    [Enhancement] Anonymous new can be replaced with lambda.[AsyncPubClient]

--- eventmesh-connectors/eventmesh-openfunction-connector/src/main/java/org/apache/eventmesh/connector/openfunction/client/CallbackServiceGrpc.java
+++ eventmesh-connectors/eventmesh-openfunction-connector/src/main/java/org/apache/eventmesh/connector/openfunction/client/CallbackServiceGrpc.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.eventmesh.connector.openfunction.client;
 
 import static io.grpc.MethodDescriptor.generateFullMethodName;
@@ -7,7 +24,7 @@ import static io.grpc.stub.ClientCalls.futureUnaryCall;
 import static io.grpc.stub.ServerCalls.asyncUnaryCall;
 import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
 
-import org.apache.eventmesh.connect.openfunction.EventMeshGrpcService;
+import org.apache.eventmesh.common.protocol.grpc.cloudevents.CloudEvent;
 
 /**
  *
@@ -24,16 +41,16 @@ public final class CallbackServiceGrpc {
 
     // Static method descriptors that strictly reflect the proto.
     @io.grpc.ExperimentalApi("https://github.com/grpc/grpc-java/issues/1901")
-    public static final io.grpc.MethodDescriptor<org.apache.eventmesh.common.protocol.grpc.cloudevents.CloudEvent,
+    public static final io.grpc.MethodDescriptor<CloudEvent,
         org.apache.eventmesh.common.protocol.grpc.cloudevents.CloudEvent> METHOD_ON_TOPIC_EVENT =
-        io.grpc.MethodDescriptor.<org.apache.eventmesh.common.protocol.grpc.cloudevents.CloudEvent, org.apache.eventmesh.common.protocol.grpc.cloudevents.CloudEvent>newBuilder()
+        io.grpc.MethodDescriptor.<CloudEvent, CloudEvent>newBuilder()
             .setType(io.grpc.MethodDescriptor.MethodType.UNARY)
             .setFullMethodName(generateFullMethodName(
                 "org.apache.eventmesh.cloudevents.v1.CallbackService", "OnTopicEvent"))
             .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
-                org.apache.eventmesh.common.protocol.grpc.cloudevents.CloudEvent.getDefaultInstance()))
+                CloudEvent.getDefaultInstance()))
             .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
-                org.apache.eventmesh.common.protocol.grpc.cloudevents.CloudEvent.getDefaultInstance()))
+                CloudEvent.getDefaultInstance()))
             .build();
 
     /**
