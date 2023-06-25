e3011e38284563331b2fffcdfd71f6bd55ccf3b1
dipanker <dipanker_kr@outlook.com>
2023-06-25 23:02:09 +0530

    #4107 made fields final
    Signed-off-by: dipanker <dipanker_kr@outlook.com>

--- eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/core/protocol/tcp/client/session/push/DownStreamMsgContext.java
+++ eventmesh-runtime/src/main/java/org/apache/eventmesh/runtime/core/protocol/tcp/client/session/push/DownStreamMsgContext.java
@@ -47,12 +47,12 @@ public class DownStreamMsgContext extends RetryContext {
     @Setter
     private Session session;
 
-    private AbstractContext consumeConcurrentlyContext;
+    private final AbstractContext consumeConcurrentlyContext;
 
-    private MQConsumerWrapper consumer;
+    private final MQConsumerWrapper consumer;
 
     @Getter
-    private SubscriptionItem subscriptionItem;
+    private final SubscriptionItem subscriptionItem;
 
     private long lastPushTime;
 
@@ -60,7 +60,7 @@ public class DownStreamMsgContext extends RetryContext {
 
     private final long expireTime;
 
-    public boolean msgFromOtherEventMesh;
+    public final boolean msgFromOtherEventMesh;
 
     public DownStreamMsgContext(CloudEvent event, Session session, MQConsumerWrapper consumer,
         AbstractContext consumeConcurrentlyContext, boolean msgFromOtherEventMesh,
