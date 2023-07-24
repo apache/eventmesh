22abd32525e46135636be25e9f143a3ba7ce71e0
Pil0tXia <admin@pil0txia.com>
2023-07-25 00:41:11 +0800

    Fix: CI report reference not found

--- eventmesh-webhook/eventmesh-webhook-api/src/main/java/org/apache/eventmesh/webhook/api/WebHookConfigOperation.java
+++ eventmesh-webhook/eventmesh-webhook-api/src/main/java/org/apache/eventmesh/webhook/api/WebHookConfigOperation.java
@@ -27,10 +27,9 @@ import java.util.List;
  * They correspond to the persistent configuration of {@linkplain org.apache.eventmesh.webhook.api.WebHookConfig WebHookConfig}
  * for {@code file} and {@code Nacos}, respectively.
  * <p>
- * However, the {@code HookConfigOperationManager}, which is located in the {@link org.apache.eventmesh.webhook.receive.storage}
- * package, differs from the other two implementations which are located in the {@link org.apache.eventmesh.webhook.admin}
- * package. Refer to {@code org.apache.eventmesh.runtime.admin.handler.QueryWebHookConfigByIdHandler}
- * for the reasons and details.
+ * However, the {@code HookConfigOperationManager}, which is located in the {@code org.apache.eventmesh.webhook.receive.storage}
+ * package, differs from the other two implementations which are located in the {@code org.apache.eventmesh.webhook.admin}
+ * package. Refer to {@code QueryWebHookConfigByIdHandler} for the reasons and details.
  */
 public interface WebHookConfigOperation {
 
