da4c20549c48f1909f58102708e8b2dbfb4affd2
kyooosukedn <danielpurnomo50@gmail.com>
2023-07-26 12:27:19 +0200

    Removed catch block

--- eventmesh-common/src/test/java/org/apache/eventmesh/common/file/WatchFileManagerTest.java
+++ eventmesh-common/src/test/java/org/apache/eventmesh/common/file/WatchFileManagerTest.java
@@ -58,14 +58,11 @@ public class WatchFileManagerTest {
         WatchFileManager.registerFileChangeListener(f.getParent(), fileChangeListener);
 
         try (final BufferedReader reader = new BufferedReader(new FileReader(file));
-            final FileWriter fw = new FileWriter(file)) {
-            Properties properties = new Properties();
-            properties.load(reader);
-            properties.setProperty("eventMesh.server.newAdd", "newAdd");
-            properties.store(fw, "newAdd");
-        }
-        catch (IOException e) {
-            LOGGER.error("WatchFileManagerTest caught exception: " + e.getMessage(), e);
+             final FileWriter fw = new FileWriter(file)) {
+             Properties properties = new Properties();
+             properties.load(reader);
+             properties.setProperty("eventMesh.server.newAdd", "newAdd");
+             properties.store(fw, "newAdd");
         }
 
         ThreadUtils.sleep(500, TimeUnit.MILLISECONDS);
