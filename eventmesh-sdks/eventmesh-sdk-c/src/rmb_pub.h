c9808bc6d425ef008208b442b15904bbe7cf91d4
zhurq <rainzrq@qq.com>
2023-08-04 15:45:35 +0800

    license

--- eventmesh-sdks/eventmesh-sdk-c/src/rmb_pub.h
+++ eventmesh-sdks/eventmesh-sdk-c/src/rmb_pub.h
@@ -1,9 +1,23 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * rmb_pub.h
  * ---rmb的发包管理，包括发送广播、RR模式等
- *
- * History:
- * 	2014-11-06	RR模式、发topic包
  */
 #ifndef RMB_PUB_H_
 #define RMB_PUB_H_
--- eventmesh-sdks/eventmesh-sdk-c/src/wemq_fifo.c
+++ eventmesh-sdks/eventmesh-sdk-c/src/wemq_fifo.c
@@ -1,33 +1,20 @@
 /*
- * A generic kernel FIFO implementation
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Copyright (C) 2009/2010 Stefani Seibold <stefani@seibold.net>
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-/*
-#include <linux/kernel.h>
-#include <linux/export.h>
-#include <linux/slab.h>
-#include <linux/err.h>
-#include <linux/log2.h>
-#include <linux/uaccess.h>
-#include <linux/wemq_kfifo.h>
-*/
-//#include "wemq_fifo.h"
+
 #include "rmb_define.h"
 #define min(x,y) ({\
 				                typeof(x) _x = (x);\
