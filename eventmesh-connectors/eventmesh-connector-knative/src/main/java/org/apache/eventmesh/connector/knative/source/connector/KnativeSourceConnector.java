0b24d3da2316429a642cb97779aae9f636f372de
fabian4 <fabian4@163.com>
2023-09-05 16:34:18 +0800

    feat: add eventmesh-connector-knative.

--- eventmesh-connectors/eventmesh-connector-knative/src/main/java/org/apache/eventmesh/connector/knative/source/connector/KnativeSourceConnector.java
+++ eventmesh-connectors/eventmesh-connector-knative/src/main/java/org/apache/eventmesh/connector/knative/source/connector/KnativeSourceConnector.java
@@ -17,31 +17,15 @@
 
 package org.apache.eventmesh.connector.knative.source.connector;
 
-import static org.asynchttpclient.Dsl.asyncHttpClient;
-
-import org.apache.eventmesh.common.ThreadPoolFactory;
 import org.apache.eventmesh.connector.knative.source.config.KnativeSourceConfig;
 import org.apache.eventmesh.openconnect.api.config.Config;
 import org.apache.eventmesh.openconnect.api.connector.ConnectorContext;
 import org.apache.eventmesh.openconnect.api.connector.SourceConnectorContext;
 import org.apache.eventmesh.openconnect.api.source.Source;
 import org.apache.eventmesh.openconnect.offsetmgmt.api.data.ConnectRecord;
-import org.apache.eventmesh.openconnect.util.CloudEventUtil;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
-
-import org.asynchttpclient.AsyncHttpClient;
-import org.asynchttpclient.ListenableFuture;
-import org.asynchttpclient.Response;
-import org.asynchttpclient.util.HttpConstants;
-
-import io.cloudevents.CloudEvent;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -51,15 +35,6 @@ public class KnativeSourceConnector implements Source {
     private KnativeSourceConfig sourceConfig;
 
     private static final AtomicBoolean started = new AtomicBoolean(false);
-
-    private static final int DEFAULT_BATCH_SIZE = 10;
-
-    private BlockingQueue<CloudEvent> queue;
-
-    private final ThreadPoolExecutor executor = ThreadPoolFactory.createThreadPoolExecutor(
-            Runtime.getRuntime().availableProcessors() * 2,
-            Runtime.getRuntime().availableProcessors() * 2,
-            "EventMesh-RabbitMQSourceConnector-");
 
     @Override
     public Class<? extends Config> configClass() {
@@ -74,13 +49,11 @@ public class KnativeSourceConnector implements Source {
     public void init(ConnectorContext connectorContext) throws Exception {
         SourceConnectorContext sourceConnectorContext = (SourceConnectorContext) connectorContext;
         this.sourceConfig = (KnativeSourceConfig) sourceConnectorContext.getSourceConfig();
-        this.queue = new LinkedBlockingQueue<>(1000);
     }
 
     @Override
     public void start() throws Exception {
         started.compareAndSet(false, true);
-        executor.execute(new KnativeSourceHandler(this.sourceConfig));
     }
 
     @Override
@@ -99,54 +72,8 @@ public class KnativeSourceConnector implements Source {
 
     @Override
     public List<ConnectRecord> poll() {
-        List<ConnectRecord> connectRecords = new ArrayList<>(DEFAULT_BATCH_SIZE);
-        for (int count = 0; count < DEFAULT_BATCH_SIZE; ++count) {
-            try {
-                CloudEvent event = queue.poll(3, TimeUnit.SECONDS);
-                if (event == null) {
-                    break;
-                }
-
-                connectRecords.add(CloudEventUtil.convertEventToRecord(event));
-            } catch (InterruptedException e) {
-                break;
-            }
-        }
-        return connectRecords;
-    }
-
-    public class KnativeSourceHandler implements Runnable {
-
-        private final KnativeSourceConfig sourceConfig;
-
-        private final AtomicBoolean running = new AtomicBoolean(true);
-
-        private final transient AsyncHttpClient asyncHttpClient = asyncHttpClient();
-
-        public KnativeSourceHandler(KnativeSourceConfig sourceConfig) {
-            this.sourceConfig = sourceConfig;
-        }
-
-        @Override
-        public void run() {
-            while (running.get()) {
-                try {
-                    ListenableFuture<Response> execute = asyncHttpClient.prepareGet("http://" + sourceConfig.getConnectorConfig().getServiceAddr() + "/" + "*").execute();
-                    Response response = execute.get(10, TimeUnit.SECONDS);
-
-                    if (response.getStatusCode() != HttpConstants.ResponseStatusCodes.OK_200) {
-                        log.error("HTTP response code error: " + response.getStatusCode());
-                    }
-                    String responseBody = response.getResponseBody();
-
-                } catch (Exception ex) {
-                    log.error("[KnativeSourceHandler] thread run happen exception.", ex);
-                }
-            }
-        }
-
-        public void stop() {
-            running.compareAndSet(true, false);
-        }
+        // todo: create a sink service and expose a public endpoint to Knative broker
+        // https://knative.dev/docs/eventing/sinks/
+        return null;
     }
 }
