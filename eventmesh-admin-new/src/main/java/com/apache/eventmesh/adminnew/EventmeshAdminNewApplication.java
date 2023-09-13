06fe20c17e5f921e08c4de8732a75162da580d8c
Pil0tXia <admin@pil0txia.com>
2023-09-13 18:34:42 +0800

    init: Spring Initializr

---
+++ eventmesh-admin-new/src/main/java/com/apache/eventmesh/adminnew/EventmeshAdminNewApplication.java
@@ -0,0 +1,13 @@
+package com.apache.eventmesh.adminnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventmeshAdminNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventmeshAdminNewApplication.class, args);
+    }
+
+}
---
+++ eventmesh-admin-new/src/test/java/com/apache/eventmesh/adminnew/EventmeshAdminNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apache.eventmesh.adminnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventmeshAdminNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
