35b7182d5d1ec78acf6c852545bd2389c43d0004
yanrongzhen <bruceyan@didiglobal.com>
2023-09-21 23:36:01 +0800

    fix some styles.

--- eventmesh-storage-plugin/eventmesh-storage-rocketmq/src/main/java/org/apache/eventmesh/storage/rocketmq/producer/AbstractProducer.java
+++ eventmesh-storage-plugin/eventmesh-storage-rocketmq/src/main/java/org/apache/eventmesh/storage/rocketmq/producer/AbstractProducer.java
@@ -102,26 +102,21 @@ public abstract class AbstractProducer {
                 if (e.getCause() instanceof RemotingTimeoutException) {
                     return new RMQTimeoutException(
                         String.format("Send message to broker timeout, %dms, Topic=%s, msgId=%s",
-                            this.rocketmqProducer.getSendMsgTimeout(), topic, msgId),
-                        e);
+                            this.rocketmqProducer.getSendMsgTimeout(), topic, msgId), e);
                 } else if (e.getCause() instanceof MQBrokerException
                     || e.getCause() instanceof RemotingConnectException) {
                     if (e.getCause() instanceof MQBrokerException) {
                         MQBrokerException brokerException = (MQBrokerException) e.getCause();
-                        return new StorageRuntimeException(
-                            String.format("Received a broker exception, Topic=%s, msgId=%s, %s",
-                                topic, msgId, brokerException.getErrorMessage()),
-                            e);
+                        return new StorageRuntimeException(String.format("Received a broker exception, Topic=%s, msgId=%s, %s",
+                                topic, msgId, brokerException.getErrorMessage()), e);
                     }
 
                     if (e.getCause() instanceof RemotingConnectException) {
                         RemotingConnectException connectException =
                             (RemotingConnectException) e.getCause();
                         return new StorageRuntimeException(
-                            String.format(
-                                "Network connection experiences failures. Topic=%s, msgId=%s, %s",
-                                topic, msgId, connectException.getMessage()),
-                            e);
+                            String.format("Network connection experiences failures. Topic=%s, msgId=%s, %s",
+                                topic, msgId, connectException.getMessage()), e);
                     }
                 }
             } else {
@@ -135,8 +130,7 @@ public abstract class AbstractProducer {
                 } else if (ResponseCode.MESSAGE_ILLEGAL == clientException.getResponseCode()) {
                     return new RMQMessageFormatException(
                         String.format("A illegal message for RocketMQ, Topic=%s, msgId=%s",
-                            topic, msgId),
-                        e);
+                            topic, msgId), e);
                 }
             }
         }
