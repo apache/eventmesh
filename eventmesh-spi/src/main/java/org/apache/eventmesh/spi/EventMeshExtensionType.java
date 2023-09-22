dc3553fc299b839a060edb9b2aeedce5eff0c055
Pil0tXia <41445332+Pil0tXia@users.noreply.github.com>
2023-09-22 18:24:22 +0800

    [ISSUE #4448] Fix meta plugin dist startup failure and upgrade gradle to 8.2.1 (#4449)
    * fix: Uniform meta pluginType naming
    
    * dep: upgrade gradle wrapper to 8.2.1

--- eventmesh-spi/src/main/java/org/apache/eventmesh/spi/EventMeshExtensionType.java
+++ eventmesh-spi/src/main/java/org/apache/eventmesh/spi/EventMeshExtensionType.java
@@ -24,7 +24,7 @@ public enum EventMeshExtensionType {
     UNKNOWN("unknown"),
     CONNECTOR("connector"),
     STORAGE("storage"),
-    META("meta"),
+    META("metaStorage"),
     SECURITY("security"),
     PROTOCOL("protocol"),
     METRICS("metrics"),
