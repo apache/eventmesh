26713fc79c4f2beeae64d991b550728ab67e1526
Roshan Kumar <132130039+roshan8084@users.noreply.github.com>
2023-10-05 08:22:17 +0530

    Update BroadCastSubClient.java

--- eventmesh-runtime/src/test/java/org/apache/eventmesh/runtime/demo/BroadCastSubClient.java
+++ eventmesh-runtime/src/test/java/org/apache/eventmesh/runtime/demo/BroadCastSubClient.java
@@ -21,7 +21,6 @@ import org.apache.eventmesh.common.protocol.SubscriptionMode;
 import org.apache.eventmesh.common.protocol.SubscriptionType;
 import org.apache.eventmesh.common.protocol.tcp.Command;
 import org.apache.eventmesh.common.protocol.tcp.EventMeshMessage;
-import org.apache.eventmesh.common.protocol.tcp.Package;
 import org.apache.eventmesh.runtime.client.common.ClientConstants;
 import org.apache.eventmesh.runtime.client.common.MessageUtils;
 import org.apache.eventmesh.runtime.client.hook.ReceiveMsgHook;
@@ -39,20 +38,17 @@ public class BroadCastSubClient {
             client.init();
             client.heartbeat();
             client.justSubscribe(ClientConstants.BROADCAST_TOPIC, SubscriptionMode.BROADCASTING, SubscriptionType.ASYNC);
-            client.registerBusiHandler(new ReceiveMsgHook() {
 
-                @Override
-                public void handle(Package msg, ChannelHandlerContext ctx) {
-                    if (msg.getHeader().getCommand() == Command.BROADCAST_MESSAGE_TO_CLIENT) {
-                        if (msg.getBody() instanceof EventMeshMessage) {
-                            String body = ((EventMeshMessage) msg.getBody()).getBody();
-                            if (log.isInfoEnabled()) {
-                                log.info("receive message -------------------------------" + body);
-                            }
+            client.registerBusiHandler((msg, ctx) -> {
+                if (msg.getHeader().getCommand() == Command.BROADCAST_MESSAGE_TO_CLIENT) {
+                    if (msg.getBody() instanceof EventMeshMessage) {
+                        String body = ((EventMeshMessage) msg.getBody()).getBody();
+                        if ((log.isInfoEnabled())) {
+                            log.info("receive message:{}",body);
                         }
                     }
                 }
-            });
+             });
         }
     }
 }
