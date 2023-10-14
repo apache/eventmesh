34cd378f5794390d470bd29d5c688dd874684fa5
Allon Mureinik <allon@synopsys.com>
2023-10-14 10:47:48 +0300

    Fix EventMeshGrpcProducerTest#testPublishWithException
    Although EventMeshGrpcProducerTest#testPublishWithException passes,
    it's wrong to do so.
    This tests calls the EventMeshGrpcProducer#produce method with a valid
    EventMeshMessage instance, so it does not, in fact, throw an error.
    The test then doesn't fail on the following line. Since no exception
    is ever thrown, the catch block is never reached, and the assertion in
    it is never called.
    
    This patch fixes the test by passing a truly invalid message (just a
    string), and adds an explicit failure if an exception is not called.

--- eventmesh-sdks/eventmesh-sdk-java/src/test/java/org/apache/eventmesh/client/grpc/producer/EventMeshGrpcProducerTest.java
+++ eventmesh-sdks/eventmesh-sdk-java/src/test/java/org/apache/eventmesh/client/grpc/producer/EventMeshGrpcProducerTest.java
@@ -18,6 +18,7 @@
 package org.apache.eventmesh.client.grpc.producer;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.argThat;
@@ -94,7 +95,8 @@ public class EventMeshGrpcProducerTest {
     @Test
     public void testPublishWithException() {
         try {
-            producer.publish(defaultEventMeshMessageBuilder().content("mockExceptionContent").build());
+            producer.publish("Not a supported message");
+            failBecauseExceptionWasNotThrown(IllegalArgumentException.class);
         } catch (Exception e) {
             assertThat(e).isNotNull();
         }
