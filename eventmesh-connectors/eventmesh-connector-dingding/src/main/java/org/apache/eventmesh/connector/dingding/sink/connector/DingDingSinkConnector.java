fc900e57dcd1a057931c03ae90038384e2776695
yanrongzhen <bruceyan@didiglobal.com>
2023-11-16 10:19:34 +0800

    [ISSUE #4411] Add DingDing sink connnector. (#4557)
    * Add dingding sink connector.
    
    * fix: Remove unused config.
    
    * fix: Remove unused config.
    
    * fix: review
    
    * Add example config.
    
    * fix: review
    
    * fix: continue when record data is null.

---
+++ eventmesh-connectors/eventmesh-connector-dingding/src/main/java/org/apache/eventmesh/connector/dingding/sink/connector/DingDingSinkConnector.java
@@ -0,0 +1,193 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.eventmesh.connector.dingding.sink.connector;
+
+import org.apache.eventmesh.common.utils.JsonUtils;
+import org.apache.eventmesh.connector.dingding.common.constants.ConnectRecordExtensionKeys;
+import org.apache.eventmesh.connector.dingding.config.DingDingMessageTemplateType;
+import org.apache.eventmesh.connector.dingding.sink.config.DingDingSinkConfig;
+import org.apache.eventmesh.openconnect.api.config.Config;
+import org.apache.eventmesh.openconnect.api.connector.ConnectorContext;
+import org.apache.eventmesh.openconnect.api.connector.SinkConnectorContext;
+import org.apache.eventmesh.openconnect.api.sink.Sink;
+import org.apache.eventmesh.openconnect.offsetmgmt.api.data.ConnectRecord;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+import com.aliyun.dingtalkoauth2_1_0.models.GetAccessTokenRequest;
+import com.aliyun.dingtalkrobot_1_0.models.OrgGroupSendHeaders;
+import com.aliyun.dingtalkrobot_1_0.models.OrgGroupSendRequest;
+import com.aliyun.tea.TeaException;
+import com.aliyun.teautil.Common;
+import com.aliyun.teautil.models.RuntimeOptions;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DingDingSinkConnector implements Sink {
+
+    public static final Cache<String, String> AUTH_CACHE = CacheBuilder.newBuilder()
+        .initialCapacity(12)
+        .maximumSize(10)
+        .concurrencyLevel(5)
+        .expireAfterWrite(20, TimeUnit.MINUTES)
+        .build();
+
+    public static final String ACCESS_TOKEN_CACHE_KEY = "access_token";
+
+    private DingDingSinkConfig sinkConfig;
+
+    private com.aliyun.dingtalkrobot_1_0.Client sendMessageClient;
+
+    private com.aliyun.dingtalkoauth2_1_0.Client authClient;
+
+    private volatile boolean isRunning = false;
+
+    @Override
+    public Class<? extends Config> configClass() {
+        return DingDingSinkConfig.class;
+    }
+
+    @Override
+    public void init(Config config) throws Exception {
+        // init config for dingding sink connector
+        this.sinkConfig = (DingDingSinkConfig) config;
+        sendMessageClient = createSendMessageClient();
+        authClient = createOAuthClient();
+    }
+
+    @Override
+    public void init(ConnectorContext connectorContext) throws Exception {
+        // init config for dingding source connector
+        SinkConnectorContext sinkConnectorContext = (SinkConnectorContext) connectorContext;
+        this.sinkConfig = (DingDingSinkConfig) sinkConnectorContext.getSinkConfig();
+        sendMessageClient = createSendMessageClient();
+        authClient = createOAuthClient();
+    }
+
+    @Override
+    public void start() {
+        isRunning = true;
+    }
+
+    @Override
+    public void commit(ConnectRecord record) {
+
+    }
+
+    @Override
+    public String name() {
+        return this.sinkConfig.getSinkConnectorConfig().getConnectorName();
+    }
+
+    @Override
+    public void stop() {
+        isRunning = false;
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    @SneakyThrows
+    @Override
+    public void put(List<ConnectRecord> sinkRecords) {
+        for (ConnectRecord record : sinkRecords) {
+            try {
+                if (Objects.isNull(record.getData())) {
+                    log.warn("ConnectRecord data is null, ignore.");
+                    continue;
+                }
+                String accessToken = getAccessToken();
+                OrgGroupSendHeaders orgGroupSendHeaders =
+                    new OrgGroupSendHeaders();
+                orgGroupSendHeaders.xAcsDingtalkAccessToken = accessToken;
+
+                String templateTypeKey = record.getExtension(ConnectRecordExtensionKeys.DINGDING_TEMPLATE_TYPE_KEY);
+                if (null == templateTypeKey || "null".equals(templateTypeKey)) {
+                    templateTypeKey = DingDingMessageTemplateType.PLAIN_TEXT.getTemplateKey();
+                }
+                DingDingMessageTemplateType templateType = DingDingMessageTemplateType.of(templateTypeKey);
+
+                Map<String, String> contentMap = new HashMap<>();
+                if (DingDingMessageTemplateType.PLAIN_TEXT == templateType) {
+                    contentMap.put("content", new String((byte[]) record.getData()));
+                } else if (DingDingMessageTemplateType.MARKDOWN == templateType) {
+                    String title = Optional.ofNullable(record.getExtension(ConnectRecordExtensionKeys.DINGDING_MARKDOWN_MESSAGE_TITLE))
+                        .orElse("EventMesh-Message");
+                    contentMap.put("title", title);
+                    contentMap.put("text", String.valueOf(record.getData()));
+                }
+
+                OrgGroupSendRequest orgGroupSendRequest =
+                    new OrgGroupSendRequest()
+                        .setMsgParam(JsonUtils.toJSONString(contentMap))
+                        .setMsgKey(templateType.getTemplateKey())
+                        .setOpenConversationId(sinkConfig.getSinkConnectorConfig().getOpenConversationId())
+                        .setRobotCode(sinkConfig.getSinkConnectorConfig().getRobotCode())
+                        .setCoolAppCode(sinkConfig.getSinkConnectorConfig().getCoolAppCode());
+
+                try {
+                    sendMessageClient.orgGroupSendWithOptions(orgGroupSendRequest, orgGroupSendHeaders, new RuntimeOptions());
+                } catch (TeaException e) {
+                    if (!Common.empty(e.code) && !Common.empty(e.message)) {
+                        String errorMessage = e.getMessage();
+                        if ("invalidParameter.token.invalid".equals(errorMessage)) {
+                            AUTH_CACHE.invalidate(ACCESS_TOKEN_CACHE_KEY);
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                log.error("Failed to sink message to DingDing.", e);
+            }
+        }
+    }
+
+    @SneakyThrows
+    private String getAccessToken() {
+        return AUTH_CACHE.get(ACCESS_TOKEN_CACHE_KEY, () -> {
+            GetAccessTokenRequest getAccessTokenRequest =
+                new GetAccessTokenRequest()
+                    .setAppKey(sinkConfig.getSinkConnectorConfig().getAppKey())
+                    .setAppSecret(sinkConfig.getSinkConnectorConfig().getAppSecret());
+            return authClient.getAccessToken(getAccessTokenRequest).getBody().getAccessToken();
+        });
+    }
+
+    public static com.aliyun.dingtalkrobot_1_0.Client createSendMessageClient() throws Exception {
+        com.aliyun.teaopenapi.models.Config config = new com.aliyun.teaopenapi.models.Config();
+        config.protocol = "https";
+        config.regionId = "central";
+        return new com.aliyun.dingtalkrobot_1_0.Client(config);
+    }
+
+    public static com.aliyun.dingtalkoauth2_1_0.Client createOAuthClient() throws Exception {
+        com.aliyun.teaopenapi.models.Config config = new com.aliyun.teaopenapi.models.Config();
+        config.protocol = "https";
+        config.regionId = "central";
+        return new com.aliyun.dingtalkoauth2_1_0.Client(config);
+    }
+}
---
+++ eventmesh-connectors/eventmesh-connector-dingding/src/test/java/org/apache/eventmesh/connector/dingding/sink/connector/DingDingSinkConnectorTest.java
@@ -0,0 +1,112 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.eventmesh.connector.dingding.sink.connector;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.apache.eventmesh.connector.dingding.common.constants.ConnectRecordExtensionKeys;
+import org.apache.eventmesh.connector.dingding.config.DingDingMessageTemplateType;
+import org.apache.eventmesh.connector.dingding.sink.config.DingDingSinkConfig;
+import org.apache.eventmesh.openconnect.offsetmgmt.api.data.ConnectRecord;
+import org.apache.eventmesh.openconnect.offsetmgmt.api.data.RecordOffset;
+import org.apache.eventmesh.openconnect.offsetmgmt.api.data.RecordPartition;
+import org.apache.eventmesh.openconnect.util.ConfigUtil;
+
+import java.lang.reflect.Field;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.commons.support.HierarchyTraversalMode;
+import org.junit.platform.commons.support.ReflectionSupport;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.aliyun.dingtalkoauth2_1_0.models.GetAccessTokenResponse;
+import com.aliyun.dingtalkoauth2_1_0.models.GetAccessTokenResponseBody;
+
+@ExtendWith(MockitoExtension.class)
+public class DingDingSinkConnectorTest {
+
+    @Spy
+    private DingDingSinkConnector connector;
+
+    @Mock
+    private com.aliyun.dingtalkrobot_1_0.Client sendMessageClient;
+
+    @Mock
+    private com.aliyun.dingtalkoauth2_1_0.Client authClient;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        Mockito.doReturn(null).when(sendMessageClient)
+            .orgGroupSendWithOptions(Mockito.any(), Mockito.any(), Mockito.any());
+        GetAccessTokenResponse response = new GetAccessTokenResponse();
+        GetAccessTokenResponseBody body = new GetAccessTokenResponseBody();
+        body.setAccessToken("testAccessToken");
+        response.setBody(body);
+        Mockito.doReturn(response).when(authClient).getAccessToken(Mockito.any());
+
+        DingDingSinkConfig sinkConfig = (DingDingSinkConfig) ConfigUtil.parse(connector.configClass());
+        connector.init(sinkConfig);
+        Field sendMessageClientField = ReflectionSupport.findFields(connector.getClass(),
+            (f) -> f.getName().equals("sendMessageClient"),
+            HierarchyTraversalMode.BOTTOM_UP).get(0);
+        Field authClientField = ReflectionSupport.findFields(connector.getClass(),
+            (f) -> f.getName().equals("authClient"),
+            HierarchyTraversalMode.BOTTOM_UP).get(0);
+        sendMessageClientField.setAccessible(true);
+        authClientField.setAccessible(true);
+        sendMessageClientField.set(connector, sendMessageClient);
+        authClientField.set(connector, authClient);
+        connector.start();
+    }
+
+    @Test
+    public void testSendMessageToDingDing() throws Exception {
+        final int times = 3;
+        List<ConnectRecord> records = new ArrayList<>();
+        for (int i = 0; i < times; i++) {
+            RecordPartition partition = new RecordPartition();
+            RecordOffset offset = new RecordOffset();
+            ConnectRecord connectRecord = new ConnectRecord(partition, offset,
+                System.currentTimeMillis(), "Hello, EventMesh!".getBytes(StandardCharsets.UTF_8));
+            connectRecord.addExtension(ConnectRecordExtensionKeys.DINGDING_TEMPLATE_TYPE_KEY,
+                DingDingMessageTemplateType.PLAIN_TEXT.getTemplateKey());
+            records.add(connectRecord);
+        }
+        connector.put(records);
+        verify(sendMessageClient, times(times)).orgGroupSendWithOptions(any(), any(), any());
+        // verify for access token cache.
+        verify(authClient, times(1)).getAccessToken(any());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        DingDingSinkConnector.AUTH_CACHE.invalidate(DingDingSinkConnector.ACCESS_TOKEN_CACHE_KEY);
+        connector.stop();
+    }
+}
