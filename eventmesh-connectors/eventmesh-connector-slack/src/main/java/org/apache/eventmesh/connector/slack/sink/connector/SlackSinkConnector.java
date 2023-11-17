39044fd7e22264c3d8a388d9f92349585fa48b18
yanrongzhen <bruceyan@didiglobal.com>
2023-11-17 15:08:25 +0800

    Add slack sink connector.

---
+++ eventmesh-connectors/eventmesh-connector-slack/src/main/java/org/apache/eventmesh/connector/slack/sink/connector/SlackSinkConnector.java
@@ -0,0 +1,130 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.eventmesh.connector.slack.sink.connector;
+
+import org.apache.eventmesh.connector.slack.sink.config.SlackSinkConfig;
+import org.apache.eventmesh.openconnect.api.config.Config;
+import org.apache.eventmesh.openconnect.api.connector.ConnectorContext;
+import org.apache.eventmesh.openconnect.api.connector.SinkConnectorContext;
+import org.apache.eventmesh.openconnect.api.sink.Sink;
+import org.apache.eventmesh.openconnect.offsetmgmt.api.data.ConnectRecord;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+import java.util.Objects;
+
+import com.slack.api.Slack;
+import com.slack.api.methods.MethodsClient;
+import com.slack.api.methods.response.chat.ChatPostMessageResponse;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Slack sink connector.
+ * Slack doc: <a href="https://api.slack.com/messaging/sending">...</a>
+ */
+@Slf4j
+public class SlackSinkConnector implements Sink {
+
+    private SlackSinkConfig sinkConfig;
+
+    private volatile boolean isRunning = false;
+
+    private MethodsClient client;
+
+    @Override
+    public Class<? extends Config> configClass() {
+        return SlackSinkConfig.class;
+    }
+
+    @Override
+    public void init(Config config) throws Exception {
+        // init config for dingding sink connector
+        this.sinkConfig = (SlackSinkConfig) config;
+        this.client = Slack.getInstance().methods();
+    }
+
+    @Override
+    public void init(ConnectorContext connectorContext) throws Exception {
+        // init config for dingding source connector
+        SinkConnectorContext sinkConnectorContext = (SinkConnectorContext) connectorContext;
+        this.sinkConfig = (SlackSinkConfig) sinkConnectorContext.getSinkConfig();
+        this.client = Slack.getInstance().methods();
+    }
+
+    @Override
+    public void start() {
+        isRunning = true;
+    }
+
+    @Override
+    public void commit(ConnectRecord record) {
+
+    }
+
+    @Override
+    public String name() {
+        return this.sinkConfig.getSinkConnectorConfig().getConnectorName();
+    }
+
+    @Override
+    public void stop() {
+        isRunning = false;
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    @SneakyThrows
+    @Override
+    public void put(List<ConnectRecord> sinkRecords) {
+        for (ConnectRecord record : sinkRecords) {
+            publishMessage(record);
+        }
+    }
+
+    private void publishMessage(ConnectRecord record) {
+        try {
+            ChatPostMessageResponse response = client.chatPostMessage(r -> r
+                .token(sinkConfig.getSinkConnectorConfig().getAppToken())
+                .channel(sinkConfig.getSinkConnectorConfig().getChannelId())
+                .text(new String((byte[]) record.getData())));
+            if (Objects.nonNull(response) && StringUtils.isNotBlank(response.getError())) {
+                throw new IllegalAccessException(response.getError());
+            }
+        } catch (Exception e) {
+            log.error("error: {}", e.getMessage(), e);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            MethodsClient client = Slack.getInstance().methods();
+            ChatPostMessageResponse response = client.chatPostMessage(r -> r
+                .token("xoxb-6210178185715-6196113953063-aTrzZ5wiHfyZ3tjm1jE6R6ez")
+                .channel("C06668DA3GA")
+                .text("Hello, EventMesh Slack Connector!"));
+            log.info("result {}", response);
+        } catch (Exception e) {
+            log.error("error: {}", e.getMessage(), e);
+        }
+    }
+}
---
+++ eventmesh-connectors/eventmesh-connector-slack/src/test/java/org/apache/eventmesh/connector/slack/sink/connector/SlackSinkConnectorTest.java
@@ -0,0 +1,91 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.eventmesh.connector.slack.sink.connector;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.apache.eventmesh.connector.slack.sink.config.SlackSinkConfig;
+import org.apache.eventmesh.openconnect.offsetmgmt.api.data.ConnectRecord;
+import org.apache.eventmesh.openconnect.offsetmgmt.api.data.RecordOffset;
+import org.apache.eventmesh.openconnect.offsetmgmt.api.data.RecordPartition;
+import org.apache.eventmesh.openconnect.util.ConfigUtil;
+
+import java.lang.reflect.Field;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.commons.support.HierarchyTraversalMode;
+import org.junit.platform.commons.support.ReflectionSupport;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.slack.api.RequestConfigurator;
+import com.slack.api.methods.MethodsClient;
+import com.slack.api.methods.response.chat.ChatPostMessageResponse;
+
+@ExtendWith(MockitoExtension.class)
+public class SlackSinkConnectorTest {
+
+    private SlackSinkConnector sinkConnector;
+
+    @Mock
+    private MethodsClient client;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        sinkConnector = new SlackSinkConnector();
+        SlackSinkConfig sinkConfig = (SlackSinkConfig) ConfigUtil.parse(sinkConnector.configClass());
+        sinkConnector.init(sinkConfig);
+        doReturn(new ChatPostMessageResponse())
+            .when(client).chatPostMessage(any(RequestConfigurator.class));
+        Field clientField = ReflectionSupport.findFields(sinkConnector.getClass(),
+            (f) -> f.getName().equals("client"),
+            HierarchyTraversalMode.BOTTOM_UP).get(0);
+        clientField.setAccessible(true);
+        clientField.set(sinkConnector, client);
+        sinkConnector.start();
+    }
+
+    @Test
+    public void testSendMessageToSlack() throws Exception {
+        final int times = 3;
+        List<ConnectRecord> records = new ArrayList<>();
+        for (int i = 0; i < times; i++) {
+            RecordPartition partition = new RecordPartition();
+            RecordOffset offset = new RecordOffset();
+            ConnectRecord connectRecord = new ConnectRecord(partition, offset,
+                System.currentTimeMillis(), "Hello, EventMesh!".getBytes(StandardCharsets.UTF_8));
+            records.add(connectRecord);
+        }
+        sinkConnector.put(records);
+        verify(client, times(times)).chatPostMessage(any(RequestConfigurator.class));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        sinkConnector.stop();
+    }
+}
