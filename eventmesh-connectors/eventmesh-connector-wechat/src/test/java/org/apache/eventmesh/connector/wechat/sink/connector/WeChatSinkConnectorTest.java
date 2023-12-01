eb957d073e9ae161ceaa4e7f7699487eae247ec7
张忠炜#A03088 <1099297353@qq.com>
2023-12-02 01:26:02 +0800

    fix the unit test

--- eventmesh-connectors/eventmesh-connector-wechat/src/test/java/org/apache/eventmesh/connector/wechat/sink/connector/WeChatSinkConnectorTest.java
+++ eventmesh-connectors/eventmesh-connector-wechat/src/test/java/org/apache/eventmesh/connector/wechat/sink/connector/WeChatSinkConnectorTest.java
@@ -18,14 +18,26 @@
 package org.apache.eventmesh.connector.wechat.sink.connector;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import okhttp3.Call;
+import okhttp3.MediaType;
+import okhttp3.Protocol;
 import org.apache.eventmesh.connector.wechat.sink.config.WeChatSinkConfig;
+import org.apache.eventmesh.openconnect.offsetmgmt.api.data.ConnectRecord;
+import org.apache.eventmesh.openconnect.offsetmgmt.api.data.RecordOffset;
+import org.apache.eventmesh.openconnect.offsetmgmt.api.data.RecordPartition;
 import org.apache.eventmesh.openconnect.util.ConfigUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.commons.support.HierarchyTraversalMode;
 import org.junit.platform.commons.support.ReflectionSupport;
+import org.mockito.ArgumentMatcher;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -50,12 +62,38 @@ public class WeChatSinkConnectorTest {
     public void setUp() throws Exception {
         weChatSinkConnector = new WeChatSinkConnector();
         WeChatSinkConfig weChatSinkConfig = (WeChatSinkConfig) ConfigUtil.parse(weChatSinkConnector.configClass());
-        weChatSinkConnector.init(weChatSinkConfig);
-        Response mockedResponse = Mockito.mock(Response.class);
-        ResponseBody responseBody = Mockito.mock(ResponseBody.class);
-        Mockito.doReturn(responseBody).when(mockedResponse).body();
-        Mockito.doReturn(mockedResponse).when(okHttpClient).newCall(any(Request.class)).execute();
 
+        Request tokenRequest = new Request.Builder().url("https://api.weixin.qq.com/cgi-bin/token").build();
+        String tokenResponseJson = "{\"access_token\":\"ACCESS_TOKEN\",\"expires_in\":7200}";
+        ResponseBody responseBody = ResponseBody.create(tokenResponseJson, MediaType.parse("application/json; charset=utf-8"));
+        Response tokenResponse = new Response.Builder()
+            .request(tokenRequest)
+            .protocol(Protocol.HTTP_1_0)
+            .message("ok")
+            .code(200)
+            .body(responseBody)
+            .build();
+        ArgumentMatcher<Request> tokenMatcher = (anyRequest) -> tokenRequest.url().encodedPath().startsWith(anyRequest.url().encodedPath());
+        Call tokenCall = Mockito.mock(Call.class);
+        Mockito.doReturn(tokenCall).when(okHttpClient).newCall(Mockito.argThat(tokenMatcher));
+        Mockito.doReturn(tokenResponse).when(tokenCall).execute();
+
+        Request sendMessageRequest = new Request.Builder().url("https://api.weixin.qq.com/cgi-bin/message/template/send").build();
+        String sendMessageResponseJson = "{\"errcode\":0,\"errmsg\":\"ok\",\"msgid\":200228332}";
+        ResponseBody sendMessageBody = ResponseBody.create(sendMessageResponseJson, MediaType.parse("application/json; charset=utf-8"));
+        Response sendMessageResponse = new Response.Builder()
+            .code(200)
+            .protocol(Protocol.HTTP_1_0)
+            .request(sendMessageRequest)
+            .body(sendMessageBody)
+            .message("ok")
+            .build();
+        ArgumentMatcher<Request> sendMessageMatcher = (anyRequest) -> sendMessageRequest.url().encodedPath().startsWith(anyRequest.url().encodedPath());
+        Call sendMessageRequestCall = Mockito.mock(Call.class);
+        Mockito.doReturn(sendMessageRequestCall).when(okHttpClient).newCall(Mockito.argThat(sendMessageMatcher));
+        Mockito.doReturn(sendMessageResponse).when(sendMessageRequestCall).execute();
+
+        weChatSinkConnector.init(weChatSinkConfig);
         Field clientField = ReflectionSupport.findFields(weChatSinkConnector.getClass(),
             (f) -> f.getName().equals("okHttpClient"),
             HierarchyTraversalMode.BOTTOM_UP).get(0);
@@ -66,6 +104,18 @@ public class WeChatSinkConnectorTest {
 
     @Test
     public void testSendMessageToWeChat() throws Exception {
+        final int times = 1;
+        List<ConnectRecord> records = new ArrayList<>();
+        for (int i = 0; i < times; i++) {
+            RecordPartition partition = new RecordPartition();
+            RecordOffset offset = new RecordOffset();
+            ConnectRecord connectRecord = new ConnectRecord(partition, offset,
+                System.currentTimeMillis(), "Hello, EventMesh!".getBytes(StandardCharsets.UTF_8));
+            records.add(connectRecord);
+        }
+
+        weChatSinkConnector.put(records);
+        verify(okHttpClient, times(times + 1)).newCall(any(Request.class));
     }
 
 }
