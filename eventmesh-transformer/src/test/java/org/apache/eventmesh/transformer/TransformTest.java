a2d7da225f9fe88f937b501c6c01dd9b6adb2f6e
pmupkin <399284508@qq.com>
2023-12-14 00:04:19 +0800

    fix CI issue v1

--- eventmesh-transformer/src/test/java/org/apache/eventmesh/transformer/TransformTest.java
+++ eventmesh-transformer/src/test/java/org/apache/eventmesh/transformer/TransformTest.java
@@ -143,6 +143,7 @@ public class TransformTest {
     public void testTemplateTransFormerWithNonDataItem() {
         String extractJson = "{\"name\":\"$.data.name\",\"time\":\"$.time\"" + "}";
         String template = "Transformers test:data name is ${name}, time is ${time}";
+
         Throwable exception = Assertions.assertThrows(TransformException.class, ()->TransformerBuilder.buildTemplateTransFormer(extractJson, template));
         Assertions.assertEquals("invalid config: unsupported value {\"name\":\"$.data.name\",\"time\":\"$.time\"}" , exception.getMessage());
     }
