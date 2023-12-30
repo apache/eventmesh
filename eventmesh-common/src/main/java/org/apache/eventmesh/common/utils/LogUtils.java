854f39f940b720ff640c432043ded7c444e6cddb
Piotr P. Karwasz <piotr.github@karwasz.org>
2023-12-30 11:45:57 +0100

    Deprecate `LogUtils` for removal
    The `LogUtils` class is not useful because:
    
     * it wraps every logger call in `isLevelEnabled`. These are not
       necessary since the arguments to the logger calls are simple
       variables and not computationally complex expressions,
     * currently it breaks location detection of the logger calls, which
       will always show `LogUtils` as origin,
     * it prevents the project from using source code rewrite tools like
       [`rewrite-logging-frameworks`](https://github.com/openrewrite/rewrite-logging-frameworks).
    
    This commit adds deprecates the class for removal in the next major
    version and adds Error Prone's `@InlineMe` annotations, to help users
    automatically rewrite code based on `LogUtils`.

--- eventmesh-common/src/main/java/org/apache/eventmesh/common/utils/LogUtils.java
+++ eventmesh-common/src/main/java/org/apache/eventmesh/common/utils/LogUtils.java
@@ -20,191 +20,231 @@ package org.apache.eventmesh.common.utils;
 import org.slf4j.Logger;
 import org.slf4j.Marker;
 
+import com.google.errorprone.annotations.InlineMe;
+
 import lombok.experimental.UtilityClass;
 
+/**
+ * This class will be removed in the next major release.
+ * Use the {@link Logger} class directly.
+ */
+@Deprecated
 @UtilityClass
 public final class LogUtils {
 
+    @InlineMe(replacement = "logger.isTraceEnabled()")
     public static boolean isTraceEnabled(Logger logger) {
         return logger.isTraceEnabled();
     }
 
+    @InlineMe(replacement = "logger.isTraceEnabled(marker)")
     public static boolean isTraceEnabled(Logger logger, Marker marker) {
         return logger.isTraceEnabled(marker);
     }
 
+    @InlineMe(replacement = "logger.trace(msg)")
     public static void trace(Logger logger, String msg) {
         if (isTraceEnabled(logger)) {
             logger.trace(msg);
         }
     }
 
+    @InlineMe(replacement = "logger.trace(format, arg)")
     public static void trace(Logger logger, String format, Object arg) {
         if (isTraceEnabled(logger)) {
             logger.trace(format, arg);
         }
     }
 
+    @InlineMe(replacement = "logger.trace(msg, t)")
     public static void trace(Logger logger, String msg, Throwable t) {
         if (isTraceEnabled(logger)) {
             logger.trace(msg, t);
         }
     }
 
+    @InlineMe(replacement = "logger.trace(format, arguments)")
     public static void trace(Logger logger, String format, Object... arguments) {
         if (isTraceEnabled(logger)) {
             logger.trace(format, arguments);
         }
     }
 
+    @InlineMe(replacement = "logger.trace(format, arg1, arg2)")
     public static void trace(Logger logger, String format, Object arg1, Object arg2) {
         if (isTraceEnabled(logger)) {
             logger.trace(format, arg1, arg2);
         }
     }
 
+    @InlineMe(replacement = "logger.trace(marker, msg)")
     public static void trace(Logger logger, Marker marker, String msg) {
         if (isTraceEnabled(logger, marker)) {
             logger.trace(marker, msg);
         }
     }
 
+    @InlineMe(replacement = "logger.trace(marker, format, arg)")
     public static void trace(Logger logger, Marker marker, String format, Object arg) {
         if (isTraceEnabled(logger, marker)) {
             logger.trace(marker, format, arg);
         }
     }
 
+    @InlineMe(replacement = "logger.isDebugEnabled()")
     public static boolean isDebugEnabled(Logger logger) {
         return logger.isDebugEnabled();
     }
 
+    @InlineMe(replacement = "logger.debug(msg)")
     public static void debug(Logger logger, String msg) {
         if (isDebugEnabled(logger)) {
             logger.debug(msg);
         }
     }
 
+    @InlineMe(replacement = "logger.debug(format, arg)")
     public static void debug(Logger logger, String format, Object arg) {
         if (isDebugEnabled(logger)) {
             logger.debug(format, arg);
         }
     }
 
+    @InlineMe(replacement = "logger.debug(format, arg1, arg2)")
     public static void debug(Logger logger, String format, Object arg1, Object arg2) {
         if (isDebugEnabled(logger)) {
             logger.debug(format, arg1, arg2);
         }
     }
 
+    @InlineMe(replacement = "logger.debug(format, arguments)")
     public static void debug(Logger logger, String format, Object... arguments) {
         if (isDebugEnabled(logger)) {
             logger.debug(format, arguments);
         }
     }
 
+    @InlineMe(replacement = "logger.debug(msg, t)")
     public static void debug(Logger logger, String msg, Throwable t) {
         if (isDebugEnabled(logger)) {
             logger.debug(msg, t);
         }
     }
 
+    @InlineMe(replacement = "logger.isInfoEnabled()")
     public static boolean isInfoEnabled(Logger logger) {
         return logger.isInfoEnabled();
     }
 
+    @InlineMe(replacement = "logger.info(msg)")
     public static void info(Logger logger, String msg) {
         if (isInfoEnabled(logger)) {
             logger.info(msg);
         }
     }
 
+    @InlineMe(replacement = "logger.info(format, arg)")
     public static void info(Logger logger, String format, Object arg) {
         if (isInfoEnabled(logger)) {
             logger.info(format, arg);
         }
     }
 
+    @InlineMe(replacement = "logger.info(format, arg1, arg2)")
     public static void info(Logger logger, String format, Object arg1, Object arg2) {
         if (isInfoEnabled(logger)) {
             logger.info(format, arg1, arg2);
         }
     }
 
+    @InlineMe(replacement = "logger.info(format, arguments)")
     public static void info(Logger logger, String format, Object... arguments) {
         if (isInfoEnabled(logger)) {
             logger.info(format, arguments);
         }
     }
 
+    @InlineMe(replacement = "logger.info(msg, t)")
     public static void info(Logger logger, String msg, Throwable t) {
         if (isInfoEnabled(logger)) {
             logger.info(msg, t);
         }
     }
 
+    @InlineMe(replacement = "logger.isWarnEnabled()")
     public static boolean isWarnEnabled(Logger logger) {
         return logger.isWarnEnabled();
     }
 
+    @InlineMe(replacement = "logger.warn(msg)")
     public static void warn(Logger logger, String msg) {
         if (isWarnEnabled(logger)) {
             logger.warn(msg);
         }
     }
 
+    @InlineMe(replacement = "logger.warn(format, arg)")
     public static void warn(Logger logger, String format, Object arg) {
         if (isWarnEnabled(logger)) {
             logger.warn(format, arg);
         }
     }
 
+    @InlineMe(replacement = "logger.warn(format, arg1, arg2)")
     public static void warn(Logger logger, String format, Object arg1, Object arg2) {
         if (isWarnEnabled(logger)) {
             logger.warn(format, arg1, arg2);
         }
     }
 
+    @InlineMe(replacement = "logger.warn(format, arguments)")
     public static void warn(Logger logger, String format, Object... arguments) {
         if (isWarnEnabled(logger)) {
             logger.warn(format, arguments);
         }
     }
 
+    @InlineMe(replacement = "logger.warn(msg, t)")
     public static void warn(Logger logger, String msg, Throwable t) {
         if (isWarnEnabled(logger)) {
             logger.warn(msg, t);
         }
     }
 
+    @InlineMe(replacement = "logger.isErrorEnabled()")
     public static boolean isErrorEnabled(Logger logger) {
         return logger.isErrorEnabled();
     }
 
+    @InlineMe(replacement = "logger.error(msg)")
     public static void error(Logger logger, String msg) {
         if (isErrorEnabled(logger)) {
             logger.error(msg);
         }
     }
 
+    @InlineMe(replacement = "logger.error(format, arg)")
     public static void error(Logger logger, String format, Object arg) {
         if (isErrorEnabled(logger)) {
             logger.error(format, arg);
         }
     }
 
+    @InlineMe(replacement = "logger.error(format, arg1, arg2)")
     public static void error(Logger logger, String format, Object arg1, Object arg2) {
         if (isErrorEnabled(logger)) {
             logger.error(format, arg1, arg2);
         }
     }
 
+    @InlineMe(replacement = "logger.error(format, arguments)")
     public static void error(Logger logger, String format, Object... arguments) {
         if (isErrorEnabled(logger)) {
             logger.error(format, arguments);
         }
     }
 
+    @InlineMe(replacement = "logger.error(msg, t)")
     public static void error(Logger logger, String msg, Throwable t) {
         if (isErrorEnabled(logger)) {
             logger.error(msg, t);
