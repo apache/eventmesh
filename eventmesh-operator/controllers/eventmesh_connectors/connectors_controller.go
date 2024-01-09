3dd6f3b62169fbc1a32077efd9e24a157d88c1ce
Alonexc <alonexc12581@163.com>
2024-01-09 10:35:42 +0800

    deployment operator, commands simplified.

--- eventmesh-operator/controllers/eventmesh_connectors/connectors_controller.go
+++ eventmesh-operator/controllers/eventmesh_connectors/connectors_controller.go
@@ -98,7 +98,6 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 //+kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups="",resources=pods/exec,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups="apps",resources=statefulsets,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups="apps",resources=deployments,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
