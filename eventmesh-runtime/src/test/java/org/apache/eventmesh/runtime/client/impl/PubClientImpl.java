ff7df467c674bd4e62d3a55b36cc5f5c5a25a977
scwlkq <scwlkq@gmail.com>
2024-01-21 00:40:21 +0800

    add exception arg

--- eventmesh-runtime/src/test/java/org/apache/eventmesh/runtime/client/impl/PubClientImpl.java
+++ eventmesh-runtime/src/test/java/org/apache/eventmesh/runtime/client/impl/PubClientImpl.java
@@ -73,7 +73,7 @@ public class PubClientImpl extends TCPClient implements PubClient {
             task.cancel(false);
             super.close();
         } catch (Exception e) {
-            log.error("PubClientImpl|{}|stopped!", clientNo);
+            log.error("PubClientImpl|{}|stopped!", clientNo, e);
         }
     }
 
