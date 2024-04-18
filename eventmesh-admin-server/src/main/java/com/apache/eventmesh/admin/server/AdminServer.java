4d811a464e5282cbd3503cc9546244453f7c2776
sodaRyCN <757083350@qq.com>
2024-04-18 16:24:26 +0800

    finish registry

--- eventmesh-admin-server/src/main/java/com/apache/eventmesh/admin/server/AdminServer.java
+++ eventmesh-admin-server/src/main/java/com/apache/eventmesh/admin/server/AdminServer.java
@@ -1,10 +1,23 @@
 package com.apache.eventmesh.admin.server;
 
+import com.apache.eventmesh.admin.server.registry.EventMeshAdminServerRegisterInfo;
+import com.apache.eventmesh.admin.server.registry.RegistryService;
 import org.apache.eventmesh.common.utils.PagedList;
 
 import com.apache.eventmesh.admin.server.task.Task;
 
 public class AdminServer implements Admin {
+
+    private RegistryService registryService;
+
+    private EventMeshAdminServerRegisterInfo registerInfo;
+
+    public AdminServer(RegistryService registryService, EventMeshAdminServerRegisterInfo registerInfo) {
+        this.registryService = registryService;
+        this.registerInfo = registerInfo;
+    }
+
+    public static final String ConfigurationKey = "admin-server";
     @Override
     public boolean createOrUpdateTask(Task task) {
         return false;
@@ -33,10 +46,12 @@ public class AdminServer implements Admin {
     @Override
     public void start() {
 
+        registryService.register(registerInfo);
     }
 
     @Override
     public void destroy() {
-
+        registryService.unRegister(registerInfo);
+        registryService.shutdown();
     }
 }
---
+++ eventmesh-admin-server/src/main/java/com/apache/eventmesh/admin/server/registry/AbstractRegistryListener.java
@@ -0,0 +1,14 @@
+package com.apache.eventmesh.admin.server.registry;
+
+public abstract class AbstractRegistryListener<T> implements RegistryListener {
+    protected abstract boolean checkType(Object data);
+    @Override
+    @SuppressWarnings("unchecked")
+    public void onChange(Object data) {
+        if (!checkType(data)) {
+            return;
+        }
+        process((T)data);
+    }
+    protected abstract void process(T data);
+}
