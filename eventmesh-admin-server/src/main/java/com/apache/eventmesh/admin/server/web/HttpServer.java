653292bfc262370f363d05b16a4533bddaa34c67
sodaRyCN <35725024+sodaRyCN@users.noreply.github.com>
2024-04-19 10:26:03 +0800

    EventMesh function admin (#4853)
    * own
    
    * dependency
    
    * finish registry
    
    * init

---
+++ eventmesh-admin-server/src/main/java/com/apache/eventmesh/admin/server/web/HttpServer.java
@@ -0,0 +1,9 @@
+package com.apache.eventmesh.admin.server.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/eventmesh/admin")
+public class HttpServer {
+}
