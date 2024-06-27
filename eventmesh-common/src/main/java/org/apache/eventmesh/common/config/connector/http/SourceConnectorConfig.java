d76ee25dfc9f80129fcfe50c30e8c741cee7fcdf
Zaki <91261012+cnzakii@users.noreply.github.com>
2024-06-27 14:31:36 +0800

    [ISSUE #4869] Add Webhook support for HTTP Source Connector (#4913)
    * feat: add webhook support
    
    * feat: add CommonProtocol and optimize some logic
    
    * doc: update some javaDoc
    
    * fix: update something
    
    * fix: Optimize queue related operations
    
    * fix: Optimize queue related operations
    
    * fix: Optimize queue related operations
    
    * fix: update something
    
    * fix: Remove some meaningless code
    
    * fix: update something
    
    * perf: Updates to protocol-related code, with a focus on request handling
    
    * perf: use CircularFifoQueue
    
    * perf: Use OkHttp Client to completely replace Apache Client and resolve code conflicts.

--- eventmesh-common/src/main/java/org/apache/eventmesh/common/config/connector/http/SourceConnectorConfig.java
+++ eventmesh-common/src/main/java/org/apache/eventmesh/common/config/connector/http/SourceConnectorConfig.java
@@ -17,6 +17,9 @@
 
 package org.apache.eventmesh.common.config.connector.http;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import lombok.Data;
 
 @Data
@@ -28,5 +31,28 @@ public class SourceConnectorConfig {
 
     private int port;
 
-    private int idleTimeout;
+    // timeunit: ms, default 5000ms
+    private int idleTimeout = 5000;
+
+    /**
+     * <ul>
+     *     <li>The maximum size allowed for form attributes when Content-Type is application/x-www-form-urlencoded or multipart/form-data </li>
+     *     <li>Default is 1MB (1024 * 1024 bytes). </li>
+     *     <li>If you receive a "size exceed allowed maximum capacity" error, you can increase this value. </li>
+     *     <li>Note: This applies only when handling form data submissions.</li>
+     * </ul>
+     */
+    private int maxFormAttributeSize = 1024 * 1024;
+
+    // max size of the queue, default 1000
+    private int maxStorageSize = 1000;
+
+    // batch size, default 10
+    private int batchSize = 10;
+
+    // protocol, default CloudEvent
+    private String protocol = "CloudEvent";
+
+    // extra config, e.g. GitHub secret
+    private Map<String, String> extraConfig = new HashMap<>();
 }
